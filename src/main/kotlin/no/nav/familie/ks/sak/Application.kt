94549c823b8cb97a59a1d8564506465bd9010304
Uy Quang Nguyen <uy.quang.nguyen@nav.no>
2022-09-07 14:41:37 +0200

    Tea-9476: Adjust build-pr yaml for test

---
+++ src/main/kotlin/no/nav/familie/ks/sak/Application.kt
@@ -0,0 +1,11 @@
+package no.nav.familie.ks.sak
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FamilieKsSakApplication
+
+fun main(args: Array<String>) {
+    runApplication<FamilieKsSakApplication>(*args)
+}
