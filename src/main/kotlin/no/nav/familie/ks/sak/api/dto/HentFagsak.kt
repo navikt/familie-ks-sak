6ea4c4ffa5dcc1414e60043b4f362f1931619073
haldersaikat <saikat.halder@nav.no>
2022-09-22 09:48:22 +0200

    fikset respons fra string til Enum

--- src/main/kotlin/no/nav/familie/ks/sak/api/dto/HentFagsak.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/api/dto/HentFagsak.kt
@@ -1,5 +1,15 @@
 package no.nav.familie.ks.sak.api.dto
 
+import no.nav.familie.kontrakter.felles.tilbakekreving.Behandlingsresultatstype
+import no.nav.familie.kontrakter.felles.tilbakekreving.Behandlingsstatus
+import no.nav.familie.kontrakter.felles.tilbakekreving.Behandlingstype
+import no.nav.familie.kontrakter.felles.tilbakekreving.Behandlingsårsakstype
+import no.nav.familie.ks.sak.kjerne.behandling.domene.BehandlingKategori
+import no.nav.familie.ks.sak.kjerne.behandling.domene.BehandlingStatus
+import no.nav.familie.ks.sak.kjerne.behandling.domene.BehandlingType
+import no.nav.familie.ks.sak.kjerne.behandling.domene.Behandlingsresultat
+import no.nav.familie.ks.sak.kjerne.behandling.domene.BehandlingÅrsak
+import no.nav.familie.ks.sak.kjerne.fagsak.domene.FagsakStatus
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.UUID
@@ -13,23 +23,23 @@ data class MinimalFagsakResponsDto(
     val opprettetTidspunkt: LocalDateTime,
     val id: Long,
     val søkerFødselsnummer: String,
-    val status: String,
+    val status: FagsakStatus,
     val underBehandling: Boolean,
-    val løpendeKategori: String?,
-    val behandlinger: List<BehandlingResponsDto> = emptyList(),
+    val løpendeKategori: BehandlingKategori?,
+    val behandlinger: List<MinimalBehandlingResponsDto> = emptyList(),
     val tilbakekrevingsbehandlinger: List<TilbakekrevingsbehandlingResponsDto> = emptyList(),
     val gjeldendeUtbetalingsperioder: List<UtbetalingsperiodeResponsDto> = emptyList()
 )
 
-data class BehandlingResponsDto(
+data class MinimalBehandlingResponsDto(
     val behandlingId: Long,
     val opprettetTidspunkt: LocalDateTime,
-    val kategori: String,
+    val kategori: BehandlingKategori,
     val aktiv: Boolean,
-    val årsak: String?,
-    val type: String,
-    val status: String,
-    val resultat: String,
+    val årsak: BehandlingÅrsak?,
+    val type: BehandlingType,
+    val status: BehandlingStatus,
+    val resultat: Behandlingsresultat,
     val vedtaksdato: LocalDateTime?
 )
 
@@ -37,10 +47,10 @@ data class TilbakekrevingsbehandlingResponsDto(
     val behandlingId: UUID,
     val opprettetTidspunkt: LocalDateTime,
     val aktiv: Boolean,
-    val årsak: String?,
-    val type: String,
-    val status: String,
-    val resultat: String?,
+    val årsak: Behandlingsårsakstype?,
+    val type: Behandlingstype,
+    val status: Behandlingsstatus,
+    val resultat: Behandlingsresultatstype?,
     val vedtaksdato: LocalDateTime?
 )
 
