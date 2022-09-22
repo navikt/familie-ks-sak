a7abe80c8320460fd0698c3d0c7fc40e27e6b9cc
haldersaikat <saikat.halder@nav.no>
2022-09-22 15:24:33 +0200

    la til hentBehandling tjeneste for å hente behandling detaljer

--- src/main/kotlin/no/nav/familie/ks/sak/api/BehandlingController.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/api/BehandlingController.kt
@@ -8,6 +8,8 @@ import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -24,5 +26,10 @@ class BehandlingController(private val behandlingService: BehandlingService) {
         val behandling = behandlingService.opprettBehandling(opprettBehandlingDto)
 
         return ResponseEntity.ok(Ressurs.success(behandlingService.lagBehandlingRespons(behandlingId = behandling.id)))
+    }
+
+    @GetMapping(path = ["/{behandlingId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun hentUtvidetBehandling(@PathVariable behandlingId: Long): ResponseEntity<Ressurs<BehandlingResponsDto>> {
+        return ResponseEntity.ok(Ressurs.success(behandlingService.lagBehandlingRespons(behandlingId = behandlingId)))
     }
 }
