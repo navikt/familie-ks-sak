81eb23deb6743d4a200998a2e54fc4ffa1e8a9cb
bragejahren <brage.ekroll.jahren@nav.no>
2022-10-05 08:05:48 +0200

    Oppdatert VilkårsvurderingRepositoryTest i henhold til endringer i OppslagSpringRunnerTest

--- src/test/integrasjonstester/kotlin/no/nav/familie/ks/sak/kjerne/vilkårsvurdering/domene/VilkårsvurderingRepositoryTest.kt
+++ src/test/integrasjonstester/kotlin/no/nav/familie/ks/sak/kjerne/vilkårsvurdering/domene/VilkårsvurderingRepositoryTest.kt
@@ -1,14 +1,8 @@
 package no.nav.familie.ks.sak.no.nav.familie.ks.sak.kjerne.vilkårsvurdering.domene
 
 import no.nav.familie.ks.sak.OppslagSpringRunnerTest
-import no.nav.familie.ks.sak.data.lagBehandling
-import no.nav.familie.ks.sak.data.lagFagsak
 import no.nav.familie.ks.sak.data.lagVilkårsvurdering
 import no.nav.familie.ks.sak.data.randomAktør
-import no.nav.familie.ks.sak.kjerne.behandling.domene.Behandling
-import no.nav.familie.ks.sak.kjerne.behandling.domene.BehandlingÅrsak
-import no.nav.familie.ks.sak.kjerne.fagsak.domene.Fagsak
-import no.nav.familie.ks.sak.kjerne.personident.Aktør
 import no.nav.familie.ks.sak.kjerne.vilkårsvurdering.domene.Resultat
 import no.nav.familie.ks.sak.kjerne.vilkårsvurdering.domene.VilkårsvurderingRepository
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -23,15 +17,9 @@ internal class VilkårsvurderingRepositoryTest : OppslagSpringRunnerTest() {
     @Autowired
     private lateinit var vilkårsvurderingRepository: VilkårsvurderingRepository
 
-    private lateinit var søker: Aktør
-    private lateinit var fagsak: Fagsak
-    private lateinit var behandling: Behandling
-
     @BeforeEach
     fun beforeEach() {
-        søker = lagreAktør(randomAktør())
-        fagsak = lagreFagsak(lagFagsak(søker))
-        behandling = lagreBehandling(lagBehandling(fagsak, opprettetÅrsak = BehandlingÅrsak.SØKNAD))
+        opprettSøkerFagsakOgBehandling(randomAktør())
     }
 
     @Test
