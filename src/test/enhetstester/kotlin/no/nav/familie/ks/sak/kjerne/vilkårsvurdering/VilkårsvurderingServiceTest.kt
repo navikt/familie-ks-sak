669fe68cb0fb915589ebcf990f3656646b88c674
bragejahren <brage.ekroll.jahren@nav.no>
2022-10-12 15:40:21 +0200

    Lagt til profilen 'integrasjonstest' som setter opp lokal auth server og lagt inn test for nytt endepunkt

--- src/test/enhetstester/kotlin/no/nav/familie/ks/sak/kjerne/vilkårsvurdering/VilkårsvurderingServiceTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/kjerne/vilkårsvurdering/VilkårsvurderingServiceTest.kt
@@ -3,13 +3,22 @@ package no.nav.familie.ks.sak.kjerne.vilkårsvurdering
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
 import io.mockk.mockk
 import io.mockk.slot
-import no.nav.familie.ks.sak.OppslagSpringRunnerTest
+import no.nav.familie.ks.sak.data.lagBehandling
+import no.nav.familie.ks.sak.data.lagFagsak
 import no.nav.familie.ks.sak.data.lagPersonopplysningGrunnlag
 import no.nav.familie.ks.sak.data.lagVilkårsvurdering
 import no.nav.familie.ks.sak.data.randomAktør
+import no.nav.familie.ks.sak.integrasjon.sanity.SanityService
+import no.nav.familie.ks.sak.integrasjon.sanity.domene.SanityBegrunnelse
+import no.nav.familie.ks.sak.integrasjon.sanity.domene.SanityEØSBegrunnelse
+import no.nav.familie.ks.sak.kjerne.behandling.domene.BehandlingÅrsak
 import no.nav.familie.ks.sak.kjerne.personopplysninggrunnlag.PersonopplysningGrunnlagService
+import no.nav.familie.ks.sak.kjerne.vedtak.EØSStandardbegrunnelse
+import no.nav.familie.ks.sak.kjerne.vedtak.Standardbegrunnelse
+import no.nav.familie.ks.sak.kjerne.vedtak.VedtakBegrunnelseType
 import no.nav.familie.ks.sak.kjerne.vilkårsvurdering.domene.Resultat
 import no.nav.familie.ks.sak.kjerne.vilkårsvurdering.domene.Vilkår
 import no.nav.familie.ks.sak.kjerne.vilkårsvurdering.domene.Vilkårsvurdering
@@ -18,10 +27,11 @@ import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.containsInAnyOrder
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
-class VilkårsvurderingServiceTest : OppslagSpringRunnerTest() {
+@ExtendWith(MockKExtension::class)
+class VilkårsvurderingServiceTest {
 
     @MockK
     private lateinit var vilkårsvurderingRepository: VilkårsvurderingRepository
@@ -29,18 +39,23 @@ class VilkårsvurderingServiceTest : OppslagSpringRunnerTest() {
     @MockK
     private lateinit var personopplysningGrunnlagService: PersonopplysningGrunnlagService
 
+    @MockK
+    private lateinit var sanityService: SanityService
+
     @InjectMockKs
     private lateinit var vilkårsvurderingService: VilkårsvurderingService
 
-    @BeforeEach
-    fun beforeEach() {
-        opprettSøkerFagsakOgBehandling()
-    }
+    private val søker = randomAktør()
+
+    private val fagsak = lagFagsak(søker)
+
+    private val behandling = lagBehandling(fagsak, opprettetÅrsak = BehandlingÅrsak.SØKNAD)
 
     @Test
     fun `opprettVilkårsvurdering - skal opprette tom vilkårsvurdering dersom det ikke finnes tidligere vedtatte behandlinger på fagsak`() {
-        val barn1 = lagreAktør(randomAktør())
-        val barn2 = lagreAktør(randomAktør())
+        val barn1 = randomAktør()
+        val barn2 = randomAktør()
+
         val lagretVilkårsvurderingSlot = slot<Vilkårsvurdering>()
         every { vilkårsvurderingRepository.finnAktiv(any()) } returns null
         every { personopplysningGrunnlagService.hentAktivPersonopplysningGrunnlagThrows(any()) } returns lagPersonopplysningGrunnlag(
@@ -73,8 +88,8 @@ class VilkårsvurderingServiceTest : OppslagSpringRunnerTest() {
 
     @Test
     fun `opprettVilkårsvurdering - skal opprette tom vilkårsvurdering og deaktivere eksisterende dersom det ikke finnes tidligere vedtatte behandlinger på fagsak`() {
-        val barn1 = lagreAktør(randomAktør())
-        val barn2 = lagreAktør(randomAktør())
+        val barn1 = randomAktør()
+        val barn2 = randomAktør()
         val lagretDeaktivertVilkårsvurderingSlot = slot<Vilkårsvurdering>()
         val lagretVilkårsvurderingSlot = slot<Vilkårsvurdering>()
         every { vilkårsvurderingRepository.finnAktiv(any()) } returns lagVilkårsvurdering(
@@ -97,5 +112,30 @@ class VilkårsvurderingServiceTest : OppslagSpringRunnerTest() {
 
         assertEquals(3, lagretVilkårsvurdering.personResultater.size)
         assertFalse(lagretDeaktivertVilkårsvurdering.aktiv)
+    }
+
+    @Test
+    fun `hentVilkårsbegrunnelser - skal returnere et map med begrunnelsestyper mappet mot liste av begrunnelser`() {
+        every { sanityService.hentSanityBegrunnelser() } returns listOf(
+            SanityBegrunnelse(
+                Standardbegrunnelse.DUMMY.sanityApiNavn,
+                "navnISystem",
+                Vilkår.values().toList()
+            )
+        )
+
+        every { sanityService.hentSanityEØSBegrunnelser() } returns listOf(
+            SanityEØSBegrunnelse(
+                EØSStandardbegrunnelse.DUMMY.sanityApiNavn,
+                "navnISystem"
+            )
+        )
+
+        val vilkårsbegrunnelser = vilkårsvurderingService.hentVilkårsbegrunnelser()
+
+        // TODO: Endre denne testen når vi får lagt inn riktige Standardbegrunnelser og EØSStandardbegrunnelser
+        assertEquals(2, vilkårsbegrunnelser.size)
+        assertEquals(Vilkår.values().size, vilkårsbegrunnelser[VedtakBegrunnelseType.AVSLAG]?.size)
+        assertEquals(1, vilkårsbegrunnelser[VedtakBegrunnelseType.EØS_OPPHØR]?.size)
     }
 }
