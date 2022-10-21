0a0e936f2183f81c977470ed4cb916d33250bc8f
bragejahren <70642183+bragejahren@users.noreply.github.com>
2022-10-21 14:15:28 +0200

    Lagt til sendBrev endepunkt og tilhørende logikk minus task for distribusjon og settPåVent (#90)
    * Lagt til sendBrev endepunkt og tilhørende logikk uten task som skal distribuere brev
    
    * Fikset bygg og la inn nytt endepunkt i VilkårsvurderingController for endring av AnnenVurdering vilkår
    
    * Fikset feil tilknyttet import
    
    * Enda en utdatert import
    
    * Fikset BehandlingServiceTest
    
    * Fikset byggfeil
    
    * Lagt til LoggService-tester på nye metoder
    
    * Lagt til enhetstester for SettPåVentService
    
    Signed-off-by: bragejahren <brage.ekroll.jahren@nav.no>
    
    * Slettet SettPåVent- Controller, Service, Repository og Entitet, men lar Service-testene ligge igjen utkommentert
    
    * Fjernet alle referanser til SettPåVent
    
    * Ryddet bort utkommentert kode som ikke er relevant for PR
    
    * Fjernet endringer i OppgaveService knyttet til SettPåVent
    
    * Oppdatert kontrakter-felles versjon
    
    * Lagt inn tester for AnnenVurderingService
    
    * Utvidet VilkårsvurderingControllerTest med test av endreAnnenVurdering
    
    * Lagt til enhetstester for genererOgSendBrev
    
    * KTLINT
    
    * Justeringer etter tilbakemelding
    
    * KTLINT
    
    Signed-off-by: bragejahren <brage.ekroll.jahren@nav.no>

--- src/test/enhetstester/kotlin/no/nav/familie/ks/sak/kjerne/brev/BrevServiceTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/kjerne/brev/BrevServiceTest.kt
@@ -4,18 +4,28 @@ import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
 import io.mockk.slot
+import io.mockk.verify
+import no.nav.familie.kontrakter.felles.dokarkiv.v2.Førsteside
 import no.nav.familie.ks.sak.api.dto.ManueltBrevDto
 import no.nav.familie.ks.sak.common.exception.Feil
 import no.nav.familie.ks.sak.data.lagBehandling
 import no.nav.familie.ks.sak.data.lagFagsak
 import no.nav.familie.ks.sak.data.lagPerson
 import no.nav.familie.ks.sak.data.lagPersonopplysningGrunnlag
+import no.nav.familie.ks.sak.data.lagVilkårsvurderingMedSøkersVilkår
 import no.nav.familie.ks.sak.data.randomAktør
 import no.nav.familie.ks.sak.data.shouldNotBeNull
+import no.nav.familie.ks.sak.integrasjon.journalføring.UtgåendeJournalføringService
+import no.nav.familie.ks.sak.integrasjon.journalføring.domene.JournalføringRepository
 import no.nav.familie.ks.sak.kjerne.arbeidsfordeling.ArbeidsfordelingService
 import no.nav.familie.ks.sak.kjerne.arbeidsfordeling.domene.ArbeidsfordelingPåBehandling
+import no.nav.familie.ks.sak.kjerne.behandling.domene.BehandlingKategori
+import no.nav.familie.ks.sak.kjerne.behandling.domene.BehandlingRepository
 import no.nav.familie.ks.sak.kjerne.behandling.domene.BehandlingÅrsak
+import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.VilkårsvurderingService
+import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.domene.Resultat
 import no.nav.familie.ks.sak.kjerne.brev.domene.maler.BrevDto
 import no.nav.familie.ks.sak.kjerne.brev.domene.maler.Brevmal
 import no.nav.familie.ks.sak.kjerne.brev.domene.maler.InnhenteOpplysningerDataDto
@@ -24,6 +34,8 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
 
 @ExtendWith(MockKExtension::class)
 class BrevServiceTest {
@@ -35,6 +47,18 @@ class BrevServiceTest {
 
     @MockK
     private lateinit var arbeidsfordelingService: ArbeidsfordelingService
+
+    @MockK
+    private lateinit var utgåendeJournalføringService: UtgåendeJournalføringService
+
+    @MockK
+    private lateinit var vilkårsvurderingService: VilkårsvurderingService
+
+    @MockK
+    private lateinit var behandlingRepository: BehandlingRepository
+
+    @MockK
+    private lateinit var journalføringRepository: JournalføringRepository
 
     @InjectMockKs
     private lateinit var brevService: BrevService
@@ -110,5 +134,203 @@ class BrevServiceTest {
             "Det har skjedd en feil. Prøv igjen, og ta kontakt med brukerstøtte hvis problemet vedvarer.",
             feil.frontendFeilmelding
         )
+    }
+
+    @ParameterizedTest
+    @EnumSource(
+        value = Brevmal::class,
+        names = ["INNHENTE_OPPLYSNINGER", "INNHENTE_OPPLYSNINGER_ETTER_SØKNAD_I_SED", "VARSEL_OM_REVURDERING", "VARSEL_OM_VEDTAK_ETTER_SØKNAD_I_SED", "VARSEL_OM_REVURDERING_FRA_NASJONAL_TIL_EØS"],
+        mode = EnumSource.Mode.INCLUDE
+    )
+    fun `genererOgSendBrev - skal journalføre brev med forside for brevmaler som tilsier det`(brevmal: Brevmal) {
+        every { personopplysningGrunnlagService.hentSøker(behandlingId = behandling.id) } returns lagPerson(
+            lagPersonopplysningGrunnlag(behandlingId = behandling.id, søkerPersonIdent = søker.aktivFødselsnummer()),
+            søker
+        )
+        every { arbeidsfordelingService.hentArbeidsfordelingPåBehandling(behandlingId = behandling.id) } returns ArbeidsfordelingPåBehandling(
+            behandlingId = behandling.id,
+            behandlendeEnhetNavn = "Behandlende enhet",
+            behandlendeEnhetId = "1234"
+        )
+
+        every { behandlingRepository.hentBehandling(behandlingId = behandling.id) } returns behandling
+
+        every { brevKlient.genererBrev(any(), any()) } returns ByteArray(10)
+
+        val førstesideSlot = slot<Førsteside>()
+
+        every {
+            utgåendeJournalføringService.journalførDokument(
+                any(),
+                any(),
+                any(),
+                any(),
+                any(),
+                capture(førstesideSlot),
+                any()
+            )
+        } returns "0"
+
+        every { journalføringRepository.save(any()) } returns mockk()
+
+        every { vilkårsvurderingService.hentAktivVilkårsvurderingForBehandling(any()) } returns lagVilkårsvurderingMedSøkersVilkår(
+            søkerAktør = søker,
+            behandling = behandling,
+            resultat = Resultat.IKKE_VURDERT
+        )
+
+        brevService.genererOgSendBrev(
+            behandling.id,
+            ManueltBrevDto(
+                brevmal = brevmal,
+                mottakerIdent = søker.aktivFødselsnummer(),
+                barnasFødselsdager = emptyList()
+            )
+        )
+
+        førstesideSlot.captured.shouldNotBeNull()
+    }
+
+    @ParameterizedTest
+    @EnumSource(
+        value = Brevmal::class,
+        names = ["INFORMASJONSBREV_DELT_BOSTED", "HENLEGGE_TRUKKET_SØKNAD", "SVARTIDSBREV", "FORLENGET_SVARTIDSBREV", "INFORMASJONSBREV_KAN_SØKE", "INFORMASJONSBREV_KAN_SØKE_EØS"],
+        mode = EnumSource.Mode.INCLUDE
+    )
+    fun `genererOgSendBrev - skal journalføre brev uten forside for brevmaler som tilsier det`(brevmal: Brevmal) {
+        every { personopplysningGrunnlagService.hentSøker(behandlingId = behandling.id) } returns lagPerson(
+            lagPersonopplysningGrunnlag(behandlingId = behandling.id, søkerPersonIdent = søker.aktivFødselsnummer()),
+            søker
+        )
+        every { arbeidsfordelingService.hentArbeidsfordelingPåBehandling(behandlingId = behandling.id) } returns ArbeidsfordelingPåBehandling(
+            behandlingId = behandling.id,
+            behandlendeEnhetNavn = "Behandlende enhet",
+            behandlendeEnhetId = "1234"
+        )
+
+        every { behandlingRepository.hentBehandling(behandlingId = behandling.id) } returns behandling
+
+        every { brevKlient.genererBrev(any(), any()) } returns ByteArray(10)
+
+        every {
+            utgåendeJournalføringService.journalførDokument(
+                any(),
+                any(),
+                any(),
+                any(),
+                any(),
+                null,
+                any()
+            )
+        } returns "0"
+
+        every { journalføringRepository.save(any()) } returns mockk()
+
+        every { vilkårsvurderingService.hentAktivVilkårsvurderingForBehandling(any()) } returns lagVilkårsvurderingMedSøkersVilkår(
+            søkerAktør = søker,
+            behandling = behandling,
+            resultat = Resultat.IKKE_VURDERT
+        )
+
+        brevService.genererOgSendBrev(
+            behandling.id,
+            ManueltBrevDto(
+                brevmal = brevmal,
+                mottakerIdent = søker.aktivFødselsnummer(),
+                barnasFødselsdager = emptyList(),
+                behandlingKategori = BehandlingKategori.NASJONAL,
+                antallUkerSvarfrist = 5
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @EnumSource(
+        value = Brevmal::class,
+        names = ["VEDTAK_FØRSTEGANGSVEDTAK", "VEDTAK_ENDRING", "VEDTAK_OPPHØRT", "VEDTAK_OPPHØR_MED_ENDRING", "VEDTAK_AVSLAG", "VEDTAK_FORTSATT_INNVILGET", "VEDTAK_KORREKSJON_VEDTAKSBREV", "VEDTAK_OPPHØR_DØDSFALL", "AUTOVEDTAK_BARN_6_OG_18_ÅR_OG_SMÅBARNSTILLEGG", "AUTOVEDTAK_NYFØDT_FØRSTE_BARN", "AUTOVEDTAK_NYFØDT_BARN_FRA_FØR"],
+        mode = EnumSource.Mode.INCLUDE
+    )
+    fun `genererOgSendBrev - skal ikke journalføre brev for brevmaler som ikke kan sendes manuelt`(brevmal: Brevmal) {
+        every { personopplysningGrunnlagService.hentSøker(behandlingId = behandling.id) } returns lagPerson(
+            lagPersonopplysningGrunnlag(behandlingId = behandling.id, søkerPersonIdent = søker.aktivFødselsnummer()),
+            søker
+        )
+        every { arbeidsfordelingService.hentArbeidsfordelingPåBehandling(behandlingId = behandling.id) } returns ArbeidsfordelingPåBehandling(
+            behandlingId = behandling.id,
+            behandlendeEnhetNavn = "Behandlende enhet",
+            behandlendeEnhetId = "1234"
+        )
+
+        every { behandlingRepository.hentBehandling(behandlingId = behandling.id) } returns behandling
+
+        every { brevKlient.genererBrev(any(), any()) } returns ByteArray(10)
+
+        val feil = assertThrows<Feil> {
+            brevService.genererOgSendBrev(
+                behandling.id,
+                ManueltBrevDto(
+                    brevmal = brevmal,
+                    mottakerIdent = søker.aktivFødselsnummer()
+                )
+            )
+        }
+        assertEquals("Kan ikke mappe fra manuel brevrequest til $brevmal.", feil.message)
+    }
+
+    @ParameterizedTest
+    @EnumSource(
+        value = Brevmal::class,
+        names = ["INNHENTE_OPPLYSNINGER", "VARSEL_OM_REVURDERING"],
+        mode = EnumSource.Mode.INCLUDE
+    )
+    fun `genererOgSendBrev - skal journalføre brev og legge til AnnenVurdering for søker i vilkårsvurderingen for INNHENTE_OPPLYSNINGER og VARSEL_OM_REVURDERING`(
+        brevmal: Brevmal
+    ) {
+        every { personopplysningGrunnlagService.hentSøker(behandlingId = behandling.id) } returns lagPerson(
+            lagPersonopplysningGrunnlag(behandlingId = behandling.id, søkerPersonIdent = søker.aktivFødselsnummer()),
+            søker
+        )
+        every { arbeidsfordelingService.hentArbeidsfordelingPåBehandling(behandlingId = behandling.id) } returns ArbeidsfordelingPåBehandling(
+            behandlingId = behandling.id,
+            behandlendeEnhetNavn = "Behandlende enhet",
+            behandlendeEnhetId = "1234"
+        )
+
+        every { behandlingRepository.hentBehandling(behandlingId = behandling.id) } returns behandling
+
+        every { brevKlient.genererBrev(any(), any()) } returns ByteArray(10)
+
+        val førstesideSlot = slot<Førsteside>()
+
+        every {
+            utgåendeJournalføringService.journalførDokument(
+                any(),
+                any(),
+                any(),
+                any(),
+                any(),
+                any(),
+                any()
+            )
+        } returns "0"
+
+        every { journalføringRepository.save(any()) } returns mockk()
+
+        every { vilkårsvurderingService.hentAktivVilkårsvurderingForBehandling(any()) } returns lagVilkårsvurderingMedSøkersVilkår(
+            søkerAktør = søker,
+            behandling = behandling,
+            resultat = Resultat.IKKE_VURDERT
+        )
+
+        brevService.genererOgSendBrev(
+            behandling.id,
+            ManueltBrevDto(
+                brevmal = brevmal,
+                mottakerIdent = søker.aktivFødselsnummer(),
+                barnasFødselsdager = emptyList()
+            )
+        )
+
+        verify(exactly = 1) { vilkårsvurderingService.hentAktivVilkårsvurderingForBehandling(any()) }
     }
 }
