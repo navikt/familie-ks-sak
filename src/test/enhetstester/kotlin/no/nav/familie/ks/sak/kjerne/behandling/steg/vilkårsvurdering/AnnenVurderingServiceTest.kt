fbfd6e71767b11fa567418fd8cc33f66e80c411d
bragejahren <brage.ekroll.jahren@nav.no>
2022-10-21 12:42:53 +0200

    KTLINT

--- src/test/enhetstester/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vilkårsvurdering/AnnenVurderingServiceTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vilkårsvurdering/AnnenVurderingServiceTest.kt
@@ -21,7 +21,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
-import java.util.*
+import java.util.Optional
 
 @ExtendWith(MockKExtension::class)
 class AnnenVurderingServiceTest {
