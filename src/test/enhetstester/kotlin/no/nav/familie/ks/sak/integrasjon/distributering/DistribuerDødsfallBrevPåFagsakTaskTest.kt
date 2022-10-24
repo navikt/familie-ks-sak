bc9cf4a72adbb1ec02604a4870149bc8b5d46b15
Uy Quang Nguyen <uy.quang.nguyen@nav.no>
2022-10-24 19:17:27 +0200

    Ktlint

--- src/test/enhetstester/kotlin/no/nav/familie/ks/sak/integrasjon/distributering/DistribuerDødsfallBrevPåFagsakTaskTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/integrasjon/distributering/DistribuerDødsfallBrevPåFagsakTaskTest.kt
@@ -1,6 +1,5 @@
 package no.nav.familie.ks.sak.integrasjon.distributering
 
-import io.mockk.called
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
@@ -9,24 +8,11 @@ import io.mockk.just
 import io.mockk.mockk
 import io.mockk.runs
 import io.mockk.verify
-import no.nav.familie.ks.sak.common.exception.Feil
-import no.nav.familie.ks.sak.common.exception.FunksjonellFeil
-import no.nav.familie.ks.sak.data.lagBehandling
-import no.nav.familie.ks.sak.kjerne.behandling.domene.BehandlingType
-import no.nav.familie.ks.sak.kjerne.behandling.domene.Behandlingsresultat
-import no.nav.familie.ks.sak.kjerne.behandling.domene.BehandlingÅrsak
 import no.nav.familie.ks.sak.kjerne.brev.BrevService
-import no.nav.familie.ks.sak.kjerne.brev.domene.maler.Brevmal
-import no.nav.familie.ks.sak.kjerne.brev.hentBrevmal
-import no.nav.familie.ks.sak.kjerne.brev.hentVedtaksbrevmal
-import no.nav.familie.ks.sak.kjerne.brev.hentVedtaksbrevtype
 import no.nav.familie.prosessering.domene.Task
-import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import java.time.LocalDateTime
-import org.hamcrest.CoreMatchers.`is` as Is
 
 @ExtendWith(MockKExtension::class)
 class DistribuerDødsfallBrevPåFagsakTaskTest {
