189242beb1f2b861279ee564cc185ba9cae77a63
haldersaikat <saikat.halder@nav.no>
2022-10-26 16:15:34 +0200

    skrev enhetstester for validerYtelsePersoner

---
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/behandlingsresultat/YtelsePersonUtilsTest.kt
@@ -0,0 +1,84 @@
+package no.nav.familie.ks.sak.kjerne.behandling.steg.behandlingsresultat
+
+import no.nav.familie.ks.sak.common.exception.Feil
+import no.nav.familie.ks.sak.data.randomAktør
+import no.nav.familie.ks.sak.kjerne.beregning.domene.YtelseType
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import java.time.YearMonth
+
+internal class YtelsePersonUtilsTest {
+
+    @Test
+    fun `validerYtelsePersoner skal kaste feil hvis resultaten til en av ytelse personene ikke er vurdert`() {
+        val exception = assertThrows<Feil> {
+            YtelsePersonUtils.validerYtelsePersoner(
+                listOf(
+                    lagYtelsePerson(setOf(YtelsePersonResultat.INNVILGET)),
+                    lagYtelsePerson(setOf(YtelsePersonResultat.IKKE_VURDERT))
+                )
+            )
+        }
+        assertEquals("Minst én ytelseperson er ikke vurdert", exception.message)
+    }
+
+    @Test
+    fun `validerYtelsePersoner skal kaste feil hvis en av ytelse personene ikke får ytelse slutt`() {
+        val exception = assertThrows<Feil> {
+            YtelsePersonUtils.validerYtelsePersoner(
+                listOf(
+                    lagYtelsePerson(setOf(YtelsePersonResultat.INNVILGET)),
+                    lagYtelsePerson(resultater = setOf(YtelsePersonResultat.INNVILGET), ytelseSlutt = null)
+                )
+            )
+        }
+        assertEquals("YtelseSlutt er ikke satt ved utledning av behandlingsresultat", exception.message)
+    }
+
+    @Test
+    fun `validerYtelsePersoner skal kaste feil når ytelse slutter etter inneværende måned ved OPPHØR`() {
+        val exception = assertThrows<Feil> {
+            YtelsePersonUtils.validerYtelsePersoner(
+                listOf(
+                    lagYtelsePerson(setOf(YtelsePersonResultat.INNVILGET)),
+                    lagYtelsePerson(
+                        resultater = setOf(YtelsePersonResultat.OPPHØRT),
+                        ytelseSlutt = YearMonth.now().plusMonths(4)
+                    )
+                )
+            )
+        }
+        assertEquals(
+            "Minst én ytelseperson har fått opphør som resultat og ytelseSlutt etter inneværende måned",
+            exception.message
+        )
+    }
+
+    @Test
+    fun `validerYtelsePersoner skal ikke kaste feil ved INNVILGET og OPPHØRT`() {
+        assertDoesNotThrow {
+            YtelsePersonUtils.validerYtelsePersoner(
+                listOf(
+                    lagYtelsePerson(setOf(YtelsePersonResultat.INNVILGET)),
+                    lagYtelsePerson(
+                        resultater = setOf(YtelsePersonResultat.OPPHØRT),
+                        ytelseSlutt = YearMonth.now()
+                    )
+                )
+            )
+        }
+    }
+
+    private fun lagYtelsePerson(
+        resultater: Set<YtelsePersonResultat>,
+        ytelseSlutt: YearMonth? = YearMonth.now().plusMonths(3)
+    ) = YtelsePerson(
+        aktør = randomAktør(),
+        ytelseType = YtelseType.ORDINÆR_KONTANTSTØTTE,
+        kravOpprinnelse = listOf(KravOpprinnelse.INNEVÆRENDE),
+        resultater = resultater,
+        ytelseSlutt = ytelseSlutt
+    )
+}
