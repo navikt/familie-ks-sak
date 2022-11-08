4bfc5655885e7150b9cf132897c78e3f72988c14
Uy Quang Nguyen <uy.quang.nguyen@nav.no>
2022-11-08 16:14:27 +0100

    More tests

--- src/test/enhetstester/kotlin/no/nav/familie/ks/sak/integrasjon/økonomi/utbetalingsoppdrag/UtbetalingsoppdragGeneratorTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/integrasjon/økonomi/utbetalingsoppdrag/UtbetalingsoppdragGeneratorTest.kt
@@ -318,6 +318,275 @@ internal class UtbetalingsoppdragGeneratorTest {
 
     }
 
+    @Test
+    fun `lagTilkjentYtelseMedUtbetalingsoppdrag skal opprette revurdering med nytt barn`() {
+        val tidNå = LocalDate.now()
+
+        val aktør = fnrTilAktør(randomFnr())
+        val fagsak = lagFagsak(aktør)
+
+        val førsteBehandling = lagBehandling(opprettetÅrsak = BehandlingÅrsak.SØKNAD, fagsak = fagsak).also {
+            it.behandlingStegTilstand.forEach { it.behandlingStegStatus = BehandlingStegStatus.UTFØRT }
+        }
+
+        val vedtak = Vedtak(behandling = førsteBehandling)
+
+        val tilkjentYtelseIFørsteBehandling =
+            TilkjentYtelse(behandling = førsteBehandling, opprettetDato = tidNå, endretDato = tidNå)
+
+        val andelTilkjentYtelserIFørsteBehandling =
+            listOf(
+                lagAndelTilkjentYtelse(
+                    tilkjentYtelse = tilkjentYtelseIFørsteBehandling,
+                    behandling = førsteBehandling,
+                    aktør = aktør,
+                    stønadFom = årMåned("2020-01"),
+                    stønadTom = årMåned("2029-12"),
+                    periodeOffset = 0
+                ),
+                lagAndelTilkjentYtelse(
+                    tilkjentYtelse = tilkjentYtelseIFørsteBehandling,
+                    behandling = førsteBehandling,
+                    aktør = aktør,
+                    stønadFom = årMåned("2033-01"),
+                    stønadTom = årMåned("2034-12"),
+                    periodeOffset = 1
+                )
+            )
+
+        tilkjentYtelseIFørsteBehandling.andelerTilkjentYtelse.addAll(andelTilkjentYtelserIFørsteBehandling)
+        tilkjentYtelseIFørsteBehandling.utbetalingsoppdrag = "Oppdrag"
+
+        val oppdatertTilkjentYtelseIFørsteBehandling =
+            utbetalingsoppdragGenerator.lagTilkjentYtelseMedUtbetalingsoppdrag(
+                lagVedtakMedTilkjentYtelse(
+                    vedtak = vedtak,
+                    tilkjentYtelse = tilkjentYtelseIFørsteBehandling
+                ),
+                AndelTilkjentYtelseForIverksettingFactory()
+            )
+
+
+        val andreBehandling = lagBehandling(opprettetÅrsak = BehandlingÅrsak.SØKNAD, fagsak = fagsak)
+        val andreVedtak = Vedtak(behandling = andreBehandling)
+        val nyAktør = fnrTilAktør(randomFnr())
+
+        val tilkjentYtelseIAndreBehandling =
+            TilkjentYtelse(behandling = andreBehandling, opprettetDato = tidNå, endretDato = tidNå)
+
+        val andelTilkjentYtelserIAndreBehandling =
+            listOf(
+                lagAndelTilkjentYtelse(
+                    tilkjentYtelse = tilkjentYtelseIAndreBehandling,
+                    behandling = andreBehandling,
+                    aktør = nyAktør,
+                    stønadFom = årMåned("2022-01"),
+                    stønadTom = årMåned("2034-12"),
+                    periodeOffset = 2
+                ),
+                lagAndelTilkjentYtelse(
+                    tilkjentYtelse = tilkjentYtelseIAndreBehandling,
+                    behandling = andreBehandling,
+                    aktør = nyAktør,
+                    stønadFom = årMåned("2037-01"),
+                    stønadTom = årMåned("2039-12"),
+                    periodeOffset = 3
+                )
+            )
+
+        tilkjentYtelseIAndreBehandling.andelerTilkjentYtelse.addAll(andelTilkjentYtelserIAndreBehandling)
+        val sisteOffsetPåFagsak = 1
+
+        val oppdatertTilkjentYtelseIAndreBehandling =
+            utbetalingsoppdragGenerator.lagTilkjentYtelseMedUtbetalingsoppdrag(
+                lagVedtakMedTilkjentYtelse(
+                    vedtak = andreVedtak,
+                    sisteOffsetPerIdent = ØkonomiUtils.gjeldendeForrigeOffsetForKjede(
+                        ØkonomiUtils.kjedeinndelteAndeler(
+                            andelTilkjentYtelserIFørsteBehandling.forIverksetting()
+                        )
+                    ),
+                    sisteOffsetPåFagsak = sisteOffsetPåFagsak,
+                    tilkjentYtelse = tilkjentYtelseIAndreBehandling
+                ),
+                AndelTilkjentYtelseForIverksettingFactory(),
+                forrigeTilkjentYtelse = oppdatertTilkjentYtelseIFørsteBehandling
+            )
+
+
+        val utbetalingsoppdrag =
+            konvertTilUtbetalingsoppdrag(oppdatertTilkjentYtelseIAndreBehandling.utbetalingsoppdrag)
+
+        assertThat(utbetalingsoppdrag.utbetalingsperiode.size, Is(3))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode.all { it.sats == BigDecimal(7500) }, Is(true))
+        assertThat(
+            utbetalingsoppdrag.utbetalingsperiode.all { it.utbetalesTil == førsteBehandling.fagsak.aktør.aktivFødselsnummer() },
+            Is(true)
+        )
+        assertThat(utbetalingsoppdrag.kodeEndring, Is(Utbetalingsoppdrag.KodeEndring.ENDR))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode.count { it.opphør != null }, Is(1))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode.count { it.opphør == null }, Is(2))
+
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[0].vedtakdatoFom, Is(dato("2033-01-01")))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[0].vedtakdatoTom, Is(dato("2034-12-31")))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[0].periodeId, Is(1))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[0].forrigePeriodeId, Is(0))
+
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[1].vedtakdatoFom, Is(dato("2022-01-01")))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[1].vedtakdatoTom, Is(dato("2034-12-31")))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[1].periodeId, Is(2))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[1].forrigePeriodeId, Is(nullValue()))
+
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[2].vedtakdatoFom, Is(dato("2037-01-01")))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[2].vedtakdatoTom, Is(dato("2039-12-31")))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[2].periodeId, Is(3))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[2].forrigePeriodeId, Is(2))
+    }
+
+    @Test
+    fun `lagTilkjentYtelseMedUtbetalingsoppdrag skal generere komplett utbetalingsoppdrag selvom ingen endringer har blitt gjort`() {
+        val tidNå = LocalDate.now()
+
+        val aktør = fnrTilAktør(randomFnr())
+        val fagsak = lagFagsak(aktør)
+
+        val førsteBehandling = lagBehandling(opprettetÅrsak = BehandlingÅrsak.SØKNAD, fagsak = fagsak).also {
+            it.behandlingStegTilstand.forEach { it.behandlingStegStatus = BehandlingStegStatus.UTFØRT }
+        }
+
+        val vedtak = Vedtak(behandling = førsteBehandling)
+
+        val tilkjentYtelseIFørsteBehandling =
+            TilkjentYtelse(behandling = førsteBehandling, opprettetDato = tidNå, endretDato = tidNå)
+
+        val andelTilkjentYtelserIFørsteBehandling =
+            listOf(
+                lagAndelTilkjentYtelse(
+                    tilkjentYtelse = tilkjentYtelseIFørsteBehandling,
+                    behandling = førsteBehandling,
+                    aktør = aktør,
+                    stønadFom = årMåned("2020-01"),
+                    stønadTom = årMåned("2029-12"),
+                    periodeOffset = 0
+                ),
+                lagAndelTilkjentYtelse(
+                    tilkjentYtelse = tilkjentYtelseIFørsteBehandling,
+                    behandling = førsteBehandling,
+                    aktør = aktør,
+                    stønadFom = årMåned("2030-01"),
+                    stønadTom = årMåned("2034-12"),
+                    periodeOffset = 1
+                ),
+                lagAndelTilkjentYtelse(
+                    tilkjentYtelse = tilkjentYtelseIFørsteBehandling,
+                    behandling = førsteBehandling,
+                    aktør = aktør,
+                    stønadFom = årMåned("2035-01"),
+                    stønadTom = årMåned("2039-12"),
+                    periodeOffset = 2
+                )
+            )
+
+        tilkjentYtelseIFørsteBehandling.andelerTilkjentYtelse.addAll(andelTilkjentYtelserIFørsteBehandling)
+
+        val oppdatertTilkjentYtelseIFørsteBehandling =
+            utbetalingsoppdragGenerator.lagTilkjentYtelseMedUtbetalingsoppdrag(
+                lagVedtakMedTilkjentYtelse(
+                    vedtak = vedtak,
+                    tilkjentYtelse = tilkjentYtelseIFørsteBehandling
+                ),
+                AndelTilkjentYtelseForIverksettingFactory()
+            )
+
+
+        val andreBehandling = lagBehandling(opprettetÅrsak = BehandlingÅrsak.SØKNAD, fagsak = fagsak)
+        val andreVedtak = Vedtak(behandling = andreBehandling)
+
+        val tilkjentYtelseIAndreBehandling =
+            TilkjentYtelse(behandling = andreBehandling, opprettetDato = tidNå, endretDato = tidNå)
+
+        val andelTilkjentYtelserIAndreBehandling =
+            listOf(
+                lagAndelTilkjentYtelse(
+                    tilkjentYtelse = tilkjentYtelseIAndreBehandling,
+                    behandling = førsteBehandling,
+                    aktør = aktør,
+                    stønadFom = årMåned("2020-01"),
+                    stønadTom = årMåned("2029-12"),
+                    periodeOffset = 0
+                ),
+                lagAndelTilkjentYtelse(
+                    tilkjentYtelse = tilkjentYtelseIAndreBehandling,
+                    behandling = førsteBehandling,
+                    aktør = aktør,
+                    stønadFom = årMåned("2030-01"),
+                    stønadTom = årMåned("2034-12"),
+                    periodeOffset = 3
+                ),
+                lagAndelTilkjentYtelse(
+                    tilkjentYtelse = tilkjentYtelseIAndreBehandling,
+                    behandling = førsteBehandling,
+                    aktør = aktør,
+                    stønadFom = årMåned("2035-01"),
+                    stønadTom = årMåned("2039-12"),
+                    periodeOffset = 4
+                )
+            )
+
+        tilkjentYtelseIAndreBehandling.andelerTilkjentYtelse.addAll(andelTilkjentYtelserIAndreBehandling)
+        val sisteOffsetPåFagsak = 2
+
+        val oppdatertTilkjentYtelseIAndreBehandling =
+            utbetalingsoppdragGenerator.lagTilkjentYtelseMedUtbetalingsoppdrag(
+                lagVedtakMedTilkjentYtelse(
+                    vedtak = andreVedtak,
+                    sisteOffsetPerIdent = ØkonomiUtils.gjeldendeForrigeOffsetForKjede(
+                        ØkonomiUtils.kjedeinndelteAndeler(
+                            andelTilkjentYtelserIFørsteBehandling.forIverksetting()
+                        )
+                    ),
+                    sisteOffsetPåFagsak = sisteOffsetPåFagsak,
+                    tilkjentYtelse = tilkjentYtelseIAndreBehandling,
+                    erSimulering = true
+                ),
+                AndelTilkjentYtelseForIverksettingFactory(),
+                forrigeTilkjentYtelse = oppdatertTilkjentYtelseIFørsteBehandling
+            )
+
+        val utbetalingsoppdrag =
+            konvertTilUtbetalingsoppdrag(oppdatertTilkjentYtelseIAndreBehandling.utbetalingsoppdrag)
+
+        assertThat(utbetalingsoppdrag.utbetalingsperiode.size, Is(4))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode.all { it.sats == BigDecimal(7500) }, Is(true))
+        assertThat(
+            utbetalingsoppdrag.utbetalingsperiode.all { it.utbetalesTil == førsteBehandling.fagsak.aktør.aktivFødselsnummer() },
+            Is(true)
+        )
+        assertThat(utbetalingsoppdrag.kodeEndring, Is(Utbetalingsoppdrag.KodeEndring.ENDR))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode.count { it.opphør != null }, Is(1))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode.count { it.opphør == null }, Is(3))
+
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[0].vedtakdatoFom, Is(dato("2035-01-01")))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[0].vedtakdatoTom, Is(dato("2039-12-31")))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[0].periodeId, Is(2))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[0].forrigePeriodeId, Is(1))
+
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[1].vedtakdatoFom, Is(dato("2020-01-01")))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[1].vedtakdatoTom, Is(dato("2029-12-31")))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[1].periodeId, Is(3))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[1].forrigePeriodeId, Is(2))
+
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[2].vedtakdatoFom, Is(dato("2030-01-01")))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[2].vedtakdatoTom, Is(dato("2034-12-31")))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[2].periodeId, Is(4))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[2].forrigePeriodeId, Is(3))
+
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[3].vedtakdatoFom, Is(dato("2035-01-01")))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[3].vedtakdatoTom, Is(dato("2039-12-31")))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[3].periodeId, Is(5))
+        assertThat(utbetalingsoppdrag.utbetalingsperiode[3].forrigePeriodeId, Is(4))
+    }
+
     private fun lagVedtakMedTilkjentYtelse(
         vedtak: Vedtak,
         tilkjentYtelse: TilkjentYtelse,
