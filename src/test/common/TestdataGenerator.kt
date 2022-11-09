c5a1af1f2f3d288dc8318621c51f282351284faf
bragejahren <brage.ekroll.jahren@nav.no>
2022-11-09 14:38:56 +0100

    Fikset ktlint feil

--- src/test/common/TestdataGenerator.kt
+++ src/test/common/TestdataGenerator.kt
@@ -64,7 +64,7 @@ import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.YearMonth
-import java.util.*
+import java.util.UUID
 import kotlin.math.abs
 import kotlin.random.Random
 
