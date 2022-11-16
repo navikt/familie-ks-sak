4e9e3d01902e0396cac892dd537d3b338ffa8ac9
halvormundal <halvorbmundal@gmail.com>
2022-11-16 12:26:21 +0100

    Endre rekkefølge for VedtakBegrunnelseType

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vedtak/VedtakBegrunnelseType.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vedtak/VedtakBegrunnelseType.kt
@@ -1,13 +1,13 @@
 package no.nav.familie.ks.sak.kjerne.behandling.steg.vedtak
 
 enum class VedtakBegrunnelseType(val sorteringsrekkefølge: Int) {
+    REDUKSJON(1),
     INNVILGET(2),
     EØS_INNVILGET(2),
-    REDUKSJON(1),
     AVSLAG(3),
     OPPHØR(4),
     EØS_OPPHØR(4),
     FORTSATT_INNVILGET(5),
+    ETTER_ENDRET_UTBETALING(6),
     ENDRET_UTBETALING(7),
-    ETTER_ENDRET_UTBETALING(6)
 }
