fbb72f929d891efe564286c2be5d34809f55c8c7
Uy Nguyen <110383605+UyQuangNguyen@users.noreply.github.com>
2022-11-23 13:25:31 +0100

    Opprett stønadstatistikk service (#171)
    * Opprett stønadstatistikk service
    
    * Kode refaktorering
    
    * Kode refaktorering
    
    * Bytt ut stønadsstatistikk med stønadsstatistikk-ks

--- src/main/kotlin/no/nav/familie/ks/sak/integrasjon/pdl/PdlClient.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/integrasjon/pdl/PdlClient.kt
@@ -2,10 +2,13 @@ package no.nav.familie.ks.sak.integrasjon.pdl
 
 import no.nav.familie.http.client.AbstractPingableRestClient
 import no.nav.familie.kontrakter.felles.personopplysning.Adressebeskyttelse
+import no.nav.familie.kontrakter.felles.personopplysning.Statsborgerskap
 import no.nav.familie.ks.sak.common.exception.PdlNotFoundException
 import no.nav.familie.ks.sak.config.PdlConfig
 import no.nav.familie.ks.sak.config.PdlConfig.Companion.hentAdressebeskyttelseQuery
+import no.nav.familie.ks.sak.config.PdlConfig.Companion.hentBostedsadresseUtenlandskQuery
 import no.nav.familie.ks.sak.config.PdlConfig.Companion.hentIdenterQuery
+import no.nav.familie.ks.sak.config.PdlConfig.Companion.hentStatsborgerskapUtenHistorikkQuery
 import no.nav.familie.ks.sak.config.PdlConfig.Companion.httpHeaders
 import no.nav.familie.ks.sak.config.PersonInfoQuery
 import no.nav.familie.ks.sak.integrasjon.kallEksternTjeneste
@@ -17,6 +20,9 @@ import no.nav.familie.ks.sak.integrasjon.pdl.domene.PdlIdent
 import no.nav.familie.ks.sak.integrasjon.pdl.domene.PdlPersonData
 import no.nav.familie.ks.sak.integrasjon.pdl.domene.PdlPersonRequest
 import no.nav.familie.ks.sak.integrasjon.pdl.domene.PdlPersonRequestVariables
+import no.nav.familie.ks.sak.integrasjon.pdl.domene.PdlStatsborgerskapResponse
+import no.nav.familie.ks.sak.integrasjon.pdl.domene.PdlUtenlandskAdresssePersonUtenlandskAdresse
+import no.nav.familie.ks.sak.integrasjon.pdl.domene.PdlUtenlandskAdressseResponse
 import no.nav.familie.ks.sak.kjerne.personident.Aktør
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.cache.annotation.Cacheable
@@ -89,6 +95,48 @@ class PdlClient(
             pdlPerson.person?.validerOmPersonKanBehandlesIFagsystem() ?: throw PdlNotFoundException()
             pdlPerson.person
         }
+    }
+
+    fun hentStatsborgerskapUtenHistorikk(aktør: Aktør): List<Statsborgerskap> {
+        val pdlPersonRequest = PdlPersonRequest(
+            variables = PdlPersonRequestVariables(aktør.aktivFødselsnummer()),
+            query = hentStatsborgerskapUtenHistorikkQuery
+        )
+
+        val pdlResponse: PdlBaseRespons<PdlStatsborgerskapResponse> = kallEksternTjeneste(
+            tjeneste = "pdl",
+            uri = pdlUri,
+            formål = "Hent statsborgerskap uten historikk"
+        ) { postForEntity(pdlUri, pdlPersonRequest, httpHeaders()) }
+
+        return feilsjekkOgReturnerData(
+            ident = aktør.aktivFødselsnummer(),
+            pdlRespons = pdlResponse
+        ) {
+            it.person!!.statsborgerskap
+        }
+    }
+
+    fun hentUtenlandskBostedsadresse(aktør: Aktør): PdlUtenlandskAdresssePersonUtenlandskAdresse? {
+        val pdlPersonRequest = PdlPersonRequest(
+            variables = PdlPersonRequestVariables(aktør.aktivFødselsnummer()),
+            query = hentBostedsadresseUtenlandskQuery
+        )
+        val pdlResponse: PdlBaseRespons<PdlUtenlandskAdressseResponse> = kallEksternTjeneste(
+            tjeneste = "pdl",
+            uri = pdlUri,
+            formål = "Hent utenlandsk bostedsadresse"
+        ) {
+            postForEntity(pdlUri, pdlPersonRequest, httpHeaders())
+        }
+
+        val bostedsadresser = feilsjekkOgReturnerData(
+            ident = aktør.aktivFødselsnummer(),
+            pdlRespons = pdlResponse
+        ) {
+            it.person!!.bostedsadresse
+        }
+        return bostedsadresser.firstOrNull { bostedsadresse -> bostedsadresse.utenlandskAdresse != null }?.utenlandskAdresse
     }
 
     private fun lagPdlPersonRequest(aktivFødselsnummer: String, query: String) = PdlPersonRequest(
