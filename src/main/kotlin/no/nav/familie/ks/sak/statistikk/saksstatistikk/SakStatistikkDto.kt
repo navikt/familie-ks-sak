3b529080f22c5cda34c33c460bd355e55bc55662
halvormundal <halvorbmundal@gmail.com>
2022-12-01 08:37:12 +0100

    Legg til SakStatistikkDto

---
+++ src/main/kotlin/no/nav/familie/ks/sak/statistikk/saksstatistikk/SakStatistikkDto.kt
@@ -0,0 +1,19 @@
+package no.nav.familie.ks.sak.statistikk.saksstatistikk
+
+import no.nav.familie.eksterne.kontrakter.saksstatistikk.AktørDVH
+import java.time.LocalDate
+import java.time.ZonedDateTime
+
+data class SakStatistikkDto(
+    val funksjonellTid: ZonedDateTime,
+    val tekniskTid: ZonedDateTime,
+    val opprettetDato: LocalDate,
+    val funksjonellId: String,
+    val sakId: String,
+    val aktorId: Long,
+    val bostedsland: String,
+    val aktorer: List<AktørDVH>? = emptyList(),
+    val sakStatus: String,
+    val avsender: String,
+    val ytelseType: String = "BARNETRYGD"
+)
