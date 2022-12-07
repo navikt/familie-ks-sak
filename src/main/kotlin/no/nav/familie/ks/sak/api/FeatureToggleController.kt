0a0b99a9d589808d6d6ab67720dedf936ab40ce7
haldersaikat <saikat.halder@nav.no>
2022-12-07 09:50:47 +0100

    endret api path til /api/featuretoggles

--- src/main/kotlin/no/nav/familie/ks/sak/api/FeatureToggleController.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/api/FeatureToggleController.kt
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/feature")
+@RequestMapping("/api/featuretoggles")
 @ProtectedWithClaims(issuer = "azuread")
 @Validated
 class FeatureToggleController(private val featureToggleService: FeatureToggleService) {
