bba6d0f2a371a38bde9964d50f3b5767b6a33bbc
haldersaikat <saikat.halder@nav.no>
2022-12-21 09:33:48 +0100

    Slipper BisysConfig filter på test

--- src/main/kotlin/no/nav/familie/ks/sak/config/BisysConfig.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/config/BisysConfig.kt
@@ -5,12 +5,14 @@ import no.nav.familie.sikkerhet.OIDCUtil
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.web.filter.OncePerRequestFilter
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @Configuration
+@Profile("!integrasjonstest")
 class BisysConfig(
     private val oidcUtil: OIDCUtil,
     private val rolleConfig: RolleConfig,
