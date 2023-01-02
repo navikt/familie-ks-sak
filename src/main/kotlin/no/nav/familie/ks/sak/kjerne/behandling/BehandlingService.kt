c57590558630079bce8f9ad2803a0b846699f674
halvorbmundal <halvorbmundal@users.noreply.github.com>
2023-01-02 12:03:49 +0100

    Feature/klage eksterne kall (#242)
    * Oppdater fellesKontrakterVersion
    
    * Legg til mulighet for å hente vedtak for ekstern tjeneste
    
    * Legg til mulighet for å opprette revurdering fra klagesystemet
    
    * Rett opp skrivefeil
    
    * Rett opp skrivefeil

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/BehandlingService.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/BehandlingService.kt
@@ -54,6 +54,11 @@ class BehandlingService(
 
     fun hentBehandling(behandlingId: Long): Behandling = behandlingRepository.hentBehandling(behandlingId)
     fun hentAktivtBehandling(behandlingId: Long): Behandling = behandlingRepository.hentAktivBehandling(behandlingId)
+    fun finnAktivBehandlingPåFagsak(fagsakId: Long): Behandling? = behandlingRepository.findByFagsakAndAktiv(fagsakId)
+    fun erAktivBehandlingPåFagsak(fagsakId: Long): Boolean = finnAktivBehandlingPåFagsak(fagsakId) != null
+    fun finnÅpenBehandlingPåFagsak(fagsakId: Long): Behandling? = behandlingRepository.findByFagsakAndAktivAndOpen(fagsakId)
+    fun erÅpenBehandlingPåFagsak(fagsakId: Long): Boolean = finnÅpenBehandlingPåFagsak(fagsakId) != null
+    fun hentBehandlingerPåFagsak(fagsakId: Long): List<Behandling> = behandlingRepository.finnBehandlinger(fagsakId)
 
     fun hentSisteBehandlingSomErVedtatt(fagsakId: Long): Behandling? = behandlingRepository.finnBehandlinger(fagsakId)
         .filter { !it.erHenlagt() && it.status == BehandlingStatus.AVSLUTTET }
