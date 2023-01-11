36de01b504102964dc7b88058f124a6b30fcb535
Uy Quang Nguyen <uy.quang.nguyen@nav.no>
2023-01-11 23:17:35 +0100

    Legger til feilutbetalt valuta logikk

---
+++ src/main/kotlin/no/nav/familie/ks/sak/api/dto/FeilutbetaltValutaDto.kt
@@ -0,0 +1,19 @@
+package no.nav.familie.ks.sak.api.dto
+
+import no.nav.familie.ks.sak.kjerne.behandling.steg.vedtak.feilutbetaltvaluta.FeilutbetaltValuta
+import java.time.LocalDate
+
+data class FeilutbetaltValutaDto(
+    val id: Long?,
+    val fom: LocalDate,
+    val tom: LocalDate,
+    val feilutbetaltBeløp: Int
+)
+
+fun FeilutbetaltValuta.tilFeilutbetaltValutaDto(): FeilutbetaltValutaDto =
+    FeilutbetaltValutaDto(
+        id = id,
+        fom = fom,
+        tom = tom,
+        feilutbetaltBeløp = feilutbetaltBeløp
+    )
