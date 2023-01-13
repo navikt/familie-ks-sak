95bc0371bb85d241d76ba8cb878d23be70362326
Saikat Halder <45660476+haldersaikat@users.noreply.github.com>
2023-01-13 10:08:49 +0100

    Lister alle utdypende vilkårsvurderinger for EØS (#283)
    * Lister alle utdypende vilkårsvurderinger for EØS
    
    * fikset typo
    
    Co-authored-by: Uy Nguyen <110383605+UyQuangNguyen@users.noreply.github.com>
    
    Co-authored-by: Uy Nguyen <110383605+UyQuangNguyen@users.noreply.github.com>

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vilkårsvurdering/domene/UtdypendeVilkårsvurdering.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vilkårsvurdering/domene/UtdypendeVilkårsvurdering.kt
@@ -10,7 +10,20 @@ enum class UtdypendeVilkårsvurdering {
     DELT_BOSTED,
     DELT_BOSTED_SKAL_IKKE_DELES,
     ADOPSJON,
-    SOMMERFERIE
+    SOMMERFERIE,
+
+    // EØS
+    OMFATTET_AV_NORSK_LOVGIVNING, // Bosatt i riket SØKER vilkår
+    OMFATTET_AV_NORSK_LOVGIVNING_UTLAND, // Bosatt i riket SØKER vilkår
+    BARN_BOR_I_NORGE, // Bosatt i riket BARN vilkår
+    BARN_BOR_I_EØS, // Bosatt i riket BARN vilkår
+    BARN_BOR_I_STORBRITANNIA, // Bosatt i riket BARN vilkår
+    BARN_BOR_I_EØS_MED_SØKER, // Bor med søker vilkår
+    BARN_BOR_I_EØS_MED_ANNEN_FORELDER, // Bor med søker vilkår
+    BARN_BOR_I_NORGE_MED_SØKER, // Bor med søker vilkår
+    BARN_BOR_I_STORBRITANNIA_MED_SØKER, // Bor med søker vilkår
+    BARN_BOR_I_STORBRITANNIA_MED_ANNEN_FORELDER, // Bor med søker vilkår
+    BARN_BOR_ALENE_I_ANNET_EØS_LAND // Bor med søker vilkår
 }
 
 @Converter
