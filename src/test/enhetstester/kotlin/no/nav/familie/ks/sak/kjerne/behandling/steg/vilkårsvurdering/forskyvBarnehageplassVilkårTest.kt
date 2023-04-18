c3faebd37d81c9eeae931c81e0ae903c9f817da0
Uy Quang Nguyen <uy.quang.nguyen@nav.no>
2023-04-18 07:39:07 +0200

    Lagt til flere tester

--- src/test/enhetstester/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vilkårsvurdering/forskyvBarnehageplassVilkårTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vilkårsvurdering/forskyvBarnehageplassVilkårTest.kt
@@ -2,11 +2,14 @@ package no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering
 
 import no.nav.familie.ks.sak.data.lagVilkårResultat
 import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.domene.Vilkår
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.YearMonth
+import org.hamcrest.CoreMatchers.`is` as Is
 
 class forskyvBarnehageplassVilkårTest {
 
@@ -14,6 +17,7 @@ class forskyvBarnehageplassVilkårTest {
     private val februar = YearMonth.of(2022, 2)
     private val mars = YearMonth.of(2022, 3)
     private val april = YearMonth.of(2022, 4)
+    private val juli = YearMonth.of(2022, 7)
     private val august = YearMonth.of(2022, 8)
     private val september = YearMonth.of(2022, 9)
     private val oktober = YearMonth.of(2022, 10)
@@ -307,31 +311,252 @@ class forskyvBarnehageplassVilkårTest {
     }
 
     @Test
-    fun `forskyvBarnehageplassVilkår skal ikke forskyves en måned framover ved overgang fra periode med 33 timer`() {
-        val vilkårResultat1 = lagVilkårResultat(
-            vilkårType = Vilkår.BARNEHAGEPLASS,
-            periodeFom = august.atDay(14),
-            periodeTom = september.atEndOfMonth(),
-            antallTimer = BigDecimal.valueOf(33)
+    fun `Scenario 1 fra rundskrivet - Endring i KS skjer fra og med samme måned som en økning i barnehageplass`() {
+        val vilkårResultaterForBarn1 = listOf(
+            lagVilkårResultat(
+                vilkårType = Vilkår.BARNEHAGEPLASS,
+                periodeFom = juli.atEndOfMonth(),
+                periodeTom = september.atEndOfMonth(),
+                antallTimer = BigDecimal.valueOf(17)
+            ),
+            lagVilkårResultat(
+                vilkårType = Vilkår.BARNEHAGEPLASS,
+                periodeFom = oktober.atDay(1),
+                periodeTom = desember.atEndOfMonth(),
+                antallTimer = BigDecimal.valueOf(35)
+            )
         )
-        val vilkårResultat2 = lagVilkårResultat(
-            vilkårType = Vilkår.BARNEHAGEPLASS,
-            periodeFom = oktober.atDay(1),
-            periodeTom = desember.atDay(1),
-            antallTimer = BigDecimal.valueOf(8)
+
+        val vilkårResultaterForBarn2 = listOf(
+            lagVilkårResultat(
+                vilkårType = Vilkår.BARNEHAGEPLASS,
+                periodeFom = juli.atEndOfMonth(),
+                periodeTom = oktober.atDay(15),
+                antallTimer = BigDecimal.valueOf(24)
+            ),
+            lagVilkårResultat(
+                vilkårType = Vilkår.BARNEHAGEPLASS,
+                periodeFom = oktober.atDay(16),
+                periodeTom = desember.atEndOfMonth(),
+                antallTimer = BigDecimal.valueOf(40)
+            )
         )
 
-        val forskjøvedeVilkårResultater = listOf(vilkårResultat1, vilkårResultat2).forskyvBarnehageplassVilkår()
+        val forskjøvedeVilkårResultaterForBarn1 = vilkårResultaterForBarn1.forskyvBarnehageplassVilkår()
 
-        Assertions.assertEquals(2, forskjøvedeVilkårResultater.size)
+        assertThat(forskjøvedeVilkårResultaterForBarn1.size, Is(2))
+        assertThat(forskjøvedeVilkårResultaterForBarn1.first().fom, Is(august.atDay(1)))
+        assertThat(forskjøvedeVilkårResultaterForBarn1.first().tom, Is(september.atEndOfMonth()))
+        assertThat(forskjøvedeVilkårResultaterForBarn1.first().verdi.antallTimer, Is(BigDecimal(17)))
 
-        Assertions.assertEquals(september.atDay(1), forskjøvedeVilkårResultater.first().fom)
-        Assertions.assertEquals(september.atEndOfMonth(), forskjøvedeVilkårResultater.first().tom)
-        Assertions.assertEquals(BigDecimal.valueOf(33), forskjøvedeVilkårResultater.first().verdi.antallTimer)
+        assertThat(forskjøvedeVilkårResultaterForBarn1.last().fom, Is(oktober.atDay(1)))
+        assertThat(forskjøvedeVilkårResultaterForBarn1.last().tom, Is(desember.atEndOfMonth()))
+        assertThat(forskjøvedeVilkårResultaterForBarn1.last().verdi.antallTimer, Is(BigDecimal(35)))
 
-        Assertions.assertEquals(oktober.atDay(1), forskjøvedeVilkårResultater[1].fom)
-        Assertions.assertEquals(november.atEndOfMonth(), forskjøvedeVilkårResultater[1].tom)
-        Assertions.assertEquals(BigDecimal.valueOf(8), forskjøvedeVilkårResultater[1].verdi.antallTimer)
+        val forskjøvedeVilkårResultaterForBarn2 = vilkårResultaterForBarn2.forskyvBarnehageplassVilkår()
+
+        assertThat(forskjøvedeVilkårResultaterForBarn2.size, Is(2))
+        assertThat(forskjøvedeVilkårResultaterForBarn2.first().fom, Is(august.atDay(1)))
+        assertThat(forskjøvedeVilkårResultaterForBarn2.first().tom, Is(september.atEndOfMonth()))
+        assertThat(forskjøvedeVilkårResultaterForBarn2.first().verdi.antallTimer, Is(BigDecimal(24)))
+
+        assertThat(forskjøvedeVilkårResultaterForBarn2.last().fom, Is(oktober.atDay(1)))
+        assertThat(forskjøvedeVilkårResultaterForBarn2.last().tom, Is(desember.atEndOfMonth()))
+        assertThat(forskjøvedeVilkårResultaterForBarn2.last().verdi.antallTimer, Is(BigDecimal(40)))
+    }
+
+    @Test
+    fun `Scenario 2 fra rundskrivet - Kontantstøtte ytes fra og med måneden etter at vilkårene er oppfylt`() {
+        val vilkårResultater = listOf(
+            lagVilkårResultat(
+                vilkårType = Vilkår.BARNEHAGEPLASS,
+                periodeFom = september.atDay(1),
+                periodeTom = desember.atEndOfMonth(),
+                antallTimer = BigDecimal.valueOf(15)
+            )
+        )
+
+        val forskjøvedeVilkårResultater = vilkårResultater.forskyvBarnehageplassVilkår()
+
+        assertThat(forskjøvedeVilkårResultater.size, Is(1))
+        assertThat(forskjøvedeVilkårResultater.first().fom, Is(oktober.atDay(1)))
+        assertThat(forskjøvedeVilkårResultater.first().tom, Is(desember.atEndOfMonth()))
+        assertThat(forskjøvedeVilkårResultater.first().verdi.antallTimer, Is(BigDecimal(15)))
+    }
+
+    @Test
+    fun `Scenario 3 fra rundskrivet - Kontantstøtte opphører fra og med måneden retten til støtte opphører`() {
+        val vilkårResultater = listOf(
+            lagVilkårResultat(
+                vilkårType = Vilkår.BARNEHAGEPLASS,
+                periodeFom = juli.atDay(1),
+                periodeTom = september.atEndOfMonth(),
+                antallTimer = null
+            ),
+            lagVilkårResultat(
+                vilkårType = Vilkår.BARNEHAGEPLASS,
+                periodeFom = oktober.atDay(1),
+                periodeTom = desember.atEndOfMonth(),
+                antallTimer = BigDecimal(8)
+            )
+        )
+
+        val forskjøvedeVilkårResultater = vilkårResultater.forskyvBarnehageplassVilkår()
+
+        assertThat(forskjøvedeVilkårResultater.size, Is(2))
+        assertThat(forskjøvedeVilkårResultater.first().fom, Is(august.atDay(1)))
+        assertThat(forskjøvedeVilkårResultater.first().tom, Is(september.atEndOfMonth()))
+        assertThat(forskjøvedeVilkårResultater.first().verdi.antallTimer, Is(nullValue()))
+
+        assertThat(forskjøvedeVilkårResultater.last().fom, Is(oktober.atDay(1)))
+        assertThat(forskjøvedeVilkårResultater.last().tom, Is(desember.atEndOfMonth()))
+        assertThat(forskjøvedeVilkårResultater.last().verdi.antallTimer, Is(BigDecimal(8)))
+    }
+
+    @Test
+    fun `Scenario 4 fra rundskrivet - Kontantstøtte ytes fra og med måneden etter at barnet sluttet i barnehagen`() {
+        val vilkårResultater = listOf(
+            lagVilkårResultat(
+                vilkårType = Vilkår.BARNEHAGEPLASS,
+                periodeFom = juli.atDay(1),
+                periodeTom = september.atEndOfMonth(),
+                antallTimer = BigDecimal.valueOf(25)
+            ),
+            lagVilkårResultat(
+                vilkårType = Vilkår.BARNEHAGEPLASS,
+                periodeFom = oktober.atDay(1),
+                periodeTom = desember.atEndOfMonth(),
+                antallTimer = null
+            )
+        )
+
+        val forskjøvedeVilkårResultater = vilkårResultater.forskyvBarnehageplassVilkår()
+
+        assertThat(forskjøvedeVilkårResultater.size, Is(2))
+        assertThat(forskjøvedeVilkårResultater.first().fom, Is(august.atDay(1)))
+        assertThat(forskjøvedeVilkårResultater.first().tom, Is(september.atEndOfMonth()))
+        assertThat(forskjøvedeVilkårResultater.first().verdi.antallTimer, Is(BigDecimal.valueOf(25)))
+
+        assertThat(forskjøvedeVilkårResultater.last().fom, Is(oktober.atDay(1)))
+        assertThat(forskjøvedeVilkårResultater.last().tom, Is(desember.atEndOfMonth()))
+        assertThat(forskjøvedeVilkårResultater.last().verdi.antallTimer, Is(nullValue()))
+    }
+
+    @Test
+    fun `Scenario 5 fra rundskrivet - Reduksjon av barnehageplass skal forskyves riktig`() {
+        val vilkårResultater = listOf(
+            lagVilkårResultat(
+                vilkårType = Vilkår.BARNEHAGEPLASS,
+                periodeFom = januar.atDay(1),
+                periodeTom = august.atEndOfMonth(),
+                antallTimer = null
+            ),
+            lagVilkårResultat(
+                vilkårType = Vilkår.BARNEHAGEPLASS,
+                periodeFom = september.atDay(1),
+                periodeTom = september.atEndOfMonth(),
+                antallTimer = BigDecimal.valueOf(33)
+            ),
+            lagVilkårResultat(
+                vilkårType = Vilkår.BARNEHAGEPLASS,
+                periodeFom = oktober.atDay(1),
+                periodeTom = desember.atEndOfMonth(),
+                antallTimer = BigDecimal.valueOf(15)
+            )
+        )
+
+        val forskjøvedeVilkårResultater = vilkårResultater.forskyvBarnehageplassVilkår()
+
+        assertThat(forskjøvedeVilkårResultater.size, Is(3))
+        assertThat(forskjøvedeVilkårResultater[0].fom, Is(februar.atDay(1)))
+        assertThat(forskjøvedeVilkårResultater[0].tom, Is(august.atEndOfMonth()))
+        assertThat(forskjøvedeVilkårResultater[0].verdi.antallTimer, Is(nullValue()))
+
+        assertThat(forskjøvedeVilkårResultater[1].fom, Is(september.atDay(1)))
+        assertThat(forskjøvedeVilkårResultater[1].tom, Is(september.atEndOfMonth()))
+        assertThat(forskjøvedeVilkårResultater[1].verdi.antallTimer, Is(BigDecimal.valueOf(33)))
+
+        assertThat(forskjøvedeVilkårResultater[2].fom, Is(november.atDay(1)))
+        assertThat(forskjøvedeVilkårResultater[2].tom, Is(desember.atEndOfMonth()))
+        assertThat(forskjøvedeVilkårResultater[2].verdi.antallTimer, Is(BigDecimal.valueOf(15)))
+    }
+
+    @Test
+    fun `Scenario 6 fra rundskrivet - Slutt i barnehage skal forskyves riktig`() {
+        val vilkårResultater = listOf(
+            lagVilkårResultat(
+                vilkårType = Vilkår.BARNEHAGEPLASS,
+                periodeFom = januar.atDay(1),
+                periodeTom = august.atEndOfMonth(),
+                antallTimer = null
+            ),
+            lagVilkårResultat(
+                vilkårType = Vilkår.BARNEHAGEPLASS,
+                periodeFom = september.atDay(1),
+                periodeTom = september.atEndOfMonth(),
+                antallTimer = BigDecimal.valueOf(33)
+            ),
+            lagVilkårResultat(
+                vilkårType = Vilkår.BARNEHAGEPLASS,
+                periodeFom = oktober.atDay(1),
+                periodeTom = desember.atEndOfMonth(),
+                antallTimer = null
+            )
+        )
+
+        val forskjøvedeVilkårResultater = vilkårResultater.forskyvBarnehageplassVilkår()
+
+        assertThat(forskjøvedeVilkårResultater.size, Is(3))
+        assertThat(forskjøvedeVilkårResultater[0].fom, Is(februar.atDay(1)))
+        assertThat(forskjøvedeVilkårResultater[0].tom, Is(august.atEndOfMonth()))
+        assertThat(forskjøvedeVilkårResultater[0].verdi.antallTimer, Is(nullValue()))
+
+        assertThat(forskjøvedeVilkårResultater[1].fom, Is(september.atDay(1)))
+        assertThat(forskjøvedeVilkårResultater[1].tom, Is(september.atEndOfMonth()))
+        assertThat(forskjøvedeVilkårResultater[1].verdi.antallTimer, Is(BigDecimal.valueOf(33)))
+
+        assertThat(forskjøvedeVilkårResultater[2].fom, Is(oktober.atDay(1)))
+        assertThat(forskjøvedeVilkårResultater[2].tom, Is(desember.atEndOfMonth()))
+        assertThat(forskjøvedeVilkårResultater[2].verdi.antallTimer, Is(nullValue()))
+    }
+
+    @Test
+    fun `Scenario 7 fra rundskrivet - Slutt i barnehage skal forskyves riktig`() {
+        val vilkårResultater = listOf(
+            lagVilkårResultat(
+                vilkårType = Vilkår.BARNEHAGEPLASS,
+                periodeFom = januar.atDay(1),
+                periodeTom = august.atEndOfMonth(),
+                antallTimer = null
+            ),
+            lagVilkårResultat(
+                vilkårType = Vilkår.BARNEHAGEPLASS,
+                periodeFom = september.atDay(1),
+                periodeTom = september.atEndOfMonth(),
+                antallTimer = BigDecimal.valueOf(8)
+            ),
+            lagVilkårResultat(
+                vilkårType = Vilkår.BARNEHAGEPLASS,
+                periodeFom = oktober.atDay(1),
+                periodeTom = desember.atEndOfMonth(),
+                antallTimer = BigDecimal.valueOf(15)
+            )
+        )
+
+        val forskjøvedeVilkårResultater = vilkårResultater.forskyvBarnehageplassVilkår()
+
+        assertThat(forskjøvedeVilkårResultater.size, Is(3))
+        assertThat(forskjøvedeVilkårResultater[0].fom, Is(februar.atDay(1)))
+        assertThat(forskjøvedeVilkårResultater[0].tom, Is(august.atEndOfMonth()))
+        assertThat(forskjøvedeVilkårResultater[0].verdi.antallTimer, Is(nullValue()))
+
+        assertThat(forskjøvedeVilkårResultater[1].fom, Is(september.atDay(1)))
+        assertThat(forskjøvedeVilkårResultater[1].tom, Is(september.atEndOfMonth()))
+        assertThat(forskjøvedeVilkårResultater[1].verdi.antallTimer, Is(BigDecimal.valueOf(8)))
+
+        assertThat(forskjøvedeVilkårResultater[2].fom, Is(oktober.atDay(1)))
+        assertThat(forskjøvedeVilkårResultater[2].tom, Is(desember.atEndOfMonth()))
+        assertThat(forskjøvedeVilkårResultater[2].verdi.antallTimer, Is(BigDecimal.valueOf(15)))
     }
 
     @Test
