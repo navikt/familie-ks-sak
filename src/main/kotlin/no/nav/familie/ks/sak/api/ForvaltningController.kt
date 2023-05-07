df83422528f5264e705cf22305c7c6d3bcca3d87
Uy Nguyen <110383605+UyQuangNguyen@users.noreply.github.com>
2023-05-07 12:56:51 +0200

    Legger til knapp for å utfylle vilkårsvurdering (#336)

--- src/main/kotlin/no/nav/familie/ks/sak/api/ForvaltningController.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/api/ForvaltningController.kt
@@ -12,13 +12,16 @@ import no.nav.familie.kontrakter.felles.oppgave.OppgaveIdentV2
 import no.nav.familie.kontrakter.felles.oppgave.OpprettOppgaveRequest
 import no.nav.familie.ks.sak.api.dto.ManuellStartKonsistensavstemmingDto
 import no.nav.familie.ks.sak.api.dto.OpprettOppgaveDto
+import no.nav.familie.ks.sak.common.exception.Feil
 import no.nav.familie.ks.sak.common.util.Periode
 import no.nav.familie.ks.sak.config.BehandlerRolle
+import no.nav.familie.ks.sak.config.SpringProfile
 import no.nav.familie.ks.sak.integrasjon.familieintegrasjon.IntegrasjonClient
 import no.nav.familie.ks.sak.kjerne.avstemming.GrensesnittavstemmingTask
 import no.nav.familie.ks.sak.kjerne.avstemming.KonsistensavstemmingKjøreplanService
 import no.nav.familie.ks.sak.kjerne.avstemming.KonsistensavstemmingTask
 import no.nav.familie.ks.sak.kjerne.avstemming.domene.KonsistensavstemmingTaskDto
+import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.VilkårsvurderingService
 import no.nav.familie.ks.sak.kjerne.personident.PersonidentService
 import no.nav.familie.ks.sak.sikkerhet.TilgangService
 import no.nav.familie.ks.sak.statistikk.saksstatistikk.SakStatistikkService
@@ -27,11 +30,13 @@ import no.nav.familie.ks.sak.statistikk.stønadsstatistikk.StønadsstatistikkSer
 import no.nav.familie.prosessering.internal.TaskService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.slf4j.LoggerFactory
+import org.springframework.core.env.Environment
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -48,7 +53,9 @@ class ForvaltningController(
     private val stønadsstatistikkService: StønadsstatistikkService,
     private val taskService: TaskService,
     private val konsistensavstemmingKjøreplanService: KonsistensavstemmingKjøreplanService,
-    private val personidentService: PersonidentService
+    private val personidentService: PersonidentService,
+    private val vilkårsvurderingService: VilkårsvurderingService,
+    private val environment: Environment
 ) {
 
     private val logger = LoggerFactory.getLogger(ForvaltningController::class.java)
@@ -167,5 +174,28 @@ class ForvaltningController(
                 )
             )
         )
+    }
+
+    @PutMapping(path = ["/{behandlingId}/fyll-ut-vilkarsvurdering"])
+    fun fyllUtVilkårsvurdering(@PathVariable behandlingId: Long): ResponseEntity<Ressurs<String>> {
+        val activeProfiles = environment.activeProfiles.map { it.trim() }.toSet()
+        val erProd = SpringProfile.Prod.navn in activeProfiles
+        val erDevPostgresPreprod = SpringProfile.DevPostgresPreprod.navn in activeProfiles
+        val erPreprod = SpringProfile.Preprod.navn in activeProfiles
+
+        if (erProd) {
+            throw Feil("Skal ikke være tilgjengelig i prod")
+        } else if (!erDevPostgresPreprod && !erPreprod) {
+            throw Feil("Skal bare være tilgjengelig i for preprod eller lokalt")
+        }
+
+        tilgangService.validerTilgangTilHandling(
+            minimumBehandlerRolle = BehandlerRolle.SAKSBEHANDLER,
+            handling = "Fyll ut vilkårsvurderingen automatisk"
+        )
+
+        vilkårsvurderingService.fyllUtVilkårsvurdering(behandlingId)
+
+        return ResponseEntity.ok(Ressurs.success("Oppdaterte vilkårsvurdering"))
     }
 }
