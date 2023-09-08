2904d31570ea9d335b8b08a2eb0b03f5b644f258
bragejahren <brage.ekroll.jahren@nav.no>
2023-09-08 13:29:33 +0200

    Lagt til opphørsbegrunnelsene 44 og 45

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/brev/begrunnelser/Begrunnelse.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/brev/begrunnelser/Begrunnelse.kt
@@ -447,6 +447,14 @@ enum class Begrunnelse : IBegrunnelse {
         override val begrunnelseType = BegrunnelseType.OPPHØR
         override val sanityApiNavn = "opphorAvtaleOmDeltBostedIkkeGyldig"
     },
+    OPPHØR_DELT_BOSTED_BRUKER_MELDER_OM_DELTIDSPLASS {
+        override val begrunnelseType = BegrunnelseType.OPPHØR
+        override val sanityApiNavn = "opphorDeltBostedBrukerMelderOmDeltidsplass"
+    },
+    OPPHØR_DELT_BOSTED_KOMMUNEN_MELDER_OM_DELTIDSPLASS {
+        override val begrunnelseType = BegrunnelseType.OPPHØR
+        override val sanityApiNavn = "opphorDeltBostedKommunenMelderOmDeltidsplass"
+    },
     REDUKSJON_BARN_FLYTTET_FRA_SOKER {
         override val begrunnelseType = BegrunnelseType.REDUKSJON
         override val sanityApiNavn = "reduksjonBarnFlyttetFraSoker"
@@ -531,6 +539,7 @@ enum class Begrunnelse : IBegrunnelse {
         override val begrunnelseType = BegrunnelseType.REDUKSJON
         override val sanityApiNavn = "reduksjonDenAndreForelderenHarFottFraSammeTidsrom"
     }, ;
+
     override fun enumnavnTilString() = this.name
 }
 
