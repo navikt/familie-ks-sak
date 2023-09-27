8f1d385ae19f14afabd0397e2022cbe6bf24300f
marius-nav <Marius.Glad.Fredriksen@nav.no>
2023-09-27 10:39:26 +0200

    NAV-14677 - ktlint

--- src/main/kotlin/no/nav/familie/ks/sak/barnehagelister/domene/Barnehagebarn.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/barnehagelister/domene/Barnehagebarn.kt
@@ -29,5 +29,5 @@ data class Barnehagebarn(
     @Column(name = "KOMMUNE_NR", nullable = false, updatable = false)
     var kommuneNr: String,
     @Column(name = "ARKIV_REFERANSE", nullable = false, updatable = false)
-    var arkivReferanse: String
+    var arkivReferanse: String,
 ) : BaseEntitet()
