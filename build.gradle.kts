9be396b62fb231d16034a4e921fe3e90013c61c0
Stig Strøm <stig.helge.strom@nav.no>
2023-11-22 10:22:50 +0100

    Oppgraderer til JDK21 (#498)

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ configurations {
 
 group = "no.nav"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 val ktlint by configurations.creating
 
 repositories {
@@ -145,7 +145,7 @@ sourceSets.getByName("test") {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
