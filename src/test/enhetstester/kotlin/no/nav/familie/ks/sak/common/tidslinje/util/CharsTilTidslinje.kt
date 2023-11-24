1b73818666b0787514fb199339ee6cb48bd29552
halvormundal <halvorbmundal@gmail.com>
2023-11-24 13:49:58 +0100

    Legg til mulighet for å bygge kompetansetidslinje fra en char-tidslinje

---
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/common/tidslinje/util/CharsTilTidslinje.kt
@@ -0,0 +1,38 @@
+package no.nav.familie.ks.sak.common.tidslinje.util
+
+import no.nav.familie.ks.sak.common.tidslinje.Periode
+import no.nav.familie.ks.sak.common.tidslinje.Tidslinje
+import no.nav.familie.ks.sak.common.tidslinje.tilTidslinje
+import no.nav.familie.ks.sak.common.tidslinje.utvidelser.slåSammenLikePerioder
+import no.nav.familie.ks.sak.common.util.førsteDagIInneværendeMåned
+import no.nav.familie.ks.sak.common.util.sisteDagIInneværendeMåned
+import java.time.YearMonth
+
+fun <T> String.tilTidslinje(
+    startTidspunkt: YearMonth,
+    mapper: (Char) -> T,
+): Tidslinje<T> {
+    val charListe = this.toCharArray().toList()
+    val førsteTegnBeskriverUendelighet = charListe.first() == '<'
+    val sisteTegnBeskriverUendelighet = charListe.last() == '>'
+
+    val charListeUtenUendelighet =
+        when {
+            førsteTegnBeskriverUendelighet && sisteTegnBeskriverUendelighet -> charListe.drop(1).dropLast(1)
+            førsteTegnBeskriverUendelighet -> charListe.drop(1)
+            sisteTegnBeskriverUendelighet -> charListe.dropLast(1)
+            else -> charListe
+        }
+
+    val sisteIndex = charListeUtenUendelighet.size - 1
+    return charListeUtenUendelighet.mapIndexed { index, char ->
+        val erUendeligStartOgFørstePeriode = index == 0 && førsteTegnBeskriverUendelighet
+        val erUendeligSluttOgSistePeriode = index == sisteIndex && sisteTegnBeskriverUendelighet
+
+        val fom = if (erUendeligStartOgFørstePeriode) null else startTidspunkt.plusMonths(index.toLong()).førsteDagIInneværendeMåned()
+        val tom = if (erUendeligSluttOgSistePeriode) null else startTidspunkt.plusMonths(index.toLong()).sisteDagIInneværendeMåned()
+        val verdi = mapper(char)
+
+        Periode(verdi, fom, tom)
+    }.tilTidslinje().slåSammenLikePerioder()
+}
