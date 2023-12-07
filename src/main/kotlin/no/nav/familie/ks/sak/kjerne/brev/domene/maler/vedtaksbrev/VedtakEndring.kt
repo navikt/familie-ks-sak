f11ab8958146a9b5a990e9329d49d263c7735976
Uy Quang Nguyen <uy.quang.nguyen@nav.no>
2023-12-07 10:58:40 +0100

    Endre på navn til vedtak endring objekt felt

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/brev/domene/maler/vedtaksbrev/VedtakEndring.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/brev/domene/maler/vedtaksbrev/VedtakEndring.kt
@@ -51,7 +51,7 @@ data class VedtakEndring(
                             feilutbetaling = erFeilutbetalingPåBehandling,
                             korrigertVedtak = fellesdataForVedtaksbrev.korrigertVedtakData,
                             informasjonOmAarligKontroll = informasjonOmAarligKontroll,
-                            feilutbetaltKontantstotte = forMyeUtbetaltKontantstotte != null,
+                            forMyeUtbetaltKontantstotte = forMyeUtbetaltKontantstotte != null,
                             refusjonEosAvklart = refusjonEosAvklart,
                             refusjonEosUavklart = refusjonEosUavklart,
                         ),
@@ -80,7 +80,7 @@ data class EndringVedtakData(
         val klage: Boolean,
         val korrigertVedtak: KorrigertVedtakData?,
         val informasjonOmAarligKontroll: Boolean,
-        val feilutbetaltKontantstotte: Boolean,
+        val forMyeUtbetaltKontantstotte: Boolean,
         val refusjonEosAvklart: RefusjonEøsAvklart?,
         val refusjonEosUavklart: RefusjonEøsUavklart?,
     )
