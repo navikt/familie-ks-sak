cdee9fadb8098ddae8e82e97c1543462864c0c4e
halvormundal <halvorbmundal@gmail.com>
2023-12-14 16:15:06 +0100

    Lag tester

--- src/test/enhetstester/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vedtak/vedtaksperiode/utbetalingsperiodeMedBegrunnelser/UtbetalingsperiodeUtilTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vedtak/vedtaksperiode/utbetalingsperiodeMedBegrunnelser/UtbetalingsperiodeUtilTest.kt
@@ -1,5 +1,7 @@
 package no.nav.familie.ks.sak.kjerne.behandling.steg.vedtak.vedtaksperiode.utbetalingsperiodeMedBegrunnelser
 
+import apr
+import mars
 import no.nav.familie.ks.sak.common.tidslinje.Periode
 import no.nav.familie.ks.sak.common.util.TIDENES_ENDE
 import no.nav.familie.ks.sak.common.util.TIDENES_MORGEN
@@ -7,6 +9,7 @@ import no.nav.familie.ks.sak.common.util.førsteDagIInneværendeMåned
 import no.nav.familie.ks.sak.common.util.sisteDagIInneværendeMåned
 import no.nav.familie.ks.sak.data.lagAndelTilkjentYtelse
 import no.nav.familie.ks.sak.data.lagBehandling
+import no.nav.familie.ks.sak.data.lagKompetanse
 import no.nav.familie.ks.sak.data.lagPersonResultat
 import no.nav.familie.ks.sak.data.lagPersonopplysningGrunnlag
 import no.nav.familie.ks.sak.data.lagVedtaksperiodeMedBegrunnelser
@@ -18,22 +21,28 @@ import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.domene.Res
 import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.domene.Vilkårsvurdering
 import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.tilFørskjøvetOppfylteVilkårResultatTidslinjeMap
 import no.nav.familie.ks.sak.kjerne.beregning.AndelTilkjentYtelseMedEndreteUtbetalinger
-import no.nav.familie.ks.sak.kjerne.personident.Aktør
+import no.nav.familie.ks.sak.kjerne.beregning.TilkjentYtelseUtils
+import no.nav.familie.ks.sak.kjerne.beregning.domene.TilkjentYtelse
+import no.nav.familie.ks.sak.kjerne.eøs.kompetanse.domene.KompetanseAktivitet
+import no.nav.familie.ks.sak.kjerne.personopplysninggrunnlag.domene.Person
 import no.nav.familie.ks.sak.kjerne.personopplysninggrunnlag.domene.PersonType
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import java.time.LocalDate
 import java.time.YearMonth
 
 internal class UtbetalingsperiodeUtilTest {
     val behandling = lagBehandling(opprettetÅrsak = BehandlingÅrsak.SØKNAD)
-    private val person1Fnr = randomFnr()
-    private val person2Fnr = randomFnr()
+    val vedtak = Vedtak(behandling = behandling)
 
-    val personopplysningGrunnlag = lagPersonopplysningGrunnlag(barnasIdenter = listOf(person1Fnr, person2Fnr))
+    private val barn1Fnr = randomFnr(fødselsdato = 15.mars(2020))
+    private val barn2Fnr = randomFnr(fødselsdato = 15.apr(2022))
+    val personopplysningGrunnlag = lagPersonopplysningGrunnlag(barnasIdenter = listOf(barn1Fnr, barn2Fnr))
 
-    private val person1 = personopplysningGrunnlag.barna[0].aktør
-    private val person2 = personopplysningGrunnlag.barna[1].aktør
+    private val søker = personopplysningGrunnlag.søker
+    private val barn1 = personopplysningGrunnlag.barna[0]
+    private val barn2 = personopplysningGrunnlag.barna[1]
 
     @Test
     fun `hentPerioderMedUtbetaling skal beholde split i andel tilkjent ytelse`() {
@@ -51,7 +60,7 @@ internal class UtbetalingsperiodeUtilTest {
                     stønadFom = mars2020,
                     stønadTom = april2020,
                     sats = 1000,
-                    aktør = person1,
+                    aktør = barn1.aktør,
                 ),
                 emptyList(),
             )
@@ -63,7 +72,7 @@ internal class UtbetalingsperiodeUtilTest {
                     stønadFom = mai2020,
                     stønadTom = juli2020,
                     sats = 1000,
-                    aktør = person1,
+                    aktør = barn1.aktør,
                 ),
                 emptyList(),
             )
@@ -75,7 +84,7 @@ internal class UtbetalingsperiodeUtilTest {
                     stønadFom = mars2020,
                     stønadTom = juli2020,
                     sats = 1000,
-                    aktør = person2,
+                    aktør = barn2.aktør,
                 ),
                 emptyList(),
             )
@@ -102,8 +111,8 @@ internal class UtbetalingsperiodeUtilTest {
 
         val personResultater =
             setOf(
-                vilkårsvurdering.lagGodkjentPersonResultatForBarn(person1),
-                vilkårsvurdering.lagGodkjentPersonResultatForBarn(person2),
+                vilkårsvurdering.lagGodkjentPersonResultatForBarn(barn1),
+                vilkårsvurdering.lagGodkjentPersonResultatForBarn(barn2),
             )
 
         val faktiskResultat =
@@ -142,7 +151,7 @@ internal class UtbetalingsperiodeUtilTest {
                     stønadFom = mars2020,
                     stønadTom = mai2020,
                     sats = 1000,
-                    aktør = person1,
+                    aktør = barn1.aktør,
                 ),
                 emptyList(),
             )
@@ -154,7 +163,7 @@ internal class UtbetalingsperiodeUtilTest {
                     stønadFom = mai2020,
                     stønadTom = juli2020,
                     sats = 1000,
-                    aktør = person2,
+                    aktør = barn2.aktør,
                 ),
                 emptyList(),
             )
@@ -188,8 +197,8 @@ internal class UtbetalingsperiodeUtilTest {
 
         val personResultater =
             setOf(
-                vilkårsvurdering.lagGodkjentPersonResultatForBarn(person1),
-                vilkårsvurdering.lagGodkjentPersonResultatForBarn(person2),
+                vilkårsvurdering.lagGodkjentPersonResultatForBarn(barn1),
+                vilkårsvurdering.lagGodkjentPersonResultatForBarn(barn2),
             )
 
         val faktiskResultat =
@@ -211,14 +220,128 @@ internal class UtbetalingsperiodeUtilTest {
         )
     }
 
-    private fun Vilkårsvurdering.lagGodkjentPersonResultatForBarn(aktør: Aktør) =
+    @Nested
+    inner class EØS {
+        @Test
+        fun `Skal lage ny vedtaksperiode dersom vi får en ny kompetansene`() {
+            val (vilkårsvurdering, tilkjentYtelse) = kjørBehandlingFramTilBehandlingsresultatMedAltGodkjent()
+            val forskjøvetVilkårResultatTidslinjeMap = vilkårsvurdering.personResultater.tilFørskjøvetOppfylteVilkårResultatTidslinjeMap(personopplysningGrunnlag)
+
+            val vedtaksperioderUtenKompetanse =
+                hentPerioderMedUtbetaling(
+                    andelerTilkjentYtelse = tilkjentYtelse.andelerTilkjentYtelse.map { AndelTilkjentYtelseMedEndreteUtbetalinger(it, emptyList()) },
+                    vedtak = vedtak,
+                    forskjøvetVilkårResultatTidslinjeMap = forskjøvetVilkårResultatTidslinjeMap,
+                    kompetanser = emptyList(),
+                )
+
+            assertThat(vedtaksperioderUtenKompetanse.size).isEqualTo(2)
+
+            val kompetanser =
+                listOf(
+                    lagKompetanse(
+                        fom = tilkjentYtelse.andelerTilkjentYtelse.first().stønadFom,
+                        tom = tilkjentYtelse.andelerTilkjentYtelse.first().stønadFom.plusMonths(1),
+                        barnAktører = setOf(barn1.aktør),
+                    ),
+                )
+
+            val vedtaksperioderMedKompetanse =
+                hentPerioderMedUtbetaling(
+                    andelerTilkjentYtelse = tilkjentYtelse.andelerTilkjentYtelse.map { AndelTilkjentYtelseMedEndreteUtbetalinger(it, emptyList()) },
+                    vedtak = vedtak,
+                    forskjøvetVilkårResultatTidslinjeMap = forskjøvetVilkårResultatTidslinjeMap,
+                    kompetanser = kompetanser,
+                )
+
+            assertThat(vedtaksperioderMedKompetanse.size).isEqualTo(3)
+        }
+
+        @Test
+        fun `Skal lage ny vedtaksperiode dersom det er endring i kompetansene`() {
+            val (vilkårsvurdering, tilkjentYtelse) = kjørBehandlingFramTilBehandlingsresultatMedAltGodkjent()
+            val forskjøvetVilkårResultatTidslinjeMap = vilkårsvurdering.personResultater.tilFørskjøvetOppfylteVilkårResultatTidslinjeMap(personopplysningGrunnlag)
+
+            val vedtaksperioderUtenKompetanse =
+                hentPerioderMedUtbetaling(
+                    andelerTilkjentYtelse = tilkjentYtelse.andelerTilkjentYtelse.map { AndelTilkjentYtelseMedEndreteUtbetalinger(it, emptyList()) },
+                    vedtak = vedtak,
+                    forskjøvetVilkårResultatTidslinjeMap = forskjøvetVilkårResultatTidslinjeMap,
+                    kompetanser = emptyList(),
+                )
+
+            assertThat(vedtaksperioderUtenKompetanse.size).isEqualTo(2)
+
+            val kompetanser =
+                listOf(
+                    lagKompetanse(
+                        fom = tilkjentYtelse.andelerTilkjentYtelse.first().stønadFom,
+                        tom = tilkjentYtelse.andelerTilkjentYtelse.first().stønadFom,
+                        barnAktører = setOf(barn1.aktør),
+                        annenForeldersAktivitet = KompetanseAktivitet.ARBEIDER,
+                    ),
+                    lagKompetanse(
+                        fom = tilkjentYtelse.andelerTilkjentYtelse.first().stønadFom.plusMonths(1),
+                        tom = tilkjentYtelse.andelerTilkjentYtelse.first().stønadFom.plusMonths(1),
+                        barnAktører = setOf(barn1.aktør),
+                        annenForeldersAktivitet = KompetanseAktivitet.INAKTIV,
+                    ),
+                )
+
+            val vedtaksperioderMedKompetanse =
+                hentPerioderMedUtbetaling(
+                    andelerTilkjentYtelse = tilkjentYtelse.andelerTilkjentYtelse.map { AndelTilkjentYtelseMedEndreteUtbetalinger(it, emptyList()) },
+                    vedtak = vedtak,
+                    forskjøvetVilkårResultatTidslinjeMap = forskjøvetVilkårResultatTidslinjeMap,
+                    kompetanser = kompetanser,
+                )
+
+            assertThat(vedtaksperioderMedKompetanse.size).isEqualTo(4)
+        }
+    }
+
+    data class DataEtterVilkårsvurdering(
+        val vilkårsvurdering: Vilkårsvurdering,
+        val tilkjentYtelse: TilkjentYtelse,
+    )
+
+    private fun kjørBehandlingFramTilBehandlingsresultatMedAltGodkjent(): DataEtterVilkårsvurdering {
+        val vilkårsvurdering = Vilkårsvurdering(behandling = behandling)
+        val personResultater =
+            setOf(
+                vilkårsvurdering.lagGodkjentPersonResultatForSøker(søker),
+                vilkårsvurdering.lagGodkjentPersonResultatForBarn(barn1),
+                vilkårsvurdering.lagGodkjentPersonResultatForBarn(barn2),
+            )
+        vilkårsvurdering.personResultater = personResultater
+
+        val tilkjentYtelse =
+            TilkjentYtelseUtils.beregnTilkjentYtelse(
+                vilkårsvurdering = vilkårsvurdering,
+                personopplysningGrunnlag = personopplysningGrunnlag,
+            )
+        return DataEtterVilkårsvurdering(vilkårsvurdering, tilkjentYtelse)
+    }
+
+    private fun Vilkårsvurdering.lagGodkjentPersonResultatForBarn(person: Person) =
         lagPersonResultat(
             vilkårsvurdering = this,
-            aktør = aktør,
+            aktør = person.aktør,
             resultat = Resultat.OPPFYLT,
-            periodeFom = LocalDate.now().minusYears(1),
-            periodeTom = LocalDate.now().plusYears(2),
+            periodeFom = person.fødselsdato.plusYears(1),
+            periodeTom = person.fødselsdato.plusYears(2),
             lagFullstendigVilkårResultat = true,
             personType = PersonType.BARN,
+        )
+
+    private fun Vilkårsvurdering.lagGodkjentPersonResultatForSøker(person: Person) =
+        lagPersonResultat(
+            vilkårsvurdering = this,
+            aktør = person.aktør,
+            resultat = Resultat.OPPFYLT,
+            periodeFom = person.fødselsdato,
+            periodeTom = null,
+            lagFullstendigVilkårResultat = true,
+            personType = PersonType.SØKER,
         )
 }
