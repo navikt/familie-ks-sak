36468f02c86da38d919d7d40870bbbce1ca1aa0b
Tor Nærland <tor.nerland@nav.no>
2024-02-05 10:38:24 +0100

    sql-script og jpa entity/interface

---
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/brev/mottaker/BrevmottakerRepository.kt
@@ -0,0 +1,10 @@
+package no.nav.familie.ks.sak.kjerne.brev.mottaker
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+
+interface BrevmottakerRepository : JpaRepository<BrevmottakerDb, Long> {
+
+    @Query(value = "SELECT b FROM Brevmottaker b WHERE b.behandlingId = :behandlingId")
+    fun finnBrevMottakereForBehandling(behandlingId: Long): List<BrevmottakerDb>
+}
