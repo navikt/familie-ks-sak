947e0bcf7f2e29527c1b1aedcc0de06aa6ff263d
halvorbmundal <halvorbmundal@users.noreply.github.com>
2024-02-16 14:56:41 +0100

    Fjern kode brukt til å sende ut informasjonsbrev om mulig lovendring for kontantstøtten i 2024 (#612)
    Avhenger av https://github.com/navikt/familie-kontrakter/pull/873

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/brev/domene/maler/BrevDto.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/brev/domene/maler/BrevDto.kt
@@ -53,8 +53,6 @@ typealias Flettefelt = List<String>?
  * for detaljer om alt som skal inn når du legger til en ny brevmal.
  ***/
 enum class Brevmal(val erVedtaksbrev: Boolean, val apiNavn: String, val visningsTekst: String) {
-    @Deprecated("Kun til bruk for mulig lovendring august 2024")
-    INFORMASJONSBREV_MULIG_LOVENDRING_2024(false, "informasjonOmKontantstotte", "Kontantstøtte – viktig informasjon"),
     INFORMASJONSBREV_DELT_BOSTED(false, "informasjonsbrevDeltBosted", "Informasjonsbrev delt bosted"),
     INFORMASJONSBREV_TIL_FORELDER_OMFATTET_NORSK_LOVGIVNING_HAR_FÅTT_EN_SØKNAD_FRA_ANNEN_FORELDER(
         erVedtaksbrev = false,
@@ -140,7 +138,6 @@ enum class Brevmal(val erVedtaksbrev: Boolean, val apiNavn: String, val visnings
             INFORMASJONSBREV_KAN_SØKE_EØS,
             INFORMASJONSBREV_TIL_FORELDER_OMFATTET_NORSK_LOVGIVNING_HAR_FÅTT_EN_SØKNAD_FRA_ANNEN_FORELDER,
             INFORMASJONSBREV_TIL_FORELDER_OMFATTET_NORSK_LOVGIVNING_VARSEL_OM_REVURDERING,
-            INFORMASJONSBREV_MULIG_LOVENDRING_2024,
             -> false
 
             VEDTAK_FØRSTEGANGSVEDTAK,
@@ -174,7 +171,6 @@ enum class Brevmal(val erVedtaksbrev: Boolean, val apiNavn: String, val visnings
             VARSEL_OM_REVURDERING_FRA_NASJONAL_TIL_EØS -> Dokumenttype.KONTANTSTØTTE_VARSEL_OM_REVURDERING_FRA_NASJONAL_TIL_EØS
             INFORMASJONSBREV_KAN_SØKE_EØS -> Dokumenttype.KONTANTSTØTTE_INFORMASJONSBREV_KAN_SØKE_EØS
             VARSEL_ANNEN_FORELDER_MED_SELVSTENDIG_RETT_SØKT -> Dokumenttype.KONTANTSTØTTE_VARSEL_ANNEN_FORELDER_MED_SELVSTENDIG_RETT_SØKT
-            INFORMASJONSBREV_MULIG_LOVENDRING_2024 -> Dokumenttype.KONTANTSTØTTE_INFORMASJONSBREV_MULIG_LOVENDRING_2024
 
             VEDTAK_ENDRING,
             VEDTAK_OPPHØRT,
@@ -219,7 +215,6 @@ enum class Brevmal(val erVedtaksbrev: Boolean, val apiNavn: String, val visnings
                 AUTOVEDTAK_BARN_6_OG_18_ÅR_OG_SMÅBARNSTILLEGG -> Distribusjonstype.VEDTAK
                 AUTOVEDTAK_NYFØDT_FØRSTE_BARN -> Distribusjonstype.VEDTAK
                 AUTOVEDTAK_NYFØDT_BARN_FRA_FØR -> Distribusjonstype.VEDTAK
-                INFORMASJONSBREV_MULIG_LOVENDRING_2024 -> Distribusjonstype.VIKTIG
             }
 
     fun setterBehandlingPåVent(): Boolean =
