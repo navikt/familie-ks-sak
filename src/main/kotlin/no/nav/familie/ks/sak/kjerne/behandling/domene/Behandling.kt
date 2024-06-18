aab1e5c5d8a46c07bf583d3e9618ef9686ca0bf4
Tor Nærland <tor.nerland@nav.no>
2024-06-18 22:00:32 +0200

    Generert overstyrende equals() og hashCode()

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/domene/Behandling.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/domene/Behandling.kt
@@ -77,8 +77,25 @@ data class Behandling(
             "type=$type, " +
             "kategori=$kategori, " +
             "status=$status, " +
-            "resultat=$resultat, " +
-            "aktivertTidspunkt=$aktivertTidspunkt)"
+            "resultat=$resultat)"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Behandling
+
+        if (id != other.id) return false
+        if (fagsak != other.fagsak) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + fagsak.hashCode()
+        return result
     }
 
     fun validerBehandlingstype(sisteBehandlingSomErVedtatt: Behandling? = null) {
