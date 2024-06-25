16af8b9af33f4699a88b8212bc8ff5310e1072ce
halvor <halvorbmundal@gmail.com>
2024-06-25 17:14:00 +0200

    Fiks litt på logikk for generering av barnets alder-vilkåret

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vilkårsvurdering/AutomatiskSatteVilkårUtils.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vilkårsvurdering/AutomatiskSatteVilkårUtils.kt
@@ -1,4 +1,5 @@
 import no.nav.familie.ks.sak.common.util.DATO_LOVENDRING_2024
+import no.nav.familie.ks.sak.common.util.erSammeEllerEtter
 import no.nav.familie.ks.sak.kjerne.behandling.domene.Behandling
 import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.domene.PersonResultat
 import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.domene.Resultat
@@ -15,8 +16,11 @@ fun lagAutomatiskGenererteVilkårForBarnetsAlder(
     val periodeFomBarnetsAlderLov2024 = fødselsdato.plusMonths(13)
     val periodeTomBarnetsAlderLov2024 = fødselsdato.plusMonths(19)
 
-    val erTruffetAvRegelverk2021 = periodeFomBarnetsAlderLov2024.isBefore(DATO_LOVENDRING_2024)
-    val erTruffetAvRegelverk2024 = periodeTomBarnetsAlderLov2024.isAfter(DATO_LOVENDRING_2024)
+    val periodeFomBarnetsAlderLov2021 = fødselsdato.plusYears(1)
+    val periodeTomBarnetsAlderLov2021 = fødselsdato.plusYears(2)
+
+    val erTruffetAvRegelverk2021 = periodeFomBarnetsAlderLov2021.isBefore(DATO_LOVENDRING_2024)
+    val erTruffetAvRegelverk2024 = periodeTomBarnetsAlderLov2024.erSammeEllerEtter(DATO_LOVENDRING_2024)
 
     val vilkårResultatEtterRegelverk2021 =
         if (erTruffetAvRegelverk2021) {
@@ -27,8 +31,8 @@ fun lagAutomatiskGenererteVilkårForBarnetsAlder(
                 vilkårType = Vilkår.BARNETS_ALDER,
                 begrunnelse = "Vurdert og satt automatisk",
                 behandlingId = behandling.id,
-                periodeFom = fødselsdato.plusYears(1),
-                periodeTom = minOf(fødselsdato.plusYears(2), DATO_LOVENDRING_2024),
+                periodeFom = periodeFomBarnetsAlderLov2021,
+                periodeTom = minOf(periodeTomBarnetsAlderLov2021, DATO_LOVENDRING_2024),
                 regelsett = VilkårRegelsett.LOV_AUGUST_2021,
             )
         } else {
@@ -44,8 +48,8 @@ fun lagAutomatiskGenererteVilkårForBarnetsAlder(
                 vilkårType = Vilkår.BARNETS_ALDER,
                 begrunnelse = "Vurdert og satt automatisk",
                 behandlingId = behandling.id,
-                periodeFom = maxOf(fødselsdato.plusMonths(13), DATO_LOVENDRING_2024),
-                periodeTom = fødselsdato.plusMonths(19),
+                periodeFom = maxOf(periodeFomBarnetsAlderLov2024, DATO_LOVENDRING_2024),
+                periodeTom = periodeTomBarnetsAlderLov2024,
                 regelsett = VilkårRegelsett.LOV_AUGUST_2024,
             )
         } else {
