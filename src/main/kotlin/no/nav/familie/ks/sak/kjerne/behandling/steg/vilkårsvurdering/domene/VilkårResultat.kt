c3f01ffd5eb8ccb7f90167b929125955d46d9d34
halvor <halvorbmundal@gmail.com>
2024-06-26 10:07:21 +0200

    Legg til `erAutomatiskVurdert` i toString

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vilkårsvurdering/domene/VilkårResultat.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vilkårsvurdering/domene/VilkårResultat.kt
@@ -160,7 +160,7 @@ class VilkårResultat(
     )
 
     override fun toString(): String {
-        return """ VilkårResultat(id=$id,vilkårType=$vilkårType,periodeFom=$periodeFom,periodeTom=$periodeTom,resultat=$resultat,evalueringÅrsaker=$evalueringÅrsaker") """
+        return """ VilkårResultat(id=$id,vilkårType=$vilkårType,periodeFom=$periodeFom,periodeTom=$periodeTom,resultat=$resultat,evalueringÅrsaker=$evalueringÅrsaker",erAutomatiskVurdert=$erAutomatiskVurdert) """
     }
 
     companion object {
