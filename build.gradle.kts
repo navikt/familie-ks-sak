9f084521b349a3de373cb6a9941a0fe73b1ce5a2
Stig Strøm <stig.helge.strom@nav.no>
2024-06-28 13:04:26 +0200

    Revert "Bump the major-dependencies group across 1 directory with 7 updates" (#727)
    Reverts navikt/familie-ks-sak#721

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    val kotlinVersion = "2.0.0"
+    val kotlinVersion = "1.9.24"
     kotlin("jvm") version kotlinVersion
 
     id("org.springframework.boot") version "3.3.0"
