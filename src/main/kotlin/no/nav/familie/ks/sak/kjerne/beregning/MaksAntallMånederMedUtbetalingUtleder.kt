cc2fd6101f46bfeda6dc95b756c35b59c8e8e740
bragejahren <brage.ekroll.jahren@nav.no>
2024-06-28 16:24:42 +0200

    Forbedrer lesbarhet av metoden utledMaksAntallMånederMedUtbetaling

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/beregning/MaksAntallMånederMedUtbetalingUtleder.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/beregning/MaksAntallMånederMedUtbetalingUtleder.kt
@@ -2,23 +2,25 @@ package no.nav.familie.ks.sak.kjerne.beregning
 
 import no.nav.familie.ks.sak.common.util.DATO_LOVENDRING_2024
 import no.nav.familie.ks.sak.common.util.tilYearMonth
+import no.nav.familie.ks.sak.common.util.toYearMonth
 import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.VilkårLovverk
 import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.VilkårLovverkInformasjonForBarn
 import java.time.temporal.ChronoUnit
 
 fun utledMaksAntallMånederMedUtbetaling(vilkårLovverkInformasjonForBarn: VilkårLovverkInformasjonForBarn): Long {
     return when (vilkårLovverkInformasjonForBarn.lovverk) {
-        VilkårLovverk.LOVVERK_2021_OG_2024 -> 7L
+        VilkårLovverk.LOVVERK_2024,
+        VilkårLovverk.LOVVERK_2021_OG_2024,
+        -> 7L
         VilkårLovverk.LOVVVERK_2021 -> {
-            val tomBarnetsAlderEllerDatoForLovendring =
+            val sisteMuligeUtbetaling =
                 minOf(
                     vilkårLovverkInformasjonForBarn.periodeTomBarnetsAlderLov2021.tilYearMonth(),
-                    DATO_LOVENDRING_2024.tilYearMonth(),
-                )
-            val fomBarnetsAlder = vilkårLovverkInformasjonForBarn.periodeFomBarnetsAlderLov2024.tilYearMonth()
-            fomBarnetsAlder.until(tomBarnetsAlderEllerDatoForLovendring, ChronoUnit.MONTHS)
-        }
+                    DATO_LOVENDRING_2024.toYearMonth(),
+                ).minusMonths(1)
+            val førsteMuligeUtbetaling = vilkårLovverkInformasjonForBarn.periodeFomBarnetsAlderLov2021.tilYearMonth().plusMonths(1)
 
-        VilkårLovverk.LOVVERK_2024 -> 7L
+            førsteMuligeUtbetaling.until(sisteMuligeUtbetaling, ChronoUnit.MONTHS) + 1L
+        }
     }
 }
