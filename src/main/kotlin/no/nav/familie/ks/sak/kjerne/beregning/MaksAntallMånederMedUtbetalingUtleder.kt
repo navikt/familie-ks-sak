578c8a644077a058dc214a0dcb321dc2de19d904
Uy Nguyen <110383605+UyQuangNguyen@users.noreply.github.com>
2024-07-23 14:55:20 +0200

    Fix/adopsjon maks utbetalingsperiode (#755)
    Favrokort:
    https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-21771
    
    Bygget videre på det Kristine og Hanne jobbet med!
    
    ---------
    
    Co-authored-by: Kristine Steine <kristine.steine@bekk.no>

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/beregning/MaksAntallMånederMedUtbetalingUtleder.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/beregning/MaksAntallMånederMedUtbetalingUtleder.kt
@@ -2,23 +2,34 @@ package no.nav.familie.ks.sak.kjerne.beregning
 
 import no.nav.familie.ks.sak.common.util.DATO_LOVENDRING_2024
 import no.nav.familie.ks.sak.common.util.tilYearMonth
-import no.nav.familie.ks.sak.common.util.toYearMonth
 import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.VilkårLovverk
 import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.VilkårLovverkInformasjonForBarn
+import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.domene.VilkårResultat
 import java.time.temporal.ChronoUnit
 
-fun utledMaksAntallMånederMedUtbetaling(vilkårLovverkInformasjonForBarn: VilkårLovverkInformasjonForBarn): Long =
+fun utledMaksAntallMånederMedUtbetaling(
+    vilkårLovverkInformasjonForBarn: VilkårLovverkInformasjonForBarn,
+    barnetsAlderVilkårResultater: List<VilkårResultat>,
+): Long =
     when (vilkårLovverkInformasjonForBarn.lovverk) {
         VilkårLovverk.LOVVERK_2024,
         VilkårLovverk.LOVVERK_2021_OG_2024,
         -> 7L
         VilkårLovverk.LOVVERK_2021 -> {
+            val førsteBarnetsAlderVilkårResultatFom = barnetsAlderVilkårResultater.sortedBy { it.periodeFom }.first().periodeFom!!
+            val sisteBarnetsAlderVilkårResultatTom = barnetsAlderVilkårResultater.sortedBy { it.periodeTom }.last().periodeTom!!
+            val minstAvTomEllerLovEndringDato = minOf(sisteBarnetsAlderVilkårResultatTom, DATO_LOVENDRING_2024)
+            val dagenFørLovendring = DATO_LOVENDRING_2024.minusDays(1)
+
             val sisteMuligeUtbetaling =
-                minOf(
-                    vilkårLovverkInformasjonForBarn.periodeTomBarnetsAlderLov2021.tilYearMonth(),
-                    DATO_LOVENDRING_2024.toYearMonth(),
-                ).minusMonths(1)
-            val førsteMuligeUtbetaling = vilkårLovverkInformasjonForBarn.periodeFomBarnetsAlderLov2021.tilYearMonth().plusMonths(1)
+                when {
+                    vilkårLovverkInformasjonForBarn.fødselsdato.plusYears(2)
+                        != dagenFørLovendring && sisteBarnetsAlderVilkårResultatTom == dagenFørLovendring -> minstAvTomEllerLovEndringDato
+
+                    else -> minstAvTomEllerLovEndringDato.minusMonths(1)
+                }
+
+            val førsteMuligeUtbetaling = førsteBarnetsAlderVilkårResultatFom.tilYearMonth().plusMonths(1)
 
             førsteMuligeUtbetaling.until(sisteMuligeUtbetaling, ChronoUnit.MONTHS) + 1L
         }
