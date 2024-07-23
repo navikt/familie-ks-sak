7e154146ff9f9ecc2ae36be0a2090282bc3caefc
fredrikpf <fredrik.pfeil@gmail.com>
2024-07-23 13:14:00 +0200

    ikke opprett behandleSakOppgave når det er automatisk behandling
    ikke forvent at behandling hopper rett til AVSLUTT_BEHANDLING etter opprettAutomatiskBehandlingOgKjørTilBehandlingsresultat()
    
    sett opphørsDato til tidligste fom i forrige behandling dersom denne ikke har opphørsdato
    
    Signed-off-by: fredrikpf <fredrik.pfeil@gmail.com>

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/behandlingsresultat/BehandlingsresultatOpphørUtils.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/behandlingsresultat/BehandlingsresultatOpphørUtils.kt
@@ -96,7 +96,7 @@ object BehandlingsresultatOpphørUtils {
             .finnOpphørsdato()
             ?: forrigeAndelerIBehandling
                 .filtrerBortIrrelevanteAndeler(endretAndeler = endretAndelerForForrigeBehandling)
-                .minOfOrNull { it.stønadTom } // TODO: Skal dette være stønadFom eller stønadTom?
+                .minOfOrNull { it.stønadFom }
 
     /**
      * Hvis det ikke fantes noen andeler i forrige behandling defaulter vi til inneværende måned
@@ -128,20 +128,21 @@ object BehandlingsresultatOpphørUtils {
         val andelTilkjentYtelseTidslinje = andelerPåPersonFiltrertPåType.map { it.tilPeriode() }.tilTidslinje()
         val endretUtbetalingAndelTidslinje = endretAndelerPåPerson.map { it.tilPeriode() }.tilTidslinje()
 
-        return andelTilkjentYtelseTidslinje.kombinerMed(endretUtbetalingAndelTidslinje) { andelTilkjentYtelse, endretUtbetalingAndel ->
-            val kalkulertUtbetalingsbeløp = andelTilkjentYtelse?.kalkulertUtbetalingsbeløp ?: return@kombinerMed null
-            val endringsperiodeÅrsak = endretUtbetalingAndel?.årsak ?: return@kombinerMed andelTilkjentYtelse
+        return andelTilkjentYtelseTidslinje
+            .kombinerMed(endretUtbetalingAndelTidslinje) { andelTilkjentYtelse, endretUtbetalingAndel ->
+                val kalkulertUtbetalingsbeløp = andelTilkjentYtelse?.kalkulertUtbetalingsbeløp ?: return@kombinerMed null
+                val endringsperiodeÅrsak = endretUtbetalingAndel?.årsak ?: return@kombinerMed andelTilkjentYtelse
 
-            when (endringsperiodeÅrsak) {
-                Årsak.ALLEREDE_UTBETALT,
-                Årsak.ENDRE_MOTTAKER,
-                Årsak.ETTERBETALING_3MND,
-                ->
-                    // Vi ønsker å filtrere bort andeler som har 0 i kalkulertUtbetalingsbeløp
-                    if (kalkulertUtbetalingsbeløp == 0) null else andelTilkjentYtelse
+                when (endringsperiodeÅrsak) {
+                    Årsak.ALLEREDE_UTBETALT,
+                    Årsak.ENDRE_MOTTAKER,
+                    Årsak.ETTERBETALING_3MND,
+                    ->
+                        // Vi ønsker å filtrere bort andeler som har 0 i kalkulertUtbetalingsbeløp
+                        if (kalkulertUtbetalingsbeløp == 0) null else andelTilkjentYtelse
 
-                Årsak.DELT_BOSTED -> andelTilkjentYtelse
-            }
-        }.tilAndelTilkjentYtelse()
+                    Årsak.DELT_BOSTED -> andelTilkjentYtelse
+                }
+            }.tilAndelTilkjentYtelse()
     }
 }
