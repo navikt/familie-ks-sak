f43fbd000689e3d38e40746fd904d59102705782
fredrikpf <fredrik.pfeil@gmail.com>
2024-08-07 20:05:34 +0200

    legg til valideringer på at fagsak er løpende og at det ikke er fremtidig opphør
    Signed-off-by: fredrikpf <fredrik.pfeil@gmail.com>

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/autovedtak/AutovedtakLovendringTask.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/autovedtak/AutovedtakLovendringTask.kt
@@ -1,5 +1,10 @@
 package no.nav.familie.ks.sak.kjerne.autovedtak
 
+import no.nav.familie.ks.sak.common.exception.Feil
+import no.nav.familie.ks.sak.kjerne.behandling.BehandlingService
+import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.VilkårsvurderingService
+import no.nav.familie.ks.sak.kjerne.fagsak.FagsakService
+import no.nav.familie.ks.sak.kjerne.fagsak.domene.FagsakStatus
 import no.nav.familie.ks.sak.statistikk.stønadsstatistikk.PubliserVedtakTask.Companion.TASK_STEP_TYPE
 import no.nav.familie.prosessering.AsyncTaskStep
 import no.nav.familie.prosessering.TaskStepBeskrivelse
@@ -15,21 +20,42 @@ import java.util.Properties
     maxAntallFeil = 1,
     settTilManuellOppfølgning = true,
 )
-class AutovedtakLovendringTask(
+class AutovedtakLovendringIkkeFremtidigOpphørTask(
     val autovedtakLovendringService: AutovedtakLovendringService,
+    val fagsakService: FagsakService,
+    val behandlingService: BehandlingService,
+    val vilkårsvurderingService: VilkårsvurderingService,
 ) : AsyncTaskStep {
     override fun doTask(task: Task) {
-        autovedtakLovendringService.revurderFagsak(fagsakId = task.payload.toLong())
+        val fagsakId = task.payload.toLong()
+        val fagsak = fagsakService.hentFagsak(fagsakId)
+        if (fagsak.status != FagsakStatus.LØPENDE) {
+            throw Feil("Fagsak $fagsakId er ikke løpende")
+        }
+
+        val aktivBehandling =
+            behandlingService.hentSisteBehandlingSomErIverksatt(fagsakId)
+                ?: throw Feil("Fant ingen aktiv behandling for fagsak $fagsakId")
+        val vilkårsvurdering =
+            vilkårsvurderingService.finnAktivVilkårsvurdering(aktivBehandling.id)
+                ?: throw Feil("Fant ingen vilkårsvurdering for behandling ${aktivBehandling.id}")
+
+        val vilkårResultaterPåBehandling = vilkårsvurdering.personResultater.flatMap { it.vilkårResultater }
+
+        val behandlingHarFremtidigOpphør = vilkårResultaterPåBehandling.any { it.søkerHarMeldtFraOmBarnehageplass == true }
+        check(behandlingHarFremtidigOpphør) { "Siste iverksatte behandling=${aktivBehandling.id} på fagsak=$fagsakId har fremtidig opphør." }
+
+        autovedtakLovendringService.revurderFagsak(fagsakId = fagsakId)
     }
 
     companion object {
-        val logger = LoggerFactory.getLogger(AutovedtakLovendringTask::class.java)
+        val logger = LoggerFactory.getLogger(AutovedtakLovendringIkkeFremtidigOpphørTask::class.java)
         const val TASK_STEP_TYPE = "autovedtakLovendring"
 
         fun opprettTask(
             fagsakId: Long,
-        ): Task {
-            return Task(
+        ): Task =
+            Task(
                 type = TASK_STEP_TYPE,
                 payload = fagsakId.toString(),
                 properties =
@@ -37,6 +63,5 @@ class AutovedtakLovendringTask(
                         this["fagsakId"] = fagsakId.toString()
                     },
             )
-        }
     }
 }
