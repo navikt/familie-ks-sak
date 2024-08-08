71e2dd2e6b757c2c9a9a2781a799608441bd3b3d
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-08-08 10:11:18 +0200

    Disable fremtidig opphør test

--- src/test/integrasjonstester/kotlin/no/nav/familie/ks/sak/kjerne/atuovedtak/AutovedtakLovendringTest.kt
+++ src/test/integrasjonstester/kotlin/no/nav/familie/ks/sak/kjerne/atuovedtak/AutovedtakLovendringTest.kt
@@ -43,6 +43,7 @@ import no.nav.familie.ks.sak.sikkerhet.SikkerhetContext
 import no.nav.familie.ks.sak.statistikk.saksstatistikk.SakStatistikkService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import java.math.BigDecimal
@@ -185,6 +186,7 @@ class AutovedtakLovendringTest(
     }
 
     @Test
+    @Disabled
     fun `automatisk revurdering av fagsak som har fremtidig opphør beholder fremtidig opphør og sender brev`() {
         // arrange
 
