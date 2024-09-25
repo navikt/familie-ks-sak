2a3b6fce80c8b52c03bedddf95028480a5f3a07d
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2024-09-25 12:26:09 +0200

    fiks fremmednøkler som heter noe annet i ks enn i ba
    Signed-off-by: FredrikP <57641333+fredrikpf@users.noreply.github.com>

--- src/main/resources/db/migration/V26__aktoerId_splitt_update_cascade.sql
+++ src/main/resources/db/migration/V26__aktoerId_splitt_update_cascade.sql
@@ -1,50 +1,36 @@
 -- personident
-ALTER TABLE personident DROP CONSTRAINT fk_personident;
+ALTER TABLE personident DROP CONSTRAINT personident_fk_aktoer_id_fkey;
 
 ALTER TABLE personident
-    ADD CONSTRAINT fk_personident
+    ADD CONSTRAINT personident_fk_aktoer_id_fkey
         FOREIGN KEY (fk_aktoer_id) references aktoer ON UPDATE CASCADE;
 
 -- person_resultat
-ALTER TABLE person_resultat DROP CONSTRAINT fk_person_resultat;
+ALTER TABLE person_resultat DROP CONSTRAINT person_resultat_fk_aktoer_id_fkey;
 
 ALTER TABLE person_resultat
-    ADD CONSTRAINT fk_person_resultat
+    ADD CONSTRAINT person_resultat_fk_aktoer_id_fkey
         FOREIGN KEY (fk_aktoer_id) references aktoer ON UPDATE CASCADE;
 
 -- po_person
-ALTER TABLE po_person DROP CONSTRAINT fk_po_person;
+ALTER TABLE po_person DROP CONSTRAINT po_person_fk_aktoer_id_fkey;
 
 ALTER TABLE po_person
-    ADD CONSTRAINT fk_po_person
+    ADD CONSTRAINT po_person_fk_aktoer_id_fkey
         FOREIGN KEY (fk_aktoer_id) references aktoer ON UPDATE CASCADE;
 
 -- fagsak
-ALTER TABLE fagsak DROP CONSTRAINT fagsak;
+ALTER TABLE fagsak DROP CONSTRAINT fagsak_fk_aktoer_id_fkey;
 
 ALTER TABLE fagsak
-    ADD CONSTRAINT fagsak
+    ADD CONSTRAINT fagsak_fk_aktoer_id_fkey
         FOREIGN KEY (fk_aktoer_id) references aktoer ON UPDATE CASCADE;
 
 -- andel_tilkjent_ytelse
-ALTER TABLE andel_tilkjent_ytelse DROP CONSTRAINT fk_andel_tilkjent_ytelse;
+ALTER TABLE andel_tilkjent_ytelse DROP CONSTRAINT andel_tilkjent_ytelse_fk_aktoer_id_fkey;
 
 ALTER TABLE andel_tilkjent_ytelse
-    ADD CONSTRAINT fk_andel_tilkjent_ytelse
-        FOREIGN KEY (fk_aktoer_id) references aktoer ON UPDATE CASCADE;
-
--- foedselshendelse_pre_lansering
-ALTER TABLE foedselshendelse_pre_lansering DROP CONSTRAINT fk_foedselshendelse_pre_lansering;
-
-ALTER TABLE foedselshendelse_pre_lansering
-    ADD CONSTRAINT fk_foedselshendelse_pre_lansering
-        FOREIGN KEY (fk_aktoer_id) references aktoer ON UPDATE CASCADE;
-
--- gr_periode_overgangsstonad
-ALTER TABLE gr_periode_overgangsstonad DROP CONSTRAINT fk_gr_periode_overgangsstonad;
-
-ALTER TABLE gr_periode_overgangsstonad
-    ADD CONSTRAINT fk_gr_periode_overgangsstonad
+    ADD CONSTRAINT andel_tilkjent_ytelse_fk_aktoer_id_fkey
         FOREIGN KEY (fk_aktoer_id) references aktoer ON UPDATE CASCADE;
 
 -- aktoer_til_kompetanse
