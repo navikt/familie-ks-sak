624ce6b58882992501e7ec6150c158459573752f
Thomas Alm <106926075+thoalm@users.noreply.github.com>
2024-10-07 12:15:39 +0200

    NAV-22651: Omdøpte enumer til store bokstaver (#880)
    ### 💰 Hva skal gjøres, og hvorfor?
    Omdøper for å følge enum standarden
    
    ### 🔎️ Er det noe spesielt du ønsker tilbakemelding om?
    Nei
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [ ] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    _Jeg har ikke skrevet tester fordi:_
    Ikke behov
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [x] Nei

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vilkårsvurdering/regelsett/lov2021/Graderingsforskjell.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vilkårsvurdering/regelsett/lov2021/Graderingsforskjell.kt
@@ -1,11 +1,11 @@
 package no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.regelsett.lov2021
 
 enum class Graderingsforskjell {
-    IngenUtbetalingGrunnetFullBarnehageplassTilØking,
-    Øking,
-    ØkingGrunnetSluttIBarnehage,
-    ReduksjonGårTilIngenUtbetaling,
-    Reduksjon,
-    Lik,
-    IngenUtbetalingGrunnetFørsteperiodeTilØking,
+    INGEN_UTBETALING_GRUNNET_FULL_BARNEHAGEPLASS_TIL_ØKNING,
+    ØKNING,
+    ØKNING_GRUNNET_SLUTT_I_BARNEHAGE,
+    REDUKSJON_GÅR_TIL_INGEN_UTBETALING,
+    REDUKSJON,
+    LIK,
+    INGEN_UTBETALING_GRUNNET_FØRSTE_PERIODE_TIL_ØKNING,
 }
