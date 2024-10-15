d32d2240b685d99bb05c381ff48cbf13afef57a2
Magnus Tønnessen <magnustonnessen@gmail.com>
2024-10-15 13:54:28 +0200

    Kopier kompensasjonAndel til ny behandling

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/RegistrerPersonGrunnlagSteg.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/RegistrerPersonGrunnlagSteg.kt
@@ -7,6 +7,7 @@ import no.nav.familie.ks.sak.kjerne.behandling.domene.BehandlingStatus
 import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.VilkårsvurderingService
 import no.nav.familie.ks.sak.kjerne.endretutbetaling.EndretUtbetalingAndelService
 import no.nav.familie.ks.sak.kjerne.eøs.EøsSkjemaerForNyBehandlingService
+import no.nav.familie.ks.sak.kjerne.kompensasjonsordning.KompensasjonAndelService
 import no.nav.familie.ks.sak.kjerne.personopplysninggrunnlag.PersonopplysningGrunnlagService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -19,6 +20,7 @@ class RegistrerPersonGrunnlagSteg(
     private val personopplysningGrunnlagService: PersonopplysningGrunnlagService,
     private val vilkårsvurderingService: VilkårsvurderingService,
     private val endretUtbetalingAndelService: EndretUtbetalingAndelService,
+    private val kompensasjonAndelService: KompensasjonAndelService,
     private val eøsSkjemaerForNyBehandlingService: EøsSkjemaerForNyBehandlingService,
 ) : IBehandlingSteg {
     override fun getBehandlingssteg(): BehandlingSteg = BehandlingSteg.REGISTRERE_PERSONGRUNNLAG
@@ -38,6 +40,11 @@ class RegistrerPersonGrunnlagSteg(
 
         if (sisteVedtattBehandling != null) {
             endretUtbetalingAndelService.kopierEndretUtbetalingAndelFraForrigeBehandling(
+                behandling,
+                sisteVedtattBehandling,
+            )
+
+            kompensasjonAndelService.kopierKompensasjonAndelFraForrigeBehandling(
                 behandling,
                 sisteVedtattBehandling,
             )
