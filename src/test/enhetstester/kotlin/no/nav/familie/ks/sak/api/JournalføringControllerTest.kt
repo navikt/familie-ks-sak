822be7c33b4caf330b3ee145b57b66d5bf3ac9d3
bragejahren <70642183+bragejahren@users.noreply.github.com>
2024-10-24 09:31:19 +0200

    Chore: Fjerner bruk av toggle ved henting av journalposter for bruker (#911)
    ### 💰 Hva skal gjøres, og hvorfor?
    Vi har endret returtype ved henting av journalposter for bruker. I
    overgangen til ny type brukte vi en toggle for å bestemme om vi skulle
    bruke gammel eller ny type. Nå er vi helt over på den nye typen
    `TilgangsstyrteJournalposter` og kan trygt fjerne toggle.
    
    ### ✅ Checklist
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer.
    - [ ] Jeg har skrevet tester.
    
    Ingen nye tester da jeg bare har fjernet kode.
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [x] Nei

--- src/test/enhetstester/kotlin/no/nav/familie/ks/sak/api/JournalføringControllerTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/api/JournalføringControllerTest.kt
@@ -3,13 +3,9 @@ package no.nav.familie.ks.sak.api
 import io.mockk.every
 import io.mockk.mockk
 import no.nav.familie.kontrakter.felles.PersonIdent
-import no.nav.familie.kontrakter.felles.journalpost.Journalpost
-import no.nav.familie.kontrakter.felles.journalpost.TilgangsstyrtJournalpost
-import no.nav.familie.ks.sak.config.featureToggle.FeatureToggleConfig
 import no.nav.familie.ks.sak.integrasjon.journalføring.InnkommendeJournalføringService
 import no.nav.familie.ks.sak.integrasjon.lagTilgangsstyrtJournalpost
 import no.nav.familie.ks.sak.sikkerhet.TilgangService
-import no.nav.familie.unleash.UnleashService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
@@ -18,47 +14,21 @@ import org.springframework.http.HttpStatus
 class JournalføringControllerTest {
     private val innkommendeJournalføringService: InnkommendeJournalføringService = mockk()
     private val tilgangService: TilgangService = mockk()
-    private val unleashService: UnleashService = mockk()
     private val journalføringController: JournalføringController =
         JournalføringController(
             innkommendeJournalføringService = innkommendeJournalføringService,
             tilgangService = tilgangService,
-            unleashService = unleashService,
         )
 
     @Nested
     inner class HentJournalposterForBruker {
         @Test
-        fun `skal returnere liste av journalposter når toggle er av`() {
-            // Arrange
-            val personIdent = PersonIdent("123")
-            val journalpostId = "1"
-
-            every { innkommendeJournalføringService.hentJournalposterForBruker(personIdent.ident) } returns listOf(lagTilgangsstyrtJournalpost(personIdent.ident, journalpostId = journalpostId))
-            every { unleashService.isEnabled(FeatureToggleConfig.BRUK_NYTT_RETUR_OBJEKT_FOR_JOURNALPOSTER, false) } returns false
-
-            // Act
-            val responseEntity = journalføringController.hentJournalposterForBruker(personIdentBody = personIdent)
-
-            // Assert
-            assertThat(responseEntity.statusCode).isEqualTo(HttpStatus.OK)
-            assertThat(responseEntity.body).isNotNull
-            assertThat(responseEntity.body!!.data).isNotNull
-            val journalposter = responseEntity.body!!.data!!
-            assertThat(journalposter).hasSize(1)
-            assertThat(journalposter.single()).isInstanceOf(Journalpost::class.java)
-            val journalpost = journalposter.single() as Journalpost
-            assertThat(journalpost.journalpostId).isEqualTo("1")
-        }
-
-        @Test
-        fun `skal returnere liste av tilgangsstyrte journalposter når toggle er på`() {
+        fun `skal returnere liste av tilgangsstyrte journalposter`() {
             // Arrange
             val personIdent = PersonIdent("123")
             val journalpostId = "1"
 
             every { innkommendeJournalføringService.hentJournalposterForBruker(personIdent.ident) } returns listOf(lagTilgangsstyrtJournalpost(personIdent.ident, journalpostId = journalpostId, harTilgang = true))
-            every { unleashService.isEnabled(FeatureToggleConfig.BRUK_NYTT_RETUR_OBJEKT_FOR_JOURNALPOSTER, false) } returns true
 
             // Act
             val responseEntity = journalføringController.hentJournalposterForBruker(personIdentBody = personIdent)
@@ -69,8 +39,7 @@ class JournalføringControllerTest {
             assertThat(responseEntity.body!!.data).isNotNull
             val journalposter = responseEntity.body!!.data!!
             assertThat(journalposter).hasSize(1)
-            assertThat(journalposter.single()).isInstanceOf(TilgangsstyrtJournalpost::class.java)
-            val tilgangsstyrtJournalpost = journalposter.single() as TilgangsstyrtJournalpost
+            val tilgangsstyrtJournalpost = journalposter.single()
             assertThat(tilgangsstyrtJournalpost.journalpost.journalpostId).isEqualTo("1")
             assertThat(tilgangsstyrtJournalpost.harTilgang).isTrue
         }
