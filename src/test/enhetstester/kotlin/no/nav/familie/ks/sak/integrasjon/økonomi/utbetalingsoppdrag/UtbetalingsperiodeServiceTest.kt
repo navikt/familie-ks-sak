a232b8f567aadc6a0a42847b74110d25cb5aeb8c
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2024-11-06 13:30:22 +0100

    legg til tester for periodeOffset for overgangsordningAndeler
    Signed-off-by: FredrikP <57641333+fredrikpf@users.noreply.github.com>

--- src/test/enhetstester/kotlin/no/nav/familie/ks/sak/integrasjon/økonomi/utbetalingsoppdrag/UtbetalingsperiodeServiceTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/integrasjon/økonomi/utbetalingsoppdrag/UtbetalingsperiodeServiceTest.kt
@@ -462,6 +462,123 @@ internal class UtbetalingsperiodeServiceTest {
     }
 
     @Test
+    fun `genererUtbetalingsoppdrag - skal generere nytt utbetalingsoppdrag og oppdatere andeler med offset for 2 personer og tidligere behandling for overgangsordning`() {
+        val vedtak = lagVedtak()
+        val tilkjentYtelse = lagInitiellTilkjentYtelse(vedtak.behandling)
+        val person = tilfeldigPerson()
+        val barn = tilfeldigPerson()
+        tilkjentYtelse.andelerTilkjentYtelse.addAll(
+            mutableSetOf(
+                lagAndelTilkjentYtelse(
+                    id = 6,
+                    fom = YearMonth.of(2024, 1),
+                    tom = YearMonth.of(2024, 8),
+                    beløp = 250,
+                    person = person,
+                ),
+                lagAndelTilkjentYtelse(
+                    id = 7,
+                    fom = YearMonth.of(2024, 1),
+                    tom = YearMonth.of(2024, 10),
+                    beløp = 350,
+                    person = barn,
+                ),
+                lagAndelTilkjentYtelse(
+                    id = 8,
+                    fom = YearMonth.of(2024, 11),
+                    tom = YearMonth.of(2025, 2),
+                    beløp = 350,
+                    person = barn,
+                    ytelseType = YtelseType.OVERGANGSORDNING,
+                ),
+            ),
+        )
+
+        val forrigeBehandling = lagBehandling()
+        val forrigeTilkjentYtelse = lagInitiellTilkjentYtelse(forrigeBehandling)
+        forrigeTilkjentYtelse.andelerTilkjentYtelse.addAll(
+            mutableSetOf(
+                lagAndelTilkjentYtelse(
+                    id = 1,
+                    fom = YearMonth.of(2024, 1),
+                    tom = YearMonth.of(2024, 3),
+                    beløp = 250,
+                    person = person,
+                    periodeIdOffset = 0L,
+                ),
+                lagAndelTilkjentYtelse(
+                    id = 2,
+                    fom = YearMonth.of(2024, 4),
+                    tom = YearMonth.of(2024, 8),
+                    beløp = 350,
+                    person = person,
+                    periodeIdOffset = 1L,
+                    forrigeperiodeIdOffset = 0L,
+                    ytelseType = YtelseType.OVERGANGSORDNING,
+                ),
+                lagAndelTilkjentYtelse(
+                    id = 3,
+                    fom = YearMonth.of(2024, 1),
+                    tom = YearMonth.of(2024, 10),
+                    beløp = 250,
+                    person = barn,
+                    periodeIdOffset = 2L,
+                ),
+                lagAndelTilkjentYtelse(
+                    id = 4,
+                    fom = YearMonth.of(2024, 11),
+                    tom = YearMonth.of(2024, 11),
+                    beløp = 350,
+                    person = barn,
+                    periodeIdOffset = 3L,
+                    forrigeperiodeIdOffset = 2L,
+                    ytelseType = YtelseType.OVERGANGSORDNING,
+                ),
+                lagAndelTilkjentYtelse(
+                    id = 5,
+                    fom = YearMonth.of(2024, 12),
+                    tom = YearMonth.of(2025, 2),
+                    beløp = 250,
+                    person = barn,
+                    periodeIdOffset = null, // Bare første overgangsordningandel per person får offset
+                    forrigeperiodeIdOffset = null,
+                    ytelseType = YtelseType.OVERGANGSORDNING,
+                ),
+            ),
+        )
+        val tilkjentYtelseSlot = slot<TilkjentYtelse>()
+        setUpMocks(
+            behandling = vedtak.behandling,
+            tilkjentYtelse = tilkjentYtelse,
+            tilkjentYtelseSlot = tilkjentYtelseSlot,
+            forrigeTilkjentYtelse = forrigeTilkjentYtelse,
+        )
+
+        val beregnetUtbetalingsoppdrag =
+            utbetalingsoppdragService.genererUtbetalingsoppdragOgOppdaterTilkjentYtelse(
+                vedtak = vedtak,
+                "abc123",
+            )
+
+        val lagredeAndeler = tilkjentYtelseSlot.captured.andelerTilkjentYtelse
+
+        verify(exactly = 1) { tilkjentYtelseRepository.save(any()) }
+
+        validerBeregnetUtbetalingsoppdragOgAndeler(
+            beregnetUtbetalingsoppdrag = beregnetUtbetalingsoppdrag,
+            andelerTilkjentYtelse = lagredeAndeler,
+            forventetAntallAndeler = 3,
+            forventetAntallUtbetalingsperioder = 3,
+            forventedeOffsets =
+                listOf(
+                    Pair(4L, 1L),
+                    Pair(5L, 3L),
+                    Pair(6L, 5L),
+                ),
+        )
+    }
+
+    @Test
     fun `genererUtbetalingsoppdrag - skal generere nytt utbetalingsoppdrag for simulering med en eksisterende kjede og en ny kjede`() {
         val vedtak = lagVedtak()
         val tilkjentYtelse = lagInitiellTilkjentYtelse(vedtak.behandling)
@@ -631,9 +748,9 @@ internal class UtbetalingsperiodeServiceTest {
         forventetAntallUtbetalingsperioder: Int,
     ) {
         assertThat(beregnetUtbetalingsoppdrag.utbetalingsoppdrag).isNotNull
-        assertThat(beregnetUtbetalingsoppdrag.utbetalingsoppdrag.utbetalingsperiode.size).isEqualTo(
-            forventetAntallUtbetalingsperioder,
-        )
+        assertThat(beregnetUtbetalingsoppdrag.utbetalingsoppdrag.utbetalingsperiode.size)
+            .`as`("Feil antall beregnede utbetalingsperioder: ${beregnetUtbetalingsoppdrag.utbetalingsoppdrag.utbetalingsperiode}")
+            .isEqualTo(forventetAntallUtbetalingsperioder)
 
         assertThat(beregnetUtbetalingsoppdrag.andeler).isNotEmpty
         assertThat(beregnetUtbetalingsoppdrag.andeler.size).isEqualTo(forventetAntallAndeler)
@@ -693,6 +810,6 @@ internal class UtbetalingsperiodeServiceTest {
 
         every { tilkjentYtelseRepository.save(capture(tilkjentYtelseSlot)) } returns mockk()
 
-        every { unleashService.isEnabled(any()) } returns false
+        every { unleashService.isEnabled(any()) } returns true
     }
 }
