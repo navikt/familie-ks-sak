b21f2b1432952b2c22ba895a2739e3c6f2a6a1ff
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2024-11-07 13:05:51 +0100

    Legger til avslag-allerede-utbetalt-begrunnelser api (wip)

--- src/main/kotlin/no/nav/familie/ks/sak/api/EndretUtbetalingAndelController.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/api/EndretUtbetalingAndelController.kt
@@ -3,9 +3,19 @@ package no.nav.familie.ks.sak.api
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.ks.sak.api.dto.BehandlingResponsDto
 import no.nav.familie.ks.sak.api.dto.EndretUtbetalingAndelRequestDto
+import no.nav.familie.ks.sak.api.dto.VedtakBegrunnelseTilknyttetVilkårResponseDto
 import no.nav.familie.ks.sak.config.BehandlerRolle
+import no.nav.familie.ks.sak.integrasjon.sanity.SanityService
+import no.nav.familie.ks.sak.integrasjon.sanity.domene.SanityBegrunnelse
 import no.nav.familie.ks.sak.kjerne.behandling.BehandlingService
 import no.nav.familie.ks.sak.kjerne.behandling.TilbakestillBehandlingService
+import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.standardbegrunnelserTilNedtrekksmenytekster
+import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.vedtakBegrunnelseTilRestVedtakBegrunnelseTilknyttetVilkår
+import no.nav.familie.ks.sak.kjerne.brev.begrunnelser.BegrunnelseType
+import no.nav.familie.ks.sak.kjerne.brev.begrunnelser.EØSBegrunnelse
+import no.nav.familie.ks.sak.kjerne.brev.begrunnelser.IBegrunnelse
+import no.nav.familie.ks.sak.kjerne.brev.begrunnelser.NasjonalEllerFellesBegrunnelse
+import no.nav.familie.ks.sak.kjerne.brev.begrunnelser.tilSanityBegrunnelse
 import no.nav.familie.ks.sak.kjerne.endretutbetaling.EndretUtbetalingAndelService
 import no.nav.familie.ks.sak.sikkerhet.AuditLoggerEvent
 import no.nav.familie.ks.sak.sikkerhet.TilgangService
@@ -13,6 +23,7 @@ import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
@@ -29,7 +40,8 @@ class EndretUtbetalingAndelController(
     private val tilgangService: TilgangService,
     private val behandlingService: BehandlingService,
     private val tilbakestillBehandlingService: TilbakestillBehandlingService,
-) {
+    private val sanityService: SanityService,
+    ) {
     @PutMapping(path = ["/{behandlingId}/{endretUtbetalingAndelId}"])
     fun oppdaterEndretUtbetalingAndelOgOppdaterTilkjentYtelse(
         @PathVariable behandlingId: Long,
@@ -101,5 +113,52 @@ class EndretUtbetalingAndelController(
             .tilbakestillBehandlingTilBehandlingsresultat(behandlingId = behandling.id)
 
         return ResponseEntity.ok(Ressurs.success(behandlingService.lagBehandlingRespons(behandlingId = behandling.id)))
+    }
+
+    fun vedtakBegrunnelseTilRestVedtakBegrunnelseTilknyttetVilkår(
+        sanityBegrunnelser: List<SanityBegrunnelse>,
+        vedtakBegrunnelse: IBegrunnelse,
+    ): List<VedtakBegrunnelseTilknyttetVilkårResponseDto> {
+        val sanityBegrunnelse = vedtakBegrunnelse.tilSanityBegrunnelse(sanityBegrunnelser) ?: return emptyList()
+        val visningsnavn = sanityBegrunnelse.navnISystem
+
+        return if (sanityBegrunnelse.vilkår.isEmpty()) {
+            listOf(
+                VedtakBegrunnelseTilknyttetVilkårResponseDto(
+                    id = vedtakBegrunnelse,
+                    navn = visningsnavn,
+                    vilkår = null,
+                ),
+            )
+        } else {
+            sanityBegrunnelse.vilkår.map {
+                VedtakBegrunnelseTilknyttetVilkårResponseDto(
+                    id = vedtakBegrunnelse,
+                    navn = visningsnavn,
+                    vilkår = it,
+                )
+            }
+        }
+    }
+
+    fun standardbegrunnelserTilNedtrekksmenytekster(sanityBegrunnelser: List<SanityBegrunnelse>): List<Any> =
+        (NasjonalEllerFellesBegrunnelse.entries + EØSBegrunnelse.entries)
+            .filter {it.begrunnelseType == BegrunnelseType.AVSLAG}
+            .mapValues { begrunnelseGruppe ->
+                begrunnelseGruppe.value
+                    .flatMap { avslagBegrunnelse ->
+                        vedtakBegrunnelseTilRestVedtakBegrunnelseTilknyttetVilkår(
+                            sanityBegrunnelser,
+                            avslagBegrunnelse,
+                        )
+                    }
+            }
+
+    @GetMapping(path = ["/avslag-allerede-utbetalt-begrunnelser"])
+    fun hentBegrunnelser(): ResponseEntity<Ressurs<String>> {
+        //
+        val sanityBegrunnelser = sanityService.hentSanityBegrunnelser()
+        val mappingTilSanityTekster = standardbegrunnelserTilNedtrekksmenytekster(sanityBegrunnelser)
+        return ResponseEntity.ok(Ressurs.success("yolo"));
     }
 }
