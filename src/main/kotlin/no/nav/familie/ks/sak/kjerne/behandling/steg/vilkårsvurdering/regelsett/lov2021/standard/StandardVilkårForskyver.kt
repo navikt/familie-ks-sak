73ca504a92f22631b0c2d85fad5faf3c76340268
Thomas Alm <106926075+thoalm@users.noreply.github.com>
2024-11-15 09:06:49 +0100

    Nav 22999 refaktorer forskyvningslogikk 2021 (#933)
    ### 💰 Hva skal gjøres, og hvorfor?
    Favro:
    https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-22999
    
    Splitter opp logikk i flere metoder og plasserer dem i passende filer.
    Dette er for å gjøre logikken mer oversiktelig og mer testbar.
    
    ### 🔎️ Er det noe spesielt du ønsker tilbakemelding om?
    Har refaktorert litt kode for å gjøre det lettere å lese/test.
    Funksjonelt skal det i teorien fungere som før. Om du ser at det er
    forskjell i logikk så si gjerne ifra da det ikke er hensikten.
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [x] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [x] Nei
    
    ---------
    
    Co-authored-by: Uy Nguyen <110383605+UyQuangNguyen@users.noreply.github.com>

---
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vilkårsvurdering/regelsett/lov2021/standard/StandardVilkårForskyver.kt
@@ -0,0 +1,33 @@
+package no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.regelsett.lov2021.standard
+
+import no.nav.familie.ks.sak.common.tidslinje.Periode
+import no.nav.familie.ks.sak.common.util.TIDENES_ENDE
+import no.nav.familie.ks.sak.common.util.TIDENES_MORGEN
+import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.domene.Vilkår
+import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.domene.VilkårResultat
+import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.regelsett.mapTilTilknyttetVilkårResultater
+
+private val VILKÅR_SOM_IKKE_SKAL_FORSKYVES_ETTER_STANDARD_LOGIKK =
+    setOf(
+        Vilkår.BARNEHAGEPLASS,
+    )
+
+fun forskyvStandardVilkår(
+    vilkårResultater: List<VilkårResultat>,
+): List<Periode<VilkårResultat>> {
+    validerVilkårResultater(vilkårResultater)
+    return vilkårResultater
+        .filter { it.erOppfylt() || it.erIkkeAktuelt() }
+        .sortedBy { it.periodeFom }
+        .mapTilTilknyttetVilkårResultater()
+        .map { Periode(verdi = it.gjeldende, fom = forskyvFom(it.gjeldende.periodeFom), tom = forskyvTom(it)) }
+        .filter { (it.fom ?: TIDENES_MORGEN).isBefore(it.tom ?: TIDENES_ENDE) }
+}
+
+private fun validerVilkårResultater(vilkårResultater: List<VilkårResultat>) {
+    vilkårResultater.map { it.vilkårType }.distinct().forEach {
+        if (it in VILKÅR_SOM_IKKE_SKAL_FORSKYVES_ETTER_STANDARD_LOGIKK) {
+            throw IllegalArgumentException("Vilkårtype $it skal ikke forskyves etter standard logikk")
+        }
+    }
+}
