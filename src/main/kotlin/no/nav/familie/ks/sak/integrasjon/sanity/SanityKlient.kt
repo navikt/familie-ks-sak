5c7ccde1190cfc76a94cd6b60ddccf2aa02654ab
Stig Helge Strøm <stig.helge.strom@nav.no>
2024-11-25 08:52:05 +0100

    Lagrer siste sanitybegrunnelser i inmemory cache

--- src/main/kotlin/no/nav/familie/ks/sak/integrasjon/sanity/SanityKlient.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/integrasjon/sanity/SanityKlient.kt
@@ -1,10 +1,13 @@
 package no.nav.familie.ks.sak.integrasjon.sanity
 
 import no.nav.familie.http.client.AbstractRestClient
+import no.nav.familie.ks.sak.integrasjon.familieintegrasjon.IntegrasjonClient.Companion.RETRY_BACKOFF_5000MS
 import no.nav.familie.ks.sak.integrasjon.kallEksternTjeneste
 import no.nav.familie.ks.sak.integrasjon.sanity.domene.SanityBegrunnelse
 import no.nav.familie.ks.sak.integrasjon.sanity.domene.SanityBegrunnelserResponsDto
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestOperations
 import java.net.URI
@@ -15,6 +18,11 @@ class SanityKlient(
     @Value("\${SANITY_BASE_URL}") private val sanityBaseUrl: String,
     restOperations: RestOperations,
 ) : AbstractRestClient(restOperations, "sanity") {
+    @Retryable(
+        value = [Exception::class],
+        maxAttempts = 3,
+        backoff = Backoff(delayExpression = RETRY_BACKOFF_5000MS),
+    )
     fun hentBegrunnelser(datasett: String = "ks-brev"): List<SanityBegrunnelse> {
         val uri = lagHentUri(datasett, HENT_BEGRUNNELSER)
 
