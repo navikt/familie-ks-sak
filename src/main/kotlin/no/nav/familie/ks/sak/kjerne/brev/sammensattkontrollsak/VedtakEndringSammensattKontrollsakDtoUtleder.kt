38edf72e9aef8e2ca9c2a69afee132bfb33be442
thoalm <thoalm@outlook.com>
2024-11-27 15:40:32 +0100

    NAV-22215: Flytter sammensatt kontrollsak til en egen pakke og gjør det om fra @Service til @Component

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/brev/sammensattkontrollsak/VedtakEndringSammensattKontrollsakDtoUtleder.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/brev/sammensattkontrollsak/VedtakEndringSammensattKontrollsakDtoUtleder.kt
@@ -1,32 +1,35 @@
-package no.nav.familie.ks.sak.kjerne.brev
+package no.nav.familie.ks.sak.kjerne.brev.sammensattkontrollsak
 
 import no.nav.familie.ks.sak.kjerne.behandling.steg.simulering.SimuleringService
 import no.nav.familie.ks.sak.kjerne.behandling.steg.vedtak.domene.Vedtak
 import no.nav.familie.ks.sak.kjerne.behandling.steg.vedtak.feilutbetaltvaluta.FeilutbetaltValutaService
 import no.nav.familie.ks.sak.kjerne.behandling.steg.vedtak.sammensattkontrollsak.SammensattKontrollsak
 import no.nav.familie.ks.sak.kjerne.behandling.steg.vedtak.vedtaksperiode.VedtaksperiodeService
+import no.nav.familie.ks.sak.kjerne.brev.BrevPeriodeService
+import no.nav.familie.ks.sak.kjerne.brev.EtterbetalingService
+import no.nav.familie.ks.sak.kjerne.brev.SøkersMeldepliktService
 import no.nav.familie.ks.sak.kjerne.brev.domene.maler.FeilutbetaltValuta
 import no.nav.familie.ks.sak.kjerne.brev.domene.maler.VedtakEndringSammensattKontrollsakDto
 import org.slf4j.LoggerFactory
-import org.springframework.stereotype.Service
+import org.springframework.stereotype.Component
 
-@Service
-class OpprettVedtakEndringSammensattKontrollsakDtoService(
+@Component
+class VedtakEndringSammensattKontrollsakDtoUtleder(
     private val søkersMeldepliktService: SøkersMeldepliktService,
-    private val opprettVedtakFellesfelterSammensattKontrollsakDtoService: OpprettVedtakFellesfelterSammensattKontrollsakDtoService,
+    private val vedtakFellesfelterSammensattKontrollsakDtoUtleder: VedtakFellesfelterSammensattKontrollsakDtoUtleder,
     private val etterbetalingService: EtterbetalingService,
     private val simuleringService: SimuleringService,
     private val vedtaksperiodeService: VedtaksperiodeService,
     private val feilutbetaltValutaService: FeilutbetaltValutaService,
     private val brevPeriodeService: BrevPeriodeService,
 ) {
-    private val logger = LoggerFactory.getLogger(OpprettVedtakEndringSammensattKontrollsakDtoService::class.java)
+    private val logger = LoggerFactory.getLogger(VedtakEndringSammensattKontrollsakDtoUtleder::class.java)
 
-    fun opprett(
+    fun utled(
         vedtak: Vedtak,
         sammensattKontrollsak: SammensattKontrollsak,
     ): VedtakEndringSammensattKontrollsakDto {
-        logger.debug("Oppretter ${VedtakEndringSammensattKontrollsakDto::class.simpleName} for vedtak ${vedtak.id}")
+        logger.debug("Utleder ${VedtakEndringSammensattKontrollsakDto::class.simpleName} for vedtak ${vedtak.id}")
 
         val søkerHarMeldtFraOmBarnehagePlass =
             søkersMeldepliktService.harSøkerMeldtFraOmBarnehagePlass(
@@ -42,7 +45,7 @@ class OpprettVedtakEndringSammensattKontrollsakDtoService(
                 }
 
         return VedtakEndringSammensattKontrollsakDto(
-            vedtakFellesfelter = opprettVedtakFellesfelterSammensattKontrollsakDtoService.opprett(vedtak = vedtak, sammensattKontrollsak = sammensattKontrollsak),
+            vedtakFellesfelter = vedtakFellesfelterSammensattKontrollsakDtoUtleder.utled(vedtak = vedtak, sammensattKontrollsak = sammensattKontrollsak),
             etterbetaling = etterbetalingService.hentEtterbetaling(vedtak = vedtak),
             erKlage = vedtak.behandling.erKlage(),
             erFeilutbetalingPåBehandling = simuleringService.erFeilutbetalingPåBehandling(behandlingId = vedtak.behandling.id),
