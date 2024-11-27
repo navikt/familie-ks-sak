e3c8402d1ddb1fccac06252bb620fe5f59e1e951
thoalm <thoalm@outlook.com>
2024-11-27 16:23:26 +0100

    NAV-22651: Kjørte ktlint

--- src/test/enhetstester/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vilkårsvurdering/regelsett/lov2024/standard/TomForskyverKtTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vilkårsvurdering/regelsett/lov2024/standard/TomForskyverKtTest.kt
@@ -1,14 +1,10 @@
 package no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.regelsett.lov2024.standard
 
-import no.nav.familie.ks.sak.data.lagVilkårResultat
-import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.domene.Vilkår
-import no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.regelsett.TilknyttetVilkårResultater
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
 class TomForskyverKtTest {
-
     @Test
     fun `skal returnere null når input dato er null`() {
         // Act
