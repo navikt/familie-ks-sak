e3c18cbec1e38c0076d8c9c7b15a53f53bf3c7fa
Uy Nguyen <uy.quang.nguyen@nav.no>
2024-11-29 14:04:55 +0100

    Flytt endringer fra en annen branch grunnet signatur feil

---
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/integrasjon/sanity/domene/SanityBegrunnelseTest.kt
@@ -0,0 +1,26 @@
+package no.nav.familie.ks.sak.integrasjon.sanity.domene
+
+import no.nav.familie.ks.sak.data.lagSanityBegrunnelse
+import no.nav.familie.ks.sak.kjerne.brev.begrunnelser.NasjonalEllerFellesBegrunnelse
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class SanityBegrunnelseTest {
+    @Test
+    fun `erOvergangsordningBegrunnelse skal returnere true hvis begrunnelse er en overgangsordning begrunnelse`() {
+        // Arrange
+        val sanityBegrunnelse = lagSanityBegrunnelse(apiNavn = NasjonalEllerFellesBegrunnelse.INNVILGET_OVERGANGSORDNING.sanityApiNavn)
+
+        // Assert
+        assertThat(sanityBegrunnelse.erOvergangsordningBegrunnelse()).isTrue()
+    }
+
+    @Test
+    fun `erOvergangsordningBegrunnelse skal returnere false hvis begrunnelse ikke er en overgangsordning begrunnelse`() {
+        // Arrange
+        val sanityBegrunnelse = lagSanityBegrunnelse(apiNavn = NasjonalEllerFellesBegrunnelse.INNVILGET_IKKE_BARNEHAGE.sanityApiNavn)
+
+        // Assert
+        assertThat(sanityBegrunnelse.erOvergangsordningBegrunnelse()).isFalse()
+    }
+}
