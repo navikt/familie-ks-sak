3cb427150966913a2cced39be11bd657fd4ee80a
Magnus Tønnessen <magnus.tonnessen@nav.no>
2024-12-02 16:35:29 +0100

    Legg til toggle for å stoppe godkjenning overgangsordningbehandlinger (#975)
    ### 💰 Hva skal gjøres, og hvorfor?
    
    Det skal ikke være mulig å totrinskontrollere behandlinger med årsak
    overgangsordning ennå
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [ ] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    _Jeg har ikke skrevet tester fordi:_
    Det haster å få den ut. Testet manuelt lokalt
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [ ] Nei

--- src/main/kotlin/no/nav/familie/ks/sak/config/featureToggle/FeatureToggleConfig.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/config/featureToggle/FeatureToggleConfig.kt
@@ -9,6 +9,7 @@ class FeatureToggleConfig {
         const val KAN_OPPRETTE_OG_ENDRE_SAMMENSATTE_KONTROLLSAKER = "familie-ks-sak.kan-opprette-og-endre-sammensatte-kontrollsaker"
         const val KAN_KJORE_LOVENDRING_FLERE_GANGER = "familie-ks-sak.kan-kjore-lovendring-flere-ganger"
         const val OVERGANGSORDNING = "familie-ks-sak.overgangsordning"
+        const val GODKJENNE_OVERGANGSORDNING = "familie-ks-sak.godkjenne-overgangsordning"
 
         // Ikke operasjonelle
         const val OPPRETT_SAK_PÅ_RIKTIG_ENHET_OG_SAKSBEHANDLER = "familie-ba-ks-sak.opprett-sak-paa-riktig-enhet-og-saksbehandler"
