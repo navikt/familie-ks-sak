c0d391921f0d370e2d0fd0b47c7bc50ec3618455
Uy Nguyen <uy.quang.nguyen@nav.no>
2024-12-08 01:39:05 +0100

    Vi fjerner tom dato på avslagsperioder som har samme fom som seneste vedtaksperiode, vi fjerner opphørsperioder som overlapper med avslagsperiode

--- src/test/enhetstester/kotlin/no/nav/familie/ks/sak/cucumber/StepDefinition.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/cucumber/StepDefinition.kt
@@ -164,7 +164,7 @@ class StepDefinition {
     }
 
     /**
-     * Mulige verdier: | AktørId | Fra dato | Til dato | BehandlingId |  Årsak | Prosent | Søknadstidspunkt | Avtaletidspunkt delt bosted |
+     * Mulige verdier: | AktørId | Fra dato | Til dato | BehandlingId |  Årsak | Prosent | Søknadstidspunkt | Avtaletidspunkt delt bosted | Er eksplisitt avslag |
      */
     @Og("følgende endrede utbetalinger")
     fun `følgende endrede utbetalinger`(
