1a319cfb33f86d42d03047a8bb8cf89fd97994d5
Uy Nguyen <110383605+UyQuangNguyen@users.noreply.github.com>
2024-12-17 09:46:10 +0100

    Fikser avslagsperiode som har feil fom og i tillegg feil begrunnelse favrokort (#999)
    https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-23696
    
    Det er to feil i denne saken.
    Den første er at det finnes både opphørsperiode og avslagsperiode for
    samme periode.
    
    Det andre er at avslagsperioden er feil. Den skal starte fra og med
    måneden etter, men bare for akkurat dette tilfelle. Endrer derfor
    logikken slik at vi også ser på andelene før vi bestemmer oss om
    opphøret er denne måned eller måneden etter.

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vedtak/vedtaksperiode/VedtaksperiodeService.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vedtak/vedtaksperiode/VedtaksperiodeService.kt
@@ -541,11 +541,23 @@ class VedtaksperiodeService(
                 "Fant ikke vilkårsvurdering for behandling $behandlingId ved generering av avslagsperioder",
             )
 
+        val andelerTilkjentYtelse = andelerTilkjentYtelseOgEndreteUtbetalingerService.finnAndelerTilkjentYtelseMedEndreteUtbetalinger(behandlingId)
+
         val periodegrupperteAvslagsvilkår: Map<NullablePeriode, List<VilkårResultat>> =
             vilkårsvurdering.personResultater
                 .flatMap { it.vilkårResultater }
                 .filter { it.erEksplisittAvslagPåSøknad == true }
-                .groupBy { NullablePeriode(it.periodeFom, it.periodeTom) }
+                .groupBy {
+                    val finnesAndelForPersonSomSlutterSammeMånedSomFomIVilkårResultat =
+                        andelerTilkjentYtelse.any { andel ->
+                            andel.aktør == it.personResultat?.aktør && andel.stønadTom == it.periodeFom?.toYearMonth()
+                        }
+                    if (finnesAndelForPersonSomSlutterSammeMånedSomFomIVilkårResultat) {
+                        NullablePeriode(it.periodeFom?.plusMonths(1), it.periodeTom)
+                    } else {
+                        NullablePeriode(it.periodeFom, it.periodeTom)
+                    }
+                }
 
         val avslagsperioder =
             periodegrupperteAvslagsvilkår
