9eb60cac6cf3811b1b5066d95c3a95a4e034ef21
Uy Nguyen <uy.quang.nguyen@nav.no>
2025-01-13 11:42:45 +0100

    Opphørsbegrunnelse for overgangsordning skal også gi hjemler om overgangsordning

--- src/test/enhetstester/kotlin/no/nav/familie/ks/sak/integrasjon/sanity/domene/SanityBegrunnelseTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/integrasjon/sanity/domene/SanityBegrunnelseTest.kt
@@ -8,8 +8,17 @@ import org.junit.jupiter.api.Test
 class SanityBegrunnelseTest {
     @Test
     fun `erOvergangsordningBegrunnelse skal returnere true hvis begrunnelse er en overgangsordning begrunnelse`() {
-        // Arrange
+        // Arrange && Act
         val sanityBegrunnelse = lagSanityBegrunnelse(apiNavn = NasjonalEllerFellesBegrunnelse.INNVILGET_OVERGANGSORDNING.sanityApiNavn)
+
+        // Assert
+        assertThat(sanityBegrunnelse.erOvergangsordningBegrunnelse()).isTrue()
+    }
+
+    @Test
+    fun `erOvergangsordningBegrunnelse skal returnere true hvis begrunnelse er en opphør overgangsordning begrunnelse`() {
+        // Arrange && Act
+        val sanityBegrunnelse = lagSanityBegrunnelse(apiNavn = NasjonalEllerFellesBegrunnelse.OPPHØR_OVERGANGSORDNING_OPPHØR.sanityApiNavn)
 
         // Assert
         assertThat(sanityBegrunnelse.erOvergangsordningBegrunnelse()).isTrue()
