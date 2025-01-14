c71707500a3ac6b58251bd5f26b038550224381a
Magnus Tønnessen <magnus.tonnessen@nav.no>
2025-01-14 14:17:20 +0100

    Bare se på personer fra forrige behandling når vi utleder endringsresultat (#1026)
    ### 💰 Hva skal gjøres, og hvorfor?
    
    Favro: NAV-23824
    
    Når vi utleder endringsresultat for en behandling, er det feil å se på
    personer som ikke var inkludert i forrige behandling, ettersom de
    uansett ikke har noen endringer
    
    Utleder derfor behandlingsresultat bare basert på personer som var en
    del av forrige behandling
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [x] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [x] Nei

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/behandlingsresultat/BehandlingsresultatService.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/behandlingsresultat/BehandlingsresultatService.kt
@@ -48,7 +48,6 @@ class BehandlingsresultatService(
 
         val vilkårsvurdering = vilkårsvurderingService.hentAktivVilkårsvurderingForBehandling(behandlingId = behandlingId)
 
-        val personerIBehandling = personopplysningGrunnlagService.hentAktivPersonopplysningGrunnlagThrows(behandlingId = behandling.id).personer.toSet()
         val personerIForrigeBehandling = forrigeBehandling?.let { personopplysningGrunnlagService.hentAktivPersonopplysningGrunnlagThrows(behandlingId = forrigeBehandling.id).personer.toSet() } ?: emptySet()
 
         val forrigeVilkårsvurdering = forrigeBehandling?.id?.let { vilkårsvurderingService.hentAktivVilkårsvurderingForBehandling(behandlingId = it) }
@@ -95,7 +94,6 @@ class BehandlingsresultatService(
                     nåværendeKompetanser = kompetanser.toList(),
                     forrigeKompetanser = forrigeKompetanser.toList(),
                     personerFremstiltKravFor = personerFremstiltKravFor,
-                    personerIBehandling = personerIBehandling,
                     personerIForrigeBehandling = personerIForrigeBehandling,
                 )
             } else {
