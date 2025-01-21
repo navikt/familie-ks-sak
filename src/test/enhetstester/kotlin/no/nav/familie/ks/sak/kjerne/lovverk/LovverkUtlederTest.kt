5d54af94b11a89a1d3bdf3c02c246477c19848ed
bragejahren <brage.ekroll.jahren@nav.no>
2025-01-21 13:29:08 +0100

    Mer Regelverk -> Lovverk endringer

--- src/test/enhetstester/kotlin/no/nav/familie/ks/sak/kjerne/lovverk/LovverkUtlederTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/kjerne/lovverk/LovverkUtlederTest.kt
@@ -3,9 +3,9 @@ package no.nav.familie.ks.sak.kjerne.lovverk
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-class RegelverkUtlederTest {
+class LovverkUtlederTest {
     @Test
-    fun `skal returnere regelverk FØR_LOVENDRING_2025 dersom fødselsdato er før FØDSELSDATO_GRENSE_LOVENDRING_FEBRUAR_2025`() {
+    fun `skal returnere lovverk FØR_LOVENDRING_2025 dersom fødselsdato er før FØDSELSDATO_GRENSE_LOVENDRING_FEBRUAR_2025`() {
         // Arrange
         val fødselsdato = LovverkUtleder.FØDSELSDATO_GRENSE_LOVENDRING_FEBRUAR_2025.minusDays(1)
 
@@ -14,7 +14,7 @@ class RegelverkUtlederTest {
     }
 
     @Test
-    fun `skal returnere regelverk LOVENDRING_FEBRUAR_2025 dersom fødselsdato er senere enn FØDSELSDATO_GRENSE_LOVENDRING_FEBRUAR_2025`() {
+    fun `skal returnere lovverk LOVENDRING_FEBRUAR_2025 dersom fødselsdato er senere enn FØDSELSDATO_GRENSE_LOVENDRING_FEBRUAR_2025`() {
         // Arrange
         val fødselsdato = LovverkUtleder.FØDSELSDATO_GRENSE_LOVENDRING_FEBRUAR_2025.plusDays(1)
 
@@ -23,7 +23,7 @@ class RegelverkUtlederTest {
     }
 
     @Test
-    fun `skal returnere regelverk LOVENDRING_FEBRUAR_2025 dersom fødselsdato FØDSELSDATO_GRENSE_LOVENDRING_FEBRUAR_2025`() {
+    fun `skal returnere lovverk LOVENDRING_FEBRUAR_2025 dersom fødselsdato FØDSELSDATO_GRENSE_LOVENDRING_FEBRUAR_2025`() {
         // Arrange
         val fødselsdato = LovverkUtleder.FØDSELSDATO_GRENSE_LOVENDRING_FEBRUAR_2025
 
