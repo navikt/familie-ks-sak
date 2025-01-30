340111b72c16b44067e2939b88d7360d6c23e786
schmelita <schirin.melita@gmail.com>
2025-01-30 15:27:05 +0100

    Slett ubrukt funksjon

--- src/test/enhetstester/kotlin/no/nav/familie/ks/sak/cucumber/mocking/MockUnleashService.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/cucumber/mocking/MockUnleashService.kt
@@ -4,18 +4,10 @@ import io.mockk.every
 import io.mockk.mockk
 import no.nav.familie.ks.sak.config.featureToggle.FeatureToggle
 import no.nav.familie.ks.sak.config.featureToggle.UnleashNextMedContextService
-import no.nav.familie.unleash.UnleashService
 
 fun mockUnleashNextMedContextService(isEnabledDefault: Boolean = true): UnleashNextMedContextService {
     val unleashNextMedContextService = mockk<UnleashNextMedContextService>()
     every { unleashNextMedContextService.isEnabled(any<String>()) } returns isEnabledDefault
     every { unleashNextMedContextService.isEnabled(any<FeatureToggle>()) } returns isEnabledDefault
     return unleashNextMedContextService
-}
-
-fun mockUnleashService(isEnabledDefault: Boolean = true): UnleashService {
-    val unleashService = mockk<UnleashService>()
-    every { unleashService.isEnabled(any()) } returns isEnabledDefault
-    every { unleashService.isEnabled(any(), defaultValue = any()) } returns isEnabledDefault
-    return unleashService
 }
