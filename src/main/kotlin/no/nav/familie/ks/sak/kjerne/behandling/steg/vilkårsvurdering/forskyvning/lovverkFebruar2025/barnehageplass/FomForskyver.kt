f2a9d9dc7d71251a6b1602d7d4eb8eba4bdb73f9
Thomas Alm <106926075+thoalm@users.noreply.github.com>
2025-01-30 14:22:58 +0100

    Nav 24039 forskyv vilkår lov 2025 (#1050)
    ### 💰 Hva skal gjøres, og hvorfor?
    
    https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-24039.
    
    Implementerer forskyvningslogikk for ny lovendring. Den nye logikken
    skal samsvare med 2021 sin forskyvning. Vi har valgt å skille dette ut i
    en egen pakke for ryddighets skyld, men det er mye duplikat kode.
    
    ### 🔎️ Er det noe spesielt du ønsker tilbakemelding om?
    Jeg (Gard) er på ganske tynn is domenemessig, ta helst en nøyere titt om
    det er noe jeg har misforstått. Jeg er spesielt usikker på om
    forskyvningen skal forholde seg til den totale vilkårsperioden eller
    ikke.
    
    Gi gjerne også en ekstra tanke på plassering av kode og navngivning. 
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [X] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [ ] Nei
    
    ---------
    
    Co-authored-by: Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
    Co-authored-by: bragejahren <70642183+bragejahren@users.noreply.github.com>

---
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/behandling/steg/vilkårsvurdering/forskyvning/lovverkFebruar2025/barnehageplass/FomForskyver.kt
@@ -0,0 +1,27 @@
+package no.nav.familie.ks.sak.kjerne.behandling.steg.vilkårsvurdering.forskyvning.lovverkFebruar2025.barnehageplass
+
+import no.nav.familie.ks.sak.common.util.førsteDagIInneværendeMåned
+import java.time.LocalDate
+
+fun forskyvFomBasertPåGraderingsforskjell(
+    fomDato: LocalDate?,
+    graderingsforskjellMellomDenneOgForrigePeriode: Graderingsforskjell,
+): LocalDate? {
+    if (fomDato == null) {
+        return null
+    }
+    return when (graderingsforskjellMellomDenneOgForrigePeriode) {
+        Graderingsforskjell.LIK,
+        Graderingsforskjell.ØKNING_GRUNNET_SLUTT_I_BARNEHAGE,
+        Graderingsforskjell.ØKNING,
+        -> fomDato.minusDays(1).plusMonths(1).førsteDagIInneværendeMåned()
+
+        Graderingsforskjell.INGEN_UTBETALING_GRUNNET_FULL_BARNEHAGEPLASS_TIL_ØKNING,
+        Graderingsforskjell.INGEN_UTBETALING_GRUNNET_FØRSTE_PERIODE_TIL_ØKNING,
+        -> fomDato.plusMonths(1).førsteDagIInneværendeMåned()
+
+        Graderingsforskjell.REDUKSJON_GÅR_TIL_INGEN_UTBETALING,
+        Graderingsforskjell.REDUKSJON,
+        -> fomDato.førsteDagIInneværendeMåned()
+    }
+}
