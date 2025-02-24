d68eff78f815e86bb06379e14c0e090decd9c85d
Ida Merete Enholm <ida.merete.enholm@nav.no>
2025-02-24 11:12:11 +0100

    Chore: fjern toggle for 'støtter adopsjon' (#1117)
    ### 💰 Hva skal gjøres, og hvorfor?
    Favro:
    https://favro.com/organization/98c34fb974ce445eac854de0/1844bbac3b6605eacc8f5543?card=NAV-24426
    
    Fjerner toggle for støtte for adopsjon. Dette har vært skrudd på i prod
    i noen dager og tenker det derfor er trygt å fjerne. Om noe skulle vise
    seg å være feil er det bedre å fikse feilen enn å skru av alt igjen :)
    
    ### 🔎️ Er det noe spesielt du ønsker tilbakemelding om?
    Nei
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [x] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [ ] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    _Jeg har ikke skrevet tester fordi:_
    Fjerner bare toggle
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [x] Nei

--- src/main/kotlin/no/nav/familie/ks/sak/config/featureToggle/FeatureToggle.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/config/featureToggle/FeatureToggle.kt
@@ -12,7 +12,6 @@ enum class FeatureToggle(
 
     // Ikke operasjonelle
     KAN_OPPRETTE_REVURDERING_MED_ÅRSAK_IVERKSETTE_KA_VEDTAK("familie-ks-sak.kan-opprette-revurdering-med-aarsak-iverksette-ka-vedtak"),
-    STØTTER_ADOPSJON("familie-ks-sak.stotter-adopsjon"),
 
     // NAV-24034
     BRUK_NY_SAKSBEHANDLER_NAVN_FORMAT_I_SIGNATUR("familie-ks-sak.bruk-ny-saksbehandler-navn-format-i-signatur"),
