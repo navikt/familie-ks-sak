3f8e13ead8428b786e695c43ad70e479e8c6bb80
schmelita <schirin.melita@gmail.com>
2025-03-10 14:31:44 +0100

    Fjern TODO kommentar

--- src/main/kotlin/no/nav/familie/ks/sak/api/dto/BarnehagebarnRequestParams.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/api/dto/BarnehagebarnRequestParams.kt
@@ -3,10 +3,10 @@ package no.nav.familie.ks.sak.api.dto
 data class BarnehagebarnRequestParams(
     val ident: String?,
     val kommuneNavn: String?,
-    val kunLøpendeFagsak: Boolean, // TODO Skal renames til kunLøpendeAndel
+    val kunLøpendeFagsak: Boolean,
     val limit: Int = 50,
     val offset: Int = 0,
     val sortBy: String = "kommuneNavn",
     val sortAsc: Boolean = false,
-    val kunLøpendeAndel: Boolean = false, // TODO Skal fjernes
+    val kunLøpendeAndel: Boolean = false,
 )
