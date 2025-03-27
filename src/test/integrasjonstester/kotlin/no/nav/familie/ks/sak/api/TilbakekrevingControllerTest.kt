e33317a2aa4aaa1823794d452372429fd81c245f
Ida Merete Enholm <ida.merete.enholm@nav.no>
2025-03-27 13:33:55 +0100

    skriv test for controller

---
+++ src/test/integrasjonstester/kotlin/no/nav/familie/ks/sak/api/TilbakekrevingControllerTest.kt
@@ -0,0 +1,110 @@
+package no.nav.familie.ks.sak.no.nav.familie.ks.sak.api
+
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import io.restassured.RestAssured
+import io.restassured.module.kotlin.extensions.Given
+import io.restassured.module.kotlin.extensions.Then
+import io.restassured.module.kotlin.extensions.When
+import no.nav.familie.kontrakter.felles.tilbakekreving.Behandling
+import no.nav.familie.kontrakter.felles.tilbakekreving.Behandlingsresultatstype
+import no.nav.familie.kontrakter.felles.tilbakekreving.Behandlingsstatus
+import no.nav.familie.kontrakter.felles.tilbakekreving.Behandlingstype
+import no.nav.familie.kontrakter.felles.tilbakekreving.Behandlingsårsakstype
+import no.nav.familie.ks.sak.OppslagSpringRunnerTest
+import no.nav.familie.ks.sak.common.exception.IntegrasjonException
+import no.nav.familie.ks.sak.config.BehandlerRolle
+import no.nav.familie.ks.sak.kjerne.tilbakekreving.TilbakekrevingsbehandlingHentService
+import org.hamcrest.Matchers.hasSize
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpStatus
+import java.time.LocalDateTime
+import java.util.*
+import org.hamcrest.CoreMatchers.`is` as Is
+
+class TilbakekrevingControllerTest : OppslagSpringRunnerTest() {
+    @MockkBean
+    private lateinit var tilbakekrevingsbehandlingHentService: TilbakekrevingsbehandlingHentService
+
+    private val controllerUrl = "/api/tilbakekreving"
+
+    @BeforeEach
+    fun setup(){
+        RestAssured.port = port
+
+        every { tilbakekrevingsbehandlingHentService.hentTilbakekrevingsbehandlinger(any()) } returns emptyList()
+    }
+
+    @Test
+    fun `hentTilbakekrevingsbehandlinger - skal returnere unauthorized dersom brukeren ikke har token for å hente behandling`(){
+        When {
+            get("$controllerUrl/fagsak/123456")
+        } Then {
+            statusCode(HttpStatus.UNAUTHORIZED.value())
+            body("status", Is("FEILET"))
+        }
+    }
+
+    @Test
+    fun `hentTilbakekrevingsbehandlinger - skal returnere 403 dersom brukeren ikke har rettigheter til å hente tilbakekrevingsbehandlinger`() {
+        val token = lokalTestToken(behandlerRolle = BehandlerRolle.UKJENT)
+
+        Given {
+            header("Authorization", "Bearer $token")
+        } When {
+            get("$controllerUrl/fagsak/123456")
+        } Then {
+            statusCode(HttpStatus.FORBIDDEN.value())
+        }
+    }
+
+    @Test
+    fun `hentTilbakekrevingsbehandlinger - skal returnere liste med tilbakekrevingsbehandlinger og 200 OK dersom behandlinger er hentet fra familie-tilbake`() {
+        val token = lokalTestToken(behandlerRolle = BehandlerRolle.VEILEDER)
+
+        val tilbakekrevingsbehandling = Behandling(
+            behandlingId = UUID.randomUUID(),
+            opprettetTidspunkt = LocalDateTime.now(),
+            aktiv = true,
+            årsak = Behandlingsårsakstype.REVURDERING_OPPLYSNINGER_OM_VILKÅR,
+            type = Behandlingstype.TILBAKEKREVING,
+            status = Behandlingsstatus.AVSLUTTET,
+            vedtaksdato = LocalDateTime.now(),
+            resultat = Behandlingsresultatstype.FULL_TILBAKEBETALING
+        )
+        every { tilbakekrevingsbehandlingHentService.hentTilbakekrevingsbehandlinger(fagsakId = 123456) } returns listOf(tilbakekrevingsbehandling)
+
+        Given {
+            header("Authorization", "Bearer $token")
+        } When {
+            get("$controllerUrl/fagsak/123456")
+        } Then {
+            statusCode(HttpStatus.OK.value())
+            body("status", Is("SUKSESS"))
+            body("data", hasSize<Int>(1))
+            body("data[0].behandlingId", Is(tilbakekrevingsbehandling.behandlingId.toString()))
+            body("data[0].resultat", Is(tilbakekrevingsbehandling.resultat.toString()))
+            body("data[0].årsak", Is(tilbakekrevingsbehandling.årsak.toString()))
+            body("data[0].type", Is(tilbakekrevingsbehandling.type.toString()))
+        }
+    }
+
+    @Test
+    fun `hentTilbakekrevingsbehandlinger - skal dersom behandlinger er hentet fra familie-tilbake`() {
+        val token = lokalTestToken(behandlerRolle = BehandlerRolle.BESLUTTER)
+
+        every { tilbakekrevingsbehandlingHentService.hentTilbakekrevingsbehandlinger(fagsakId = 123456) } throws IntegrasjonException("Feilet")
+
+        Given {
+            header("Authorization", "Bearer $token")
+        } When {
+            get("$controllerUrl/fagsak/123456")
+        } Then {
+            statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+            body("status", Is("FEILET"))
+            body("melding", Is("Feilet"))
+        }
+    }
+
+}
