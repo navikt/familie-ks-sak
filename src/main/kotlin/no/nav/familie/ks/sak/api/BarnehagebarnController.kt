96fb0cc287cf19c0fd0a308c19fbe4759cefaf1d
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2025-03-28 14:40:00 +0100

    slett endepunkter for å teste sending av e-post

--- src/main/kotlin/no/nav/familie/ks/sak/api/BarnehagebarnController.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/api/BarnehagebarnController.kt
@@ -1,7 +1,5 @@
 package no.nav.familie.ks.sak.api
 
-import com.azure.identity.ClientSecretCredentialBuilder
-import com.microsoft.graph.serviceclient.GraphServiceClient
 import no.nav.familie.kontrakter.felles.Ressurs
 import no.nav.familie.ks.sak.api.dto.BarnehagebarnRequestParams
 import no.nav.familie.ks.sak.barnehagelister.BarnehagebarnService
@@ -43,53 +41,6 @@ class BarnehagebarnController(
         )
         val alleBarnehagebarnPage = barnehagebarnService.hentBarnehageBarn(barnehagebarnRequestParams)
         return ResponseEntity.ok(Ressurs.success(alleBarnehagebarnPage, "OK"))
-    }
-
-    @PostMapping(
-        path = ["/barnehageliste/send-epost-custom-credentials"],
-        consumes = [MediaType.APPLICATION_JSON_VALUE],
-        produces = [MediaType.APPLICATION_JSON_VALUE],
-    )
-    fun sendEpostMedInnlogging(
-        @RequestBody loginParams: LoginParams,
-    ): ResponseEntity<Ressurs<String>> {
-        tilgangService.validerTilgangTilHandling(
-            minimumBehandlerRolle = BehandlerRolle.FORVALTER,
-            handling = "send e-postvarsel",
-        )
-
-        val credential =
-            ClientSecretCredentialBuilder()
-                .tenantId(loginParams.tenantId)
-                .clientId(loginParams.clientId)
-                .clientSecret(loginParams.clientSecret)
-                .build()
-        val customGraphServiceClient = GraphServiceClient(credential, "https://graph.microsoft.com/.default")
-        val customEpostService = EpostService(customGraphServiceClient)
-
-        customEpostService.sendEpostVarslingBarnehagelister("fredrik.markus.pfeil@nav.no", listOf("hei", "på", "deg"))
-
-        return ResponseEntity.ok(Ressurs.success("OK"))
-    }
-
-    data class LoginParams(
-        val clientId: String,
-        val clientSecret: String,
-        val tenantId: String,
-    )
-
-    @PostMapping(
-        path = ["/barnehageliste/send-epost"],
-    )
-    fun sendEpost(): ResponseEntity<Ressurs<String>> {
-        tilgangService.validerTilgangTilHandling(
-            minimumBehandlerRolle = BehandlerRolle.FORVALTER,
-            handling = "send e-postvarsel",
-        )
-
-        epostService.sendEpostVarslingBarnehagelister("fredrik.markus.pfeil@nav.no", listOf("halla"))
-
-        return ResponseEntity.ok(Ressurs.success("OK"))
     }
 
     @GetMapping(
