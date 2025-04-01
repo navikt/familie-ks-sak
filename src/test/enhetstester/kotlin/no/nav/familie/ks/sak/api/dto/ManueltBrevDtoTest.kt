8b6c11ed2870baeb60997ff99170a66b806fa4b2
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2025-04-01 15:49:04 +0200

    Informasjonsbrev: innhente opplysninger klage (#1212)
    ### 💰 Hva forsøker du å løse i denne PR'en
    Favro: NAV-24768
    
    Legger til nytt informasjonsbrev for når man ønsker å innhente
    opplysninger for en klagesak. Dette legges under informasjonsbrev på
    fagsak siden dette kan innhentes før man starter en behandling.
    
    ### 🔎️ Er det noe spesielt du ønsker å fremheve?
    Første gang jeg jobber med brev, er det noen konvensjoner eller
    prinsipper som ikke er fulgt, si ifra.
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [X] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer. I så fall, husk manuell
    deploy til miljø for å verifisere endringene.
    - [X] Jeg har skrevet tester. Hvis du ikke har skrevet tester, beskriv
    hvorfor under 👇
    
    _Jeg har ikke skrevet tester fordi:_
    
    ### 💬 Ønsker du en muntlig gjennomgang?
    - [ ] Ja
    - [ ] Nei

--- src/test/enhetstester/kotlin/no/nav/familie/ks/sak/api/dto/ManueltBrevDtoTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/api/dto/ManueltBrevDtoTest.kt
@@ -1,12 +1,14 @@
 package no.nav.familie.ks.sak.api.dto
 
 import no.nav.familie.kontrakter.felles.arbeidsfordeling.Enhet
+import no.nav.familie.ks.sak.common.exception.FunksjonellFeil
 import no.nav.familie.ks.sak.data.randomFnr
 import no.nav.familie.ks.sak.kjerne.brev.domene.maler.Brevmal
 import no.nav.familie.ks.sak.kjerne.brev.domene.maler.UtbetalingEtterKAVedtakDataDto
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class ManueltBrevDtoTest {
     val mottakerNavn = "Mottaker Navnesen"
@@ -30,6 +32,36 @@ class ManueltBrevDtoTest {
             }
             assertThat(brevDto.fritekst).isEqualTo("Fritekst avsnitt")
             assertThat(brevDto.delmalData.signatur.saksbehandler).containsExactly("Saks Behandlersen")
+        }
+
+        @Test
+        fun `tilBrev genererer 'innhente opplysninger klage'-brev som forventet`() {
+            val saksbehandler = "Saks Behandlersen"
+            val fritekstAvsnitt = "Fritekst avsnitt"
+            val manueltBrevDto =
+                lagManueltBrevDto(
+                    brevmal = Brevmal.INFORMASJONSBREV_INNHENTE_OPPLYSNINGER_KLAGE,
+                    fritekstAvsnitt = fritekstAvsnitt,
+                )
+            val brev = manueltBrevDto.tilBrev(saksbehandler).data as InformasjonsbrevInnhenteOpplysningerKlageDataDto
+            with(brev.flettefelter) {
+                assertThat(fodselsnummer).containsExactly(manueltBrevDto.mottakerIdent)
+                assertThat(navn).containsExactly(manueltBrevDto.mottakerNavn)
+                assertThat(gjelder).isNull()
+            }
+            assertThat(brev.delmalData.fritekstAvsnitt.fritekstAvsnittTekst).containsExactly(fritekstAvsnitt)
+            assertThat(brev.delmalData.signatur.saksbehandler).containsExactly(saksbehandler)
+        }
+
+        @Test
+        fun `'innhente opplysninger klage'-brev krever at fritekst avsnitt har en verdi`() {
+            val manueltBrevDto =
+                lagManueltBrevDto(
+                    brevmal = Brevmal.INFORMASJONSBREV_INNHENTE_OPPLYSNINGER_KLAGE,
+                    fritekstAvsnitt = null,
+                )
+            val funksjonellFeil = assertThrows<FunksjonellFeil> { manueltBrevDto.tilBrev("Saks Behandlersen") }
+            assertThat(funksjonellFeil.melding).isEqualTo("Du må legge til fritekst for å forklare hvilke opplysninger du ønsker å innhente.")
         }
     }
 
