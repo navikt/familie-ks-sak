113b905c662846d950628b2da95c0faa546dd900
FredrikP <57641333+fredrikpf@users.noreply.github.com>
2025-05-06 13:12:30 +0200

    legg til indekser på kolonner i barnehagebarn for å øke spørringer for å lage barnehagebarnliste

---
+++ src/main/resources/db/migration/V39__indekser_barnehagebarn.sql
@@ -0,0 +1,4 @@
+CREATE INDEX barnehagebarn_fom ON barnehagebarn (fom);
+CREATE INDEX barnehagebarn_ident ON barnehagebarn (ident);
+CREATE INDEX barnehagebarn_kommunenavn ON barnehagebarn (kommune_navn);
+CREATE INDEX vilkar_resultat_vilkar ON vilkar_resultat (vilkar);
