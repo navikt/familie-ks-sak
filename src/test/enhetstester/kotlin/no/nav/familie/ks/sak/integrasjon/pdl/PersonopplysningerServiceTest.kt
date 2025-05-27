eccc1a4c5fd909d7e9a8bd19faeb5a62dc9a5337
Uy Nguyen <uy.quang.nguyen@nav.no>
2025-05-27 10:55:05 +0200

    Endringer etter QA

--- src/test/enhetstester/kotlin/no/nav/familie/ks/sak/integrasjon/pdl/PersonopplysningerServiceTest.kt
+++ src/test/enhetstester/kotlin/no/nav/familie/ks/sak/integrasjon/pdl/PersonopplysningerServiceTest.kt
@@ -15,6 +15,7 @@ import no.nav.familie.ks.sak.integrasjon.pdl.domene.PdlNavn
 import no.nav.familie.ks.sak.integrasjon.pdl.domene.PdlPersonData
 import no.nav.familie.ks.sak.kjerne.personident.PersonidentService
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
@@ -24,94 +25,97 @@ class PersonopplysningerServiceTest {
     private val personidentService = mockk<PersonidentService>()
     private val personopplysningerService = PersonopplysningerService(pdlClient, integrasjonService, personidentService)
 
-    @Test
-    fun `Skal returnere personopplysninger med relasjon hvis det finnes`() {
-        // Arrange
-        val aktør = randomAktør()
-        val barnAktør = randomAktør()
-        val barnIdent = randomPersonident(barnAktør)
-        val fødselsdato = LocalDate.of(2000, 2, 2)
+    @Nested
+    inner class HentPersonInfoMedRelasjonerOgRegisterinformasjon {
+        @Test
+        fun `Skal returnere personopplysninger med relasjon hvis det finnes`() {
+            // Arrange
+            val aktør = randomAktør()
+            val barnAktør = randomAktør()
+            val barnIdent = randomPersonident(barnAktør)
+            val fødselsdato = LocalDate.of(2000, 2, 2)
 
-        val forelderBarnRelasjon =
-            ForelderBarnRelasjon(
-                relatertPersonsIdent = barnIdent.fødselsnummer,
-                relatertPersonsRolle = FORELDERBARNRELASJONROLLE.BARN,
-            )
+            val forelderBarnRelasjon =
+                ForelderBarnRelasjon(
+                    relatertPersonsIdent = barnIdent.fødselsnummer,
+                    relatertPersonsRolle = FORELDERBARNRELASJONROLLE.BARN,
+                )
 
-        val pdlPersonData =
-            PdlPersonData(
-                forelderBarnRelasjon = listOf(forelderBarnRelasjon),
-                folkeregisteridentifikator = emptyList(),
-                foedselsdato = listOf(PdlFødselsDato(fødselsdato.toString())),
-                bostedsadresse = emptyList(),
-            )
+            val pdlPersonData =
+                PdlPersonData(
+                    forelderBarnRelasjon = listOf(forelderBarnRelasjon),
+                    folkeregisteridentifikator = emptyList(),
+                    foedselsdato = listOf(PdlFødselsDato(fødselsdato.toString())),
+                    bostedsadresse = emptyList(),
+                )
 
-        val pdlRelasjonData =
-            PdlPersonData(
-                navn = listOf(PdlNavn("Fornavn", null, "Etternavn")),
-                folkeregisteridentifikator = emptyList(),
-                foedselsdato = listOf(PdlFødselsDato(fødselsdato.toString())),
-                bostedsadresse = emptyList(),
-            )
+            val pdlRelasjonData =
+                PdlPersonData(
+                    navn = listOf(PdlNavn("Fornavn", null, "Etternavn")),
+                    folkeregisteridentifikator = emptyList(),
+                    foedselsdato = listOf(PdlFødselsDato(fødselsdato.toString())),
+                    bostedsadresse = emptyList(),
+                )
 
-        every { pdlClient.hentPerson(aktør, PersonInfoQuery.MED_RELASJONER_OG_REGISTERINFORMASJON) } returns pdlPersonData
-        every { personidentService.hentAktør(barnIdent.fødselsnummer) } returns barnAktør
-        every { integrasjonService.sjekkTilgangTilPerson(barnAktør.aktivFødselsnummer()) } returns Tilgang(barnIdent.fødselsnummer, true)
-        every { pdlClient.hentPerson(barnAktør, PersonInfoQuery.ENKEL) } returns pdlRelasjonData
-        every { pdlClient.hentAdressebeskyttelse(any()) } returns emptyList()
+            every { pdlClient.hentPerson(aktør, PersonInfoQuery.MED_RELASJONER_OG_REGISTERINFORMASJON) } returns pdlPersonData
+            every { personidentService.hentAktør(barnIdent.fødselsnummer) } returns barnAktør
+            every { integrasjonService.sjekkTilgangTilPerson(barnAktør.aktivFødselsnummer()) } returns Tilgang(barnIdent.fødselsnummer, true)
+            every { pdlClient.hentPerson(barnAktør, PersonInfoQuery.ENKEL) } returns pdlRelasjonData
+            every { pdlClient.hentAdressebeskyttelse(any()) } returns emptyList()
 
-        // Act
-        val pdlPersonInfo = personopplysningerService.hentPersonInfoMedRelasjonerOgRegisterinformasjon(aktør)
+            // Act
+            val pdlPersonInfo = personopplysningerService.hentPersonInfoMedRelasjonerOgRegisterinformasjon(aktør)
 
-        // Assert
-        assertThat(pdlPersonInfo.forelderBarnRelasjoner).hasSize(1)
+            // Assert
+            assertThat(pdlPersonInfo.forelderBarnRelasjoner).hasSize(1)
 
-        val barnRelasjon = pdlPersonInfo.forelderBarnRelasjoner.single()
+            val barnRelasjon = pdlPersonInfo.forelderBarnRelasjoner.single()
 
-        assertThat(barnRelasjon.navn).isEqualTo("Fornavn Etternavn")
-        assertThat(barnRelasjon.aktør).isEqualTo(barnAktør)
-    }
+            assertThat(barnRelasjon.navn).isEqualTo("Fornavn Etternavn")
+            assertThat(barnRelasjon.aktør).isEqualTo(barnAktør)
+        }
 
-    @Test
-    fun `Dersom det kastes PdlPersonKanIkkeBehandlesIFagsystem skal man hoppe over personen i relasjonen`() {
-        // Arrange
-        val aktør = randomAktør()
-        val barnAktør = randomAktør()
-        val barnIdent = randomPersonident(barnAktør)
-        val fødselsdato = LocalDate.of(2000, 2, 2)
+        @Test
+        fun `Dersom det kastes PdlPersonKanIkkeBehandlesIFagsystem skal man hoppe over personen i relasjonen`() {
+            // Arrange
+            val aktør = randomAktør()
+            val barnAktør = randomAktør()
+            val barnIdent = randomPersonident(barnAktør)
+            val fødselsdato = LocalDate.of(2000, 2, 2)
 
-        val forelderBarnRelasjon =
-            ForelderBarnRelasjon(
-                relatertPersonsIdent = barnIdent.fødselsnummer,
-                relatertPersonsRolle = FORELDERBARNRELASJONROLLE.BARN,
-            )
+            val forelderBarnRelasjon =
+                ForelderBarnRelasjon(
+                    relatertPersonsIdent = barnIdent.fødselsnummer,
+                    relatertPersonsRolle = FORELDERBARNRELASJONROLLE.BARN,
+                )
 
-        val pdlPersonData =
-            PdlPersonData(
-                forelderBarnRelasjon = listOf(forelderBarnRelasjon),
-                folkeregisteridentifikator = emptyList(),
-                foedselsdato = listOf(PdlFødselsDato(fødselsdato.toString())),
-                bostedsadresse = emptyList(),
-            )
+            val pdlPersonData =
+                PdlPersonData(
+                    forelderBarnRelasjon = listOf(forelderBarnRelasjon),
+                    folkeregisteridentifikator = emptyList(),
+                    foedselsdato = listOf(PdlFødselsDato(fødselsdato.toString())),
+                    bostedsadresse = emptyList(),
+                )
 
-        val pdlRelasjonData =
-            PdlPersonData(
-                navn = listOf(PdlNavn("Fornavn", null, "Etternavn")),
-                folkeregisteridentifikator = emptyList(),
-                foedselsdato = listOf(PdlFødselsDato(fødselsdato.toString())),
-                bostedsadresse = emptyList(),
-            )
+            val pdlRelasjonData =
+                PdlPersonData(
+                    navn = listOf(PdlNavn("Fornavn", null, "Etternavn")),
+                    folkeregisteridentifikator = emptyList(),
+                    foedselsdato = listOf(PdlFødselsDato(fødselsdato.toString())),
+                    bostedsadresse = emptyList(),
+                )
 
-        every { pdlClient.hentPerson(aktør, PersonInfoQuery.MED_RELASJONER_OG_REGISTERINFORMASJON) } returns pdlPersonData
-        every { personidentService.hentAktør(barnIdent.fødselsnummer) } throws PdlPersonKanIkkeBehandlesIFagsystem("test")
-        every { integrasjonService.sjekkTilgangTilPerson(barnAktør.aktivFødselsnummer()) } returns Tilgang(barnIdent.fødselsnummer, true)
-        every { pdlClient.hentPerson(barnAktør, PersonInfoQuery.ENKEL) } returns pdlRelasjonData
-        every { pdlClient.hentAdressebeskyttelse(any()) } returns emptyList()
+            every { pdlClient.hentPerson(aktør, PersonInfoQuery.MED_RELASJONER_OG_REGISTERINFORMASJON) } returns pdlPersonData
+            every { personidentService.hentAktør(barnIdent.fødselsnummer) } throws PdlPersonKanIkkeBehandlesIFagsystem("test")
+            every { integrasjonService.sjekkTilgangTilPerson(barnAktør.aktivFødselsnummer()) } returns Tilgang(barnIdent.fødselsnummer, true)
+            every { pdlClient.hentPerson(barnAktør, PersonInfoQuery.ENKEL) } returns pdlRelasjonData
+            every { pdlClient.hentAdressebeskyttelse(any()) } returns emptyList()
 
-        // Act
-        val pdlPersonInfo = personopplysningerService.hentPersonInfoMedRelasjonerOgRegisterinformasjon(aktør)
+            // Act
+            val pdlPersonInfo = personopplysningerService.hentPersonInfoMedRelasjonerOgRegisterinformasjon(aktør)
 
-        // Assert
-        assertThat(pdlPersonInfo.forelderBarnRelasjoner).isEmpty()
+            // Assert
+            assertThat(pdlPersonInfo.forelderBarnRelasjoner).isEmpty()
+        }
     }
 }
