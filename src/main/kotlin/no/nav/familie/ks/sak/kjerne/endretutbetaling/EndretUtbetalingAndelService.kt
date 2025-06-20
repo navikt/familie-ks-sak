b78f2eec76741b03a3a90fe333bfba99aafdc436
Magnus Tønnessen <Magnus.Tonnessen@nav.no>
2025-06-20 12:17:02 +0200

    Når EndretUtbetalingAndel kopieres, må settet med personer også kopieres

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/endretutbetaling/EndretUtbetalingAndelService.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/endretutbetaling/EndretUtbetalingAndelService.kt
@@ -132,7 +132,7 @@ class EndretUtbetalingAndelService(
         forrigeBehandling: Behandling,
     ) = hentEndredeUtbetalingAndeler(forrigeBehandling.id).forEach {
         val kopiertOverEndretUtbetalingAndel =
-            it.copy(id = 0, behandlingId = behandling.id, erEksplisittAvslagPåSøknad = false, vedtaksbegrunnelser = emptyList())
+            it.copy(id = 0, behandlingId = behandling.id, erEksplisittAvslagPåSøknad = false, vedtaksbegrunnelser = emptyList(), personer = it.personer.toMutableSet())
         endretUtbetalingAndelRepository.save(kopiertOverEndretUtbetalingAndel)
     }
 
