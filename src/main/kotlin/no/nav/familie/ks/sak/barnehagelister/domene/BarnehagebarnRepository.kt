f740a223f95c6b538385d75e26760f380242863e
schmelita <schirin.melita@gmail.com>
2025-09-11 10:25:17 +0200

    fiks integrasjonstest

--- src/main/kotlin/no/nav/familie/ks/sak/barnehagelister/domene/BarnehagebarnRepository.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/barnehagelister/domene/BarnehagebarnRepository.kt
@@ -10,11 +10,13 @@ interface BarnehagebarnRepository : JpaRepository<Barnehagebarn, UUID> { // , Jp
     fun findAllByIdent(ident: String): List<Barnehagebarn>
 
     @Query(
-        """
+        """    
     WITH input AS (
-    --Vi referer til disse variablene flere ganger så da må de legges inn slik
-        SELECT CAST(:ident AS TEXT) AS ident_param, CAST(:kommuneNavn AS TEXT) AS kommunenavn_param
-),     
+        -- Referer til disse variablene flere ganger så de må legges inn slik
+        SELECT 
+        CAST(:ident AS TEXT) as ident_param,
+        CAST (:kommuneNavn AS TEXT) as kommunenavn_param
+    ),
     siste_iverksatte_behandling_i_løpende_fagsak AS (
     -- Finn nyeste iverksatte behandling med utbetaling for løpende og ikke-arkivert fagsak for barn
     SELECT f.id AS fagsakid, MAX(b.aktivert_tid) AS aktivert_tid
@@ -23,7 +25,8 @@ interface BarnehagebarnRepository : JpaRepository<Barnehagebarn, UUID> { // , Jp
              JOIN behandling b ON aty.fk_behandling_id = b.id
              JOIN fagsak f ON b.fk_fagsak_id = f.id
              JOIN tilkjent_ytelse ty ON b.id = ty.fk_behandling_id
-    WHERE (input.ident_param IS NULL OR p.foedselsnummer = input.ident_param)
+             CROSS JOIN input i
+    WHERE (i.ident_param IS NULL OR p.foedselsnummer = i.ident_param)
       AND p.aktiv = true
       AND f.status = 'LØPENDE'
       AND f.arkivert = false
@@ -32,7 +35,7 @@ interface BarnehagebarnRepository : JpaRepository<Barnehagebarn, UUID> { // , Jp
 ),
 lopende_andel_i_siste_iverksatte_behandling AS (
     -- Sjekker om barnet har en løpende andel i siste iverksatte behandling 
-    SELECT p.foedselsnummer, aty.stonad_tom + interval '1 month' >= current_date AS lopende_andel
+    SELECT p.foedselsnummer, aty.stonad_tom + interval '1 month - 1 day' >= current_date AS lopende_andel
     FROM personident p
              JOIN andel_tilkjent_ytelse aty ON p.fk_aktoer_id = aty.fk_aktoer_id
              JOIN behandling b ON aty.fk_behandling_id = b.id
@@ -40,7 +43,8 @@ lopende_andel_i_siste_iverksatte_behandling AS (
              JOIN siste_iverksatte_behandling_i_løpende_fagsak iblf
                   ON f.id = iblf.fagsakid
                   AND b.aktivert_tid = iblf.aktivert_tid
-    WHERE (input.ident_param IS NULL OR p.foedselsnummer = input.ident_param)
+    CROSS JOIN input i
+    WHERE (i.ident_param IS NULL OR p.foedselsnummer = i.ident_param)
       AND p.aktiv = true
 ),
 avvik_antall_timer_vilkar_resultat_og_barnehagebarn AS (
@@ -74,10 +78,10 @@ barnehagebarn_visning AS (
                        ON bb.ident = lab.foedselsnummer
              LEFT JOIN avvik_antall_timer_vilkar_resultat_og_barnehagebarn atv
                        ON bb.id = atv.barnehagebarn_id
-             CROSS JOIN input
+    CROSS JOIN input i
     WHERE (NOT :kunLøpendeAndeler OR lab.lopende_andel = true)
-      AND (input.ident_param IS NULL OR bb.ident = input.ident_param)
-      AND (input.kommunenavn_param IS NULL OR bb.kommune_navn ILIKE input.kommunenavn_param)
+      AND (i.ident_param IS NULL OR bb.ident = i.ident_param)
+      AND (i.kommunenavn_param IS NULL OR bb.kommune_navn ILIKE i.kommunenavn_param)
     GROUP BY bb.ident, bb.fom, bb.tom, bb.antall_timer_i_barnehage, bb.endringstype,
              bb.kommune_navn, bb.kommune_nr, atv.avvik
 )
