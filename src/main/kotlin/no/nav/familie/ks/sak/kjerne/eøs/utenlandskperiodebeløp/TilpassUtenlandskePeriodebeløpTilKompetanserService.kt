8db2a97236dc3d9569f6d1973938054aa6adae30
Uy Nguyen <110383605+UyQuangNguyen@users.noreply.github.com>
2025-12-07 23:07:25 +0100

    Legg til nasjonal rett differanseberegning kompetent land (#1541)
    ### 📮 Favro: NAV-26831
    
    ### 💰 Hva skal gjøres, og hvorfor?
    
    Favrokortet var tidligere feil, og påsto at nasjonal rett differanse
    beregning skulle legges til som en annen forelders aktivitet i
    kompetanse. Dette er feil, og i henhold til bestillingen fra team
    nasjonalt fagansvarlige skal den legges til som kompetent land.
    
    Den har derfor blitt lagt inn som kompetanseresultat her, og den skal
    oppføre seg helt som sekundærland valget.
    Eneste forskjellen er at man får annerledes begrunnelser i vedtak siden.
    <img width="1198" height="750" alt="Screenshot 2025-12-04 at 22 38 46"
    src="https://github.com/user-attachments/assets/03fcf6dc-7b36-4c8e-9457-57b49bdb2507"
    />

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/eøs/utenlandskperiodebeløp/TilpassUtenlandskePeriodebeløpTilKompetanserService.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/eøs/utenlandskperiodebeløp/TilpassUtenlandskePeriodebeløpTilKompetanserService.kt
@@ -8,7 +8,6 @@ import no.nav.familie.ks.sak.kjerne.eøs.felles.domene.EøsSkjemaRepository
 import no.nav.familie.ks.sak.kjerne.eøs.felles.domene.medBehandlingId
 import no.nav.familie.ks.sak.kjerne.eøs.felles.endringsabonnent.EøsSkjemaEndringAbonnent
 import no.nav.familie.ks.sak.kjerne.eøs.kompetanse.domene.Kompetanse
-import no.nav.familie.ks.sak.kjerne.eøs.kompetanse.domene.KompetanseResultat
 import no.nav.familie.ks.sak.kjerne.eøs.utenlandskperiodebeløp.domene.UtenlandskPeriodebeløp
 import no.nav.familie.ks.sak.kjerne.personident.Aktør
 import no.nav.familie.tidslinje.Tidslinje
@@ -80,9 +79,15 @@ internal fun tilpassUtenlandskePeriodebeløpTilKompetanser(
                 kompetanse == null -> null
                 upb == null || upb.utbetalingsland != kompetanse.annenForeldersAktivitetsland ->
                     UtenlandskPeriodebeløp.NULL.copy(utbetalingsland = kompetanse.annenForeldersAktivitetsland)
+
                 else -> upb
             }
         }.tilSkjemaer()
 }
 
-fun Map<Aktør, Tidslinje<Kompetanse>>.filtrerSekundærland() = this.mapValues { (_, tidslinje) -> tidslinje.filtrer { it?.resultat == KompetanseResultat.NORGE_ER_SEKUNDÆRLAND } }
+fun Map<Aktør, Tidslinje<Kompetanse>>.filtrerSekundærland() =
+    this.mapValues { (_, tidslinje) ->
+        tidslinje.filtrer {
+            it?.erNorgeSekundærland() == true
+        }
+    }
