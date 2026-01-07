574060fb4b57b7bebeadacbe47cb55715bcb46d4
Magnus Tønnessen <magnus.tonnessen@nav.no>
2026-01-07 10:23:59 +0100

    Porteføljejustering: validering ved flytting av oppgaver (#1567)
    ### 📮 Favro: NAV-27117
    
    ### 💰 Hva skal gjøres, og hvorfor?
    Vi ønsker å begrense hvilke oppgaver som skal flyttes:
    - Hopper over oppgave dersom tildelt enhet ikke er Vadsø
    - Hopper over oppgave dersom behandlingstype ikke er `NASJONAL`. (Dette
    ekskluderer EØS, tilbakekreving og klage)
    - Hopper over oppgave dersom ny enhet ikke er Bergen
    
    Rydder også opp litt og fjerner overflødig kode, nå som oppgaver for
    tilbakekreving og klage ikke skal flyttes
    
    ### ✅ Checklist
    _Har du husket alle punktene i listen?_
    - [ ] Jeg har testet mine endringer i henhold til akseptansekriteriene
    🕵️
    - [ ] Jeg har config- eller sql-endringer.
    - [x] Jeg har skrevet tester.

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/porteføljejustering/MappeIdMapping.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/porteføljejustering/MappeIdMapping.kt
@@ -4,15 +4,17 @@ import no.nav.familie.ks.sak.common.exception.Feil
 
 private val mappeIdVadsøTilBergen =
     mapOf(
-        100012691L to "100012789",
-        100012692L to "100012790",
-        100012693L to "100012791",
-        100012721L to "100012792",
-        100012695L to "100012765",
+        100012691L to 100012789L,
+        100012692L to 100012790L,
+        100012693L to 100012791L,
+        100012721L to 100012792L,
+        100012695L to 100012765L,
     )
 
 fun hentMappeIdHosBergenSomTilsvarerMappeIVadsø(
-    mappeIdVadsø: Long,
-): String =
-    mappeIdVadsøTilBergen
-        .getOrElse(mappeIdVadsø) { throw Feil("Finner ikke mappe id $mappeIdVadsø i mapping") }
+    mappeIdVadsø: Long?,
+): Long? =
+    mappeIdVadsø?.let {
+        mappeIdVadsøTilBergen
+            .getOrElse(mappeIdVadsø) { throw Feil("Finner ikke mappe id $mappeIdVadsø i mapping") }
+    }
