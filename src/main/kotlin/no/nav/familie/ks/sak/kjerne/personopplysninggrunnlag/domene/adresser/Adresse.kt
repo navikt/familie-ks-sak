06021b9f57d03565df66ad1f5c09ad3b3c39eea1
bragejahren <brage.ekroll.jahren@nav.no>
2026-01-19 16:23:58 +0100

    Liten javadoc for ny Adresse data-klasse

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/personopplysninggrunnlag/domene/adresser/Adresse.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/personopplysninggrunnlag/domene/adresser/Adresse.kt
@@ -8,6 +8,9 @@ import no.nav.familie.kontrakter.felles.personopplysning.UtenlandskAdresse
 import no.nav.familie.kontrakter.felles.personopplysning.Vegadresse
 import java.time.LocalDate
 
+/*
+ Felles representasjon av adresseinformasjon lagret i personopplysninggrunnlag.
+ */
 data class Adresse(
     val gyldigFraOgMed: LocalDate? = null,
     val gyldigTilOgMed: LocalDate? = null,
