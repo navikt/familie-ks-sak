e237ec44a0112a49f029098a10863d87700677e5
Magnus Tønnessen <magnus.tonnessen@nav.no>
2026-01-23 12:51:52 +0100

    Sletter kode relatert til porteføljejustering (#1585)
    ### 📮 Favro: NAV-27350
    
    ### 💰 Hva skal gjøres, og hvorfor?
    Sletter overflødig kode relatert til porteføljejustering

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/porteføljejustering/StartPorteføljejusteringTaskDto.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.familie.ks.sak.kjerne.porteføljejustering
-
-data class StartPorteføljejusteringTaskDto(
-    val antallTasks: Int? = null,
-    val behandlesAvApplikasjon: String? = null,
-    val dryRun: Boolean = true,
-)
