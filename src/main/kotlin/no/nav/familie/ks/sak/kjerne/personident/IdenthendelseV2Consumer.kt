0e4d280818fe99d3a43e9a005a7fbe3861d25b0d
Uy Nguyen <uy.quang.nguyen@nav.no>
2026-08-23 09:10:43 +0200

    NAV-30323: Fjern UKJENT-håndteringen
    Reader-defaulten UKJENT i AktorV2.avdl, warn-loggingen i konsumenten og tilhørende tester er fjernet, slik at skjemaet igjen er en ren kopi av PDL sitt. En ny identtype fra PDL vil dermed fortsatt stoppe konsumenten; det er nå dokumentert av en egen test.

--- src/main/kotlin/no/nav/familie/ks/sak/kjerne/personident/IdenthendelseV2Consumer.kt
+++ src/main/kotlin/no/nav/familie/ks/sak/kjerne/personident/IdenthendelseV2Consumer.kt
@@ -45,8 +45,6 @@ class IdenthendelseV2Consumer(
                 SECURE_LOGGER.warn("Tom aktør fra identhendelse med nøkkel $aktørIdPåHendelse")
             }
 
-            loggHvisUkjentIdenttype(aktør)
-
             val aktivAktørid =
                 aktør
                     ?.identifikatorer
@@ -75,13 +73,6 @@ class IdenthendelseV2Consumer(
             MDC.clear()
         }
         ack.acknowledge()
-    }
-
-    internal fun loggHvisUkjentIdenttype(aktør: Aktor?) {
-        if (aktør?.identifikatorer?.any { it.type == Type.UKJENT } == true) {
-            log.warn("Mottok identhendelse med ukjent identtype fra PDL. Sjekk om Type-enumen i AktorV2.avdl må oppdateres.")
-            SECURE_LOGGER.warn("Mottok identhendelse med ukjent identtype fra PDL: $aktør")
-        }
     }
 
     companion object {
